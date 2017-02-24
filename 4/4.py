056090fe4b450c698185f888d2cdb973cdeb0bc1
oneleaf <oneleaf@gmail.com>
2017-02-24 21:11:11 +0800

    modify

--- 4/4.py
+++ 4/4.py
@@ -12,6 +12,7 @@ image_size=image_h*image_w
 char_set="0123456789"
 char_size=len(char_set)
 captcha_size = 4
+batch_size = 50
 
 # 批量验证码数据
 def get_batch(batch_size=128):
@@ -26,11 +27,11 @@ def get_batch(batch_size=128):
 # 为了使得图片与计算层匹配，我们首先reshape输入图像x为4维的tensor，
 # 第一维 -1 是不限个和 None 类似， 第2、3维对应图片的宽和高，最后一维对应颜色通道的数目，这里是黑白，所以为 1 ，如果图片为 RGB 则为3 。
 x = tf.placeholder(tf.float32, [None, image_size])
-size = tf.size(x)
-x_ = tf.reshape(x, [size, image_w, image_h, 1])
-y_ = tf.placeholder(tf.float32, [None, char_size*captcha_size])
+#batch_size = tf.foldl(lambda a, n: a + 1, x, initializer=0)
+#tf.assert_equal(batch_size,50)
+x_ = tf.reshape(x, [batch_size, image_w, image_h, 1])
+y_ = tf.placeholder(tf.float32, [batch_size, char_size*captcha_size])
 
-tf.Print(size)
 #卷积层
 filter_sizes=[5, 5, 3, 3]
 filter_nums=[32,32,32,32]
@@ -98,52 +99,52 @@ for i in range(captcha_size):
         losses.append(reduced_loss_part)
 loss = tf.reduce_mean(losses)
 
-predictions=[]
-for i in range(captcha_size):
-    with tf.variable_scope('predictions-part-{}'.format(i)):
-        outputs_part = tf.slice(output,begin=[0,i*char_size],size=[-1,char_size])
-        prediction_part = tf.argmax(outputs_part,axis=1)
-        prediction_part = tf.cast(prediction_part, tf.float32)
-        predictions.append(prediction_part)
-prediction = tf.stack(predictions, axis=1)
+# predictions=[]
+# for i in range(captcha_size):
+#     with tf.variable_scope('predictions-part-{}'.format(i)):
+#         outputs_part = tf.slice(output,begin=[0,i*char_size],size=[-1,char_size])
+#         prediction_part = tf.argmax(outputs_part,axis=1)
+#         prediction_part = tf.cast(prediction_part, tf.float32)
+#         predictions.append(prediction_part)
+# prediction = tf.stack(predictions, axis=1)
 
-predictions_y=[]
-for i in range(captcha_size):
-    with tf.variable_scope('predictions-y-part-{}'.format(i)):
-        outputs_part_y = tf.slice(y_,begin=[0,i*char_size],size=[-1,char_size])
-        prediction_part_y = tf.argmax(outputs_part_y,axis=1)
-        prediction_part_y = tf.cast(prediction_part_y, tf.float32)
-        predictions_y.append(prediction_part_y)
-prediction_y = tf.stack(predictions_y, axis=1)
+# predictions_y=[]
+# for i in range(captcha_size):
+#     with tf.variable_scope('predictions-y-part-{}'.format(i)):
+#         outputs_part_y = tf.slice(y_,begin=[0,i*char_size],size=[-1,char_size])
+#         prediction_part_y = tf.argmax(outputs_part_y,axis=1)
+#         prediction_part_y = tf.cast(prediction_part_y, tf.float32)
+#         predictions_y.append(prediction_part_y)
+# prediction_y = tf.stack(predictions_y, axis=1)
 
-correct_prediction = tf.cast(tf.equal(prediction,prediction_y), tf.float32)
-correct_prediction = tf.reduce_mean(correct_prediction, axis=1)
-accuracy = tf.reduce_mean(tf.cast(tf.equal(correct_prediction, 1.0), tf.float32))
+# correct_prediction = tf.cast(tf.equal(prediction,prediction_y), tf.float32)
+# correct_prediction = tf.reduce_mean(correct_prediction, axis=1)
+# accuracy = tf.reduce_mean(tf.cast(tf.equal(correct_prediction, 1.0), tf.float32))
 global_step = tf.Variable(0, trainable=False)
 learning_rate = tf.train.exponential_decay(1e-3, global_step, 3000, 0.96, staircase=True)
 
 train_step = tf.train.MomentumOptimizer(learning_rate, momentum=0.9, use_nesterov=True).minimize(loss,global_step=global_step)
-#train_step = tf.train.AdamOptimizer(1e-4).minimize(loss)
+# train_step = tf.train.AdamOptimizer(1e-4).minimize(loss)
 
 # 比较计算结果是否正确
-#correct_prediction = tf.equal(tf.arg_max(y_conv, 1), tf.arg_max(y_, 1))
+#correct_prediction = tf.equal(tf.arg_max(output, 1), tf.arg_max(y_, 1))
+correct_prediction = tf.equal(output, y_)
 # 统计准确率
-#accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 sess = tf.InteractiveSession()
 sess.run(tf.global_variables_initializer())
-batch_size = 50
+
 for i in range(20000):
     batch = get_batch(batch_size)
     if i % 10 == 0:
+        #print(sess.run([x_],feed_dict={x: batch[0], y_: batch[1]}))
         train_accuacy,_loss = sess.run([accuracy,loss],feed_dict={x: batch[0], y_: batch[1]})
         print("step %d, training accuracy %g, loss %g"%(i, train_accuacy, _loss))
-        #print("a:",_a)
-        #print("b:",_b)
     else:    
         train_step.run(feed_dict = {x: batch[0], y_: batch[1]})
 
 # 最后测试准确率
-batch_x_test, batch_y_test = get_batch(100)
-print("test accuracy %g"%(accuracy.eval(feed_dict={x: batch_x_test, y_: batch_y_test})))
+# batch_x_test, batch_y_test = get_batch(100)
+# print("test accuracy %g"%(accuracy.eval(feed_dict={x: batch_x_test, y_: batch_y_test})))
     
