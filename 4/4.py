f0716cd59f41ea94afeb8706dd0982750e943d2a
王翩 <wp@bsw.com.cn>
2017-02-27 17:45:06 +0800

    add saver

--- 4/4.py
+++ 4/4.py
@@ -5,6 +5,7 @@ from generate_captcha import gen_captcha_text_and_image as captcha
 import numpy as np
 from utils import img2gray,img2vec,text2vec,vec2text
 import tensorflow as tf
+import os,datetime
 
 image_h=80
 image_w=200
@@ -21,14 +22,13 @@ def get_batch(batch_size=128):
     for i in range(batch_size):
         text, image = captcha(char_set=char_set,captcha_size=captcha_size,width=image_w, height=image_h)
         batch_x[i,:] = img2vec(img2gray(image))
-        batch_y[i,:] = list(text) #text2vec(char_set,text)
+        batch_y[i,:] = list(text) # 注意 这里的 lable 不能 one hot
     return batch_x, batch_y
 
 # 为了使得图片与计算层匹配，我们首先reshape输入图像x为4维的tensor，
 # 第一维 -1 是不限个和 None 类似， 第2、3维对应图片的宽和高，最后一维对应颜色通道的数目，这里是黑白，所以为 1 ，如果图片为 RGB 则为3 。
 x = tf.placeholder(tf.float32, [None, image_size])
 x_ = tf.reshape(x, [batch_size, image_w, image_h, 1])
-#y_ = tf.placeholder(tf.float32, [batch_size, char_size*captcha_size])
 y_ = tf.placeholder(tf.int32, [batch_size, captcha_size])
 
 #卷积层
@@ -78,7 +78,6 @@ for i in range(captcha_size):
     with tf.variable_scope('output-part-{}'.format(i)):
         W = tf.get_variable("weights", [hidden_sizes[-1],char_size], initializer=tf.contrib.layers.xavier_initializer())
         b = tf.get_variable("biases", [char_size], initializer=tf.constant_initializer(0.0))
-        #fc_part = tf.nn.relu(tf.matmul(full_connects[-1], W) + b)
         fc_part = tf.matmul(full_connects[-1], W) + b
         outputs.append(fc_part)
 
@@ -91,7 +90,6 @@ for i in range(captcha_size):
     with tf.variable_scope('loss-part-{}'.format(i)):
         outputs_part = tf.slice(output,begin=[0,i*char_size],size=[-1,char_size])
         targets_part = tf.slice(y_,begin=[0,i],size=[-1,1])
-#        targets_part = tf.argmax(targets_part,axis=1)
         targets_part = tf.reshape(targets_part, [-1])
         loss_part = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=outputs_part, labels=targets_part)
         reduced_loss_part = tf.reduce_mean(loss_part)
@@ -107,40 +105,57 @@ for i in range(captcha_size):
         predictions.append(prediction_part)
 prediction = tf.stack(predictions, axis=1)
 
-# predictions_y=[]
-# for i in range(captcha_size):
-#     with tf.variable_scope('predictions-y-part-{}'.format(i)):
-#         outputs_part_y = tf.slice(y_,begin=[0,i*char_size],size=[-1,char_size])
-#         prediction_part_y = tf.argmax(outputs_part_y,axis=1)
-#         prediction_part_y = tf.cast(prediction_part_y, tf.float32)
-#         predictions_y.append(prediction_part_y)
-# prediction_y = tf.stack(predictions_y, axis=1)
-
 correct_prediction = tf.cast(tf.equal(prediction,y_), tf.float32)
 correct_prediction = tf.reduce_mean(correct_prediction, axis=1)
 accuracy = tf.reduce_mean(tf.cast(tf.equal(correct_prediction, 1.0), tf.float32))
 global_step = tf.Variable(0, trainable=False)
 learning_rate = tf.train.exponential_decay(1e-3, global_step, 2000, 0.96, staircase=True)
 
-train_step = tf.train.MomentumOptimizer(learning_rate, momentum=0.9, use_nesterov=True).minimize(loss,global_step=global_step)
-# train_step = tf.train.AdamOptimizer(1e-4).minimize(loss)
-
+optimizer = tf.train.MomentumOptimizer(learning_rate, momentum=0.9, use_nesterov=True)
+# optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+# optimizer = tf.train.AdadeltaOptimizer(learning_rate)
+# optimizer = tf.train.AdagradOptimizer(learning_rate)
+# optimizer = tf.train.FtrlOptimizer(learning_rate)
+# optimizer = tf.train.RMSPropOptimizer(learning_rate)
+# optimizer = tf.train.AdamOptimizer(learning_rate)
+train_step = optimizer.minimize(loss,global_step=global_step)
 
 sess = tf.InteractiveSession()
 sess.run(tf.global_variables_initializer())
 
-for i in range(100000):
-    batch = get_batch(batch_size)
-#    if i % 2 == 0:
-    _,train_accuacy,y1,y2 = sess.run([train_step,accuracy,prediction,y_],feed_dict={x: batch[0], y_: batch[1]})
-    print("step: %s, training accuracy: %s"%(i, train_accuacy))
-    print(y1[:5])
-    print(y2[:5])
+# 定义中断后的恢复和继续训练
+saver = tf.train.Saver()
+out_dir = os.path.dirname(__file__)
+checkpoints_dir = os.path.join(out_dir, "checkpoints")
+ckpt = tf.train.get_checkpoint_state(checkpoints_dir)
+if ckpt and ckpt.model_checkpoint_path:
+    print("继续接着之前的进度进行训练")
+    saver.restore(sess, ckpt.model_checkpoint_path)
 
-#    else:    
-#        train_step.run(feed_dict = {x: batch[0], y_: batch[1]})
+# 定义图表输出
+loss_summary = tf.summary.scalar("loss", loss)
+acc_summary = tf.summary.scalar("accuracy", accuracy)
+train_summary_op = tf.summary.merge_all()
+train_summary_dir = os.path.join(out_dir, "summaries")
+train_summary_writer = tf.summary.FileWriter(train_summary_dir, sess.graph)
 
-# 最后测试准确率
-# batch_x_test, batch_y_test = get_batch(100)
-# print("test accuracy %g"%(accuracy.eval(feed_dict={x: batch_x_test, y_: batch_y_test})))
-    
+# 定义运行协调
+coord = tf.train.Coordinator()
+threads = tf.train.start_queue_runners(coord=coord, sess=sess)
+
+# 运行
+try:
+    while not coord.should_stop():
+        batch = get_batch(batch_size)
+        _, _train_summaries, _loss, _preds, _acc, _step = sess.run(
+                        [train_step, train_summary_op, loss, predictions, accuracy, global_step],
+                        feed_dict={x: batch[0], y_: batch[1]})
+        time_str = datetime.datetime.now().strftime("%d, %b %Y %H:%M:%S")                        
+        print(time_str, _step, _acc, _loss)                    
+        train_summary_writer.add_summary(_train_summaries, _step)
+        if _step % 10 == 0:
+            path = saver.save(sess, checkpoints_dir, global_step=_step)
+            print("保存训练数据到"，path)
+finally:
+    coord.request_stop()
+coord.join(threads)
