5992be342942e389fa5864c2d23081970efc93e7
oneleaf <oneleaf@gmail.com>
2017-03-03 01:09:25 +0800

    test

--- 4/4_1.py
+++ 4/4_1.py
@@ -3,6 +3,7 @@
 这个是从训练模型中加载数据
 '''
 from generate_captcha import gen_captcha_text_and_image as captcha
+from utils import img2gray, img2vec, text2vec, vec2text
 import numpy as np
 import tensorflow as tf
 import os
@@ -36,14 +37,14 @@ else:
 text, image = captcha(char_set="0123456789", captcha_size=4, width=200, height=80)
 
 # 如果是彩色照片，直接转灰度
-if image.shape[2] != 1:
-    image = tf.image.rgb_to_grayscale(image)
+#if image.shape[2] != 1:
+#    image = tf.image.rgb_to_grayscale(image)
 
 # 如果图片不符合训练的大小，需要调整图片大小    
-image = tf.image.resize_images(image, (200, 80))
-imagedata = tf.reshape(image, (1, 200, 80, 1))
+image = img2vec(img2gray(image)) #tf.image.resize_images(image, (200, 80))
+#imagedata = tf.reshape(image, (1, 200, 80, 1))
 x = tf.get_default_graph().get_tensor_by_name('x:0')
 prediction = tf.get_default_graph().get_tensor_by_name('stack:0')
-imagedata = imagedata.eval(session=sess)
-print(imagedata.shape)
-sess.run([prediction], feed_dict={x: imagedata})
+#imagedata = image.eval(session=sess)
+#print(imagedata.shape)
+sess.run([prediction], feed_dict={x: image})
