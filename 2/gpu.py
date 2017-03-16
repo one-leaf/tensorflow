23b4345d2e61f758473c23b090140536455d88cd
王翩 <wp@bsw.com.cn>
2017-03-16 14:59:15 +0800

    add one gpu

--- 2/gpu.py
+++ 2/gpu.py
@@ -12,6 +12,16 @@ print sess.run(c)
 
 
 # 新建一个 graph.
+with tf.device('/gpu:1'):
+  a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
+  b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
+  c = tf.matmul(a, b)
+# 新建 session with log_device_placement 并设置为 True.
+sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=True))
+# 运行这个 op.
+print sess.run(c)
+
+# 新建一个 graph.
 c = []
 for d in ['/gpu:0', '/gpu:1']:
   with tf.device(d):
