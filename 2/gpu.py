f93ffbe615b350dcfa43645dac80846112f9cad1
王翩 <wp@bsw.com.cn>
2017-03-16 14:53:35 +0800

    add two gpu

--- 2/gpu.py
+++ 2/gpu.py
@@ -9,3 +9,18 @@ c = tf.matmul(a, b)
 sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
 # 运行这个 op.
 print sess.run(c)
+
+
+# 新建一个 graph.
+c = []
+for d in ['/gpu:0', '/gpu:1']:
+  with tf.device(d):
+    a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3])
+    b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2])
+    c.append(tf.matmul(a, b))
+with tf.device('/cpu:0'):
+  sum = tf.add_n(c)
+# 新建session with log_device_placement并设置为True.
+sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+# 运行这个op.
+print sess.run(sum)
