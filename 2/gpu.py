2ea5a8c41f2b8a6775c9a2a2e5e3024f48bd2464
王翩 <wp@bsw.com.cn>
2017-03-16 15:18:22 +0800

    add pi

--- 2/gpu.py
+++ 2/gpu.py
@@ -34,3 +34,26 @@ with tf.device('/cpu:0'):
 sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
 # 运行这个op.
 print sess.run(sum)
+
+
+import tensorflow as tf
+import numpy as np
+c = []
+
+#Distribute the work between the GPUs
+for d in ['/gpu:0', '/gpu:1']:
+    #Generate the random 2D samples
+    i=tf.constant(np.random.uniform(size=10000), shape=[5000,2])
+    with tf.Session() as sess:
+        tf.global_variables_initializer().run()
+        #Calculate the euclidean distance to the origin
+        distances=tf.reduce_sum(tf.pow(i,2),1)
+        #Sum the samples inside the circle
+        tempsum = sess.run(tf.reduce_sum(tf.cast(tf.greater_equal(tf.cast(1.0,tf.float64),distances),tf.float64)))
+        #append the current result to the results array
+        c.append( tempsum)
+    #Do the final ratio calculation on the CPU
+    with tf.device('/cpu:0'):
+        with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as sess:
+            sum = tf.add_n(c)
+            print (sess.run(sum/20000.0)*4.0)
