24cbe97bf7c63efc764741d6cd01d16b36bb93e1
王翩 <wp@bsw.com.cn>
2017-03-21 18:23:05 +0800

    .

--- 5/2/5.py
+++ 5/2/5.py
@@ -84,7 +84,7 @@ class Game(object):
 DEBUG = True
 ACTIONS_COUNT = 3  # 可选的动作，针对 左移 不动 右移
 FUTURE_REWARD_DISCOUNT = 0.99  # 下一次奖励的衰变率
-OBSERVATION_STEPS = 500.  # 在学习前观察的次数
+OBSERVATION_STEPS = 50000.  # 在学习前观察的次数
 EXPLORE_STEPS = 500000.  # 每次机器自动参与的概率的除数
 INITIAL_RANDOM_ACTION_PROB = 1.0  # 随机移动的最大概率
 FINAL_RANDOM_ACTION_PROB = 0.05  # 随机移动的最小概率
