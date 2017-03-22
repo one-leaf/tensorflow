47f53e72f700dcaa4c88277e1d25fcdc92d417b8
王翩 <wp@bsw.com.cn>
2017-03-22 16:24:35 +0800

    keep best

--- 5/2/1.py
+++ 5/2/1.py
@@ -1,16 +1,16 @@
 # coding=utf-8
 
-import pygame
+# This is heavily based off https://github.com/asrivat1/DeepLearningVideoGames
+import os
 import random
-from pygame.locals import *
-import numpy as np
 from collections import deque
-import tensorflow as tf  # http://blog.topspeedsnail.com/archives/10116
-import cv2               # http://blog.topspeedsnail.com/archives/4755
-import os 
-import sys
-import platform
- 
+import tensorflow as tf
+import numpy as np
+import cv2
+import pygame
+from pygame.locals import *
+import platform,sys
+
 BLACK     = (0  ,0  ,0  )
 WHITE     = (255,255,255)
  
@@ -18,12 +18,9 @@ SCREEN_SIZE = [320,400]
 BAR_SIZE = [50, 5]
 BALL_SIZE = [15, 15]
  
-# 神经网络的输出
-MOVE_STAY = [0, 1, 0]
-MOVE_LEFT = [1, 0, 0]
-MOVE_RIGHT = [0, 0, 1]
-
-DEBUG = True
+MOVE_LEFT = [1, 0, 0]   # 左移
+MOVE_STAY = [0, 1, 0]   # 不动
+MOVE_RIGHT = [0, 0, 1]  # 右移
 
 class Game(object):
     def __init__(self):
@@ -43,9 +40,7 @@ class Game(object):
         self.bar_pos = pygame.Rect(self.bar_pos_x, SCREEN_SIZE[1]-BAR_SIZE[1], BAR_SIZE[0], BAR_SIZE[1])
  
     # action是MOVE_STAY、MOVE_LEFT、MOVE_RIGHT
-    # ai控制棒子左右移动；返回游戏界面像素数和对应的奖励。(像素->奖励->强化棒子往奖励高的方向移动)
     def step(self, action):
- 
         if action == MOVE_LEFT:
             self.bar_pos_x = self.bar_pos_x - 2
         elif action == MOVE_RIGHT:
@@ -81,211 +76,187 @@ class Game(object):
         pygame.display.update()
         # 返回游戏界面像素和对应的奖励
         return reward, screen_image
- 
-# 存储过往经验大小
-REPLAY_MEMORY = 100000
-# 每一批的大小 
-BATCH = 1000
 
-curr_dir = os.path.dirname(__file__)
-data_dir = os.path.join(curr_dir, "data")
-model_dir = os.path.join(curr_dir, "game_model")
-if not os.path.exists(model_dir):
-    os.mkdir(model_dir)
+# 参数设置
+DEBUG = True    # 是否开启调试 到程序目录执行 tensorboard --logdir=game_model ，访问 http://127.0.0.1:6006
+ACTIONS_COUNT = 3  # 可选的动作，针对 左移 不动 右移
+FUTURE_REWARD_DISCOUNT = 0.99  # 下一次奖励的衰变率
+OBSERVATION_STEPS = 50000.  # 在学习前观察的次数
+EXPLORE_STEPS = 500000.  # 每次机器自动参与的概率的除数
+INITIAL_RANDOM_ACTION_PROB = 1.0  # 随机移动的最大概率
+FINAL_RANDOM_ACTION_PROB = 0.05  # 随机移动的最小概率
+MEMORY_SIZE = 500000  # 记住的观察队列
+MINI_BATCH_SIZE = 100  # 每次学习的批次
+STATE_FRAMES = 4  # 每次保存的状态数
+RESIZED_SCREEN_X, RESIZED_SCREEN_Y = (80, 100)   # 图片缩小后的尺寸
+OBS_LAST_STATE_INDEX, OBS_ACTION_INDEX, OBS_REWARD_INDEX, OBS_CURRENT_STATE_INDEX = range(4)
+SAVE_EVERY_X_STEPS = 100  # 每学习多少轮后保存
+STORE_SCORES_LEN = 200.     # 分数保留的长度
 
-output = 3  # 输出层神经元数。代表3种操作-MOVE_STAY:[1, 0, 0]  MOVE_LEFT:[0, 1, 0]  MOVE_RIGHT:[0, 0, 1]
-input_image = tf.placeholder("float", [None, 80, 100, 4])  # 游戏图像，每次都是连续4张图像一组，这里借用了cmyk的4色通道，最大为4张
+# 初始化保存对象，如果有数据，就恢复
+def restore(sess):
+    curr_dir = os.path.dirname(__file__)
+    model_dir = os.path.join(curr_dir, "game_model")
+    if not os.path.exists(model_dir): os.mkdir(model_dir)
+    saver_prefix = os.path.join(model_dir, "model.ckpt")        
+    ckpt = tf.train.get_checkpoint_state(model_dir)
+    saver = tf.train.Saver(max_to_keep=1)
+    if ckpt and ckpt.model_checkpoint_path:
+        print("restore model ...")
+        saver.restore(sess, ckpt.model_checkpoint_path)
+    return saver, model_dir, saver_prefix
 
-if DEBUG:
-    tf.summary.image('input_image', tensor=input_image,  max_outputs=1)
-
-action = tf.placeholder("float", [None, output])     # 操作
- 
-# 定义CNN-卷积神经网络 参考:http://blog.topspeedsnail.com/archives/10451
-def convolutional_neural_network(input_image):
-    weights = {'w_conv1':tf.Variable(tf.random_normal([8, 8, 4, 32], mean=0.0, stddev=1.0)),
-               'w_conv2':tf.Variable(tf.random_normal([4, 4, 32, 64], mean=0.0, stddev=1.0)),
-               'w_conv3':tf.Variable(tf.random_normal([3, 3, 64, 64], mean=0.0, stddev=1.0)),
-               'w_fc4':tf.Variable(tf.random_normal([3456, 784], mean=0.0, stddev=1.0)),
-               'w_out':tf.Variable(tf.random_normal([784, output], mean=0.0, stddev=1.0))}
- 
-    biases = {'b_conv1':tf.Variable(tf.constant(0.1, shape=[32])),
-              'b_conv2':tf.Variable(tf.constant(0.1, shape=[64])),
-              'b_conv3':tf.Variable(tf.constant(0.1, shape=[64])),
-              'b_fc4':tf.Variable(tf.constant(0.1, shape=[784])),
-              'b_out':tf.Variable(tf.constant(0.1, shape=[output]))}
-    # input_image : (?, 80, 100, 4)
-    conv1 = tf.nn.relu(tf.nn.conv2d(input_image, weights['w_conv1'], strides = [1, 4, 4, 1], padding = "VALID") + biases['b_conv1'])
-
-    # conv1 : （？， 19， 24， 32）
-    conv2 = tf.nn.relu(tf.nn.conv2d(conv1, weights['w_conv2'], strides = [1, 2, 2, 1], padding = "VALID") + biases['b_conv2'])
-
-    # conv2 :  (?, 8, 11, 64)
-    conv3 = tf.nn.relu(tf.nn.conv2d(conv2, weights['w_conv3'], strides = [1, 1, 1, 1], padding = "VALID") + biases['b_conv3'])
-
-    # conv3 :  (?, 6, 9, 64)
-    conv3_flat = tf.reshape(conv3, [-1, 3456])
-
-    # conv3_flat : (?, 3456)
-    fc4 = tf.nn.relu(tf.matmul(conv3_flat, weights['w_fc4']) + biases['b_fc4'])
-    # fc4 : (?, 784)
-    output_layer = tf.nn.softmax(tf.matmul(fc4, weights['w_out']) + biases['b_out'])
-    # output_layer : (?, 3)
+# 神经网络定义
+def get_network():
+    x = tf.placeholder("float", [None, RESIZED_SCREEN_X, RESIZED_SCREEN_Y, STATE_FRAMES])   # 输入的图片，是每4张一组
+    def get_w_b(w_shape,w_name,b_name):
+        w = tf.get_variable(w_name, w_shape, initializer=tf.contrib.layers.xavier_initializer())
+        b = tf.get_variable(b_name, [w_shape[-1]], initializer=tf.constant_initializer(0.01))
+        return w,b
+    w1, b1 = get_w_b([8, 8, STATE_FRAMES, 32],"w1","b1")
+    w2, b2 = get_w_b([4, 4, 32, 64],"w2","b2")
+    w3, b3 = get_w_b([3, 3, 64, 64],"w3","b3")
+    fw1, fb1 = get_w_b([3 * 4 * 64, 256],"fw1","fb1")
+    fw2, fb2 = get_w_b([256, ACTIONS_COUNT],"fw2","fb2")
+    hidden_layer_1  = tf.nn.relu(tf.nn.conv2d(x, w1, strides=[1, 4, 4, 1], padding="SAME") + b1)
+    hidden_pool_1   = tf.nn.max_pool(hidden_layer_1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME")
+    hidden_layer_2  = tf.nn.relu(tf.nn.conv2d(hidden_pool_1, w2, strides=[1, 2, 2, 1], padding="SAME") + b2)
+    hidden_pool_2   = tf.nn.max_pool(hidden_layer_2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME")
+    hidden_layer_3  = tf.nn.relu(tf.nn.conv2d(hidden_layer_2, w3, strides=[1, 1, 1, 1], padding="SAME") + b3)
+    hidden_pool_3   = tf.nn.max_pool(hidden_layer_3, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME")
+    hidden_pool_3_flat = tf.reshape(hidden_pool_3, [-1, 3 * 4 * 64])
+    final_hidden_activations = tf.nn.relu(tf.matmul(hidden_pool_3_flat, fw1) + fb1)
+    y = tf.matmul(final_hidden_activations, fw2) + fb2
     if DEBUG:
-        tf.summary.histogram('w_conv1', weights['w_conv1'])    
-        tf.summary.histogram('b_conv1', biases['b_conv1'])    
-        tf.summary.histogram('w_conv2', weights['w_conv2'])    
-        tf.summary.histogram('b_conv2', biases['b_conv2'])    
-        tf.summary.histogram('w_conv3', weights['w_conv3'])    
-        tf.summary.histogram('b_conv3', biases['b_conv3'])    
-        tf.summary.histogram('w_fc4', weights['w_fc4'])    
-        tf.summary.histogram('b_fc4', biases['b_fc4'])    
-        tf.summary.histogram('w_out', weights['w_out'])    
-        tf.summary.histogram('b_out', biases['b_out'])    
+        tf.summary.histogram('w1', w1)    
+        tf.summary.histogram('b1', b1)    
+        tf.summary.histogram('w2', w2)    
+        tf.summary.histogram('b2', b2)   
+        tf.summary.histogram('w3', w3)    
+        tf.summary.histogram('b3', b3)            
+        tf.summary.histogram('fw1', fw1)    
+        tf.summary.histogram('fb1', fb1)  
+        tf.summary.histogram('fw2', fw2)    
+        tf.summary.histogram('fb2', fb2)  
+        filter_map1 = tf.transpose(hidden_pool_1[-1], perm=[2, 0, 1])   
+        filter_map1 = tf.reshape(filter_map1, (32, int(filter_map1.get_shape()[1]), int(filter_map1.get_shape()[2]), 1)) 
+        tf.summary.image('hidden_pool_1', tensor=filter_map1,  max_outputs=32)      
+        filter_map2 = tf.transpose(hidden_pool_2[-1], perm=[2, 0, 1])   
+        filter_map2 = tf.reshape(filter_map2, (64, int(filter_map2.get_shape()[1]), int(filter_map2.get_shape()[2]), 1)) 
+        tf.summary.image('hidden_pool_2', tensor=filter_map2,  max_outputs=64)     
+        filter_map3 = tf.transpose(hidden_pool_3[-1], perm=[2, 0, 1])   
+        filter_map3 = tf.reshape(filter_map3, (64, int(filter_map3.get_shape()[1]), int(filter_map3.get_shape()[2]), 1)) 
+        tf.summary.image('hidden_pool_3', tensor=filter_map3,  max_outputs=64)     
+    return x, y
 
-    return output_layer
- 
-# 深度强化学习入门: https://www.nervanasys.com/demystifying-deep-reinforcement-learning/
-# 训练神经网络
-def train_neural_network(input_image):
-    predict_action = convolutional_neural_network(input_image)  # (?, 3)
+# 学习
+def train():    
+    _input_layer , _output_layer = get_network()
+    
+    _action = tf.placeholder("float", [None, ACTIONS_COUNT])    # 移动的方向
+    _target = tf.placeholder("float", [None])                   # 得分
 
-    argmax = tf.placeholder("float", [None, output])    # 移动的方向
-    gt = tf.placeholder("float", [None])                # 得分
- 
     # 将预测的结果和移动的方向相乘，按照第二维度求和 [0.1,0.2,0.7] * [0, 1, 0] = [0, 0.2 ,0] = [0.2]  得到当前移动的概率
-    action = tf.reduce_sum(tf.multiply(predict_action, argmax), reduction_indices = 1)
+    readout_action = tf.reduce_sum(tf.multiply(_output_layer, _action), reduction_indices=1)
     # 将（结果和评价相减）的平方，再求平均数。 得到和评价的距离。
-    # 当评价为0的时候，距离为当前概率，当评价为1的时候，距离为移动正确的概率-1，距离最小，当前为-1时，距离最大
-    cost = tf.reduce_sum(tf.square(action - gt))
- 
+    cost = tf.reduce_mean(tf.square(_target - readout_action))
+
     # 定义学习速率和优化方法,因为大部分匹配都是0，所以学习速率必需订的非常小
     global_step = tf.Variable(0, trainable=False)
-    learning_rate = tf.train.exponential_decay(1e-4, global_step, 10000, 0.98, staircase=True)
+    learning_rate = tf.train.exponential_decay(1e-6, global_step, 10000, 0.98, staircase=True)
+    # 学习函数
+    _train_operation = tf.train.AdamOptimizer(learning_rate).minimize(cost, global_step=global_step)
 
-    optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost, global_step=global_step)
+    _observations = deque()
+    _last_scores = deque()
+    
+    # 设置最后一步是固定
+    _last_action = MOVE_STAY
+    _last_state = None          #4次的截图
+    _probability_of_random_action = INITIAL_RANDOM_ACTION_PROB
 
     game = Game()
-    D = deque()
 
-    _, image = game.step(MOVE_STAY)
-    # 转换为灰度值
-    image = cv2.cvtColor(cv2.resize(image, (100, 80)), cv2.COLOR_BGR2GRAY)
-    # 转换为二值
-    ret, image = cv2.threshold(image, 1, 255, cv2.THRESH_BINARY)    # image shape: (80, 100)
-    # 定义了一组图片包含4张照片
-    input_image_data = np.stack((image, image, image, image), axis = 2) # shape: (80, 100, 4)
+    _session = tf.Session()       
+    _session.run(tf.global_variables_initializer())
 
-    with tf.Session() as sess:
-       
-        sess.run(tf.global_variables_initializer())
+    _saver,_model_dir,_checkpoint_path = restore(_session)
 
-        if DEBUG:
-            tf.summary.scalar("cost", cost)
-            tf.summary.scalar("learning_rate", learning_rate)
-        train_summary_op = tf.summary.merge_all()
-        train_summary_writer = tf.summary.FileWriter(model_dir, sess.graph)
+    if DEBUG:
+        tf.summary.scalar("cost", cost)
+        tf.summary.scalar("learning_rate", learning_rate)        
+        _train_summary_op = tf.summary.merge_all()
+        _train_summary_writer = tf.summary.FileWriter(_model_dir, _session.graph)
 
+    while True:
+        reward, image = game.step(list(_last_action))
 
-        saver_prefix = os.path.join(model_dir, "model.ckpt")        
-        ckpt = tf.train.get_checkpoint_state(model_dir)
-        saver = tf.train.Saver(max_to_keep=1)
+        if platform.system()!="Linux":
+            for event in pygame.event.get():  # Linux不需要事件循环，其余需要否则白屏
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()   
 
-        if ckpt and ckpt.model_checkpoint_path:
-            print("restore model ...")
-            saver.restore(sess, ckpt.model_checkpoint_path)
+        image = cv2.resize(image,(RESIZED_SCREEN_Y, RESIZED_SCREEN_X))
 
-        coord = tf.train.Coordinator()
-        threads = tf.train.start_queue_runners(coord=coord, sess=sess)   
+        screen_resized_grayscaled = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+        _, screen_resized_binary = cv2.threshold(screen_resized_grayscaled, 1, 255, cv2.THRESH_BINARY)
+        if reward != 0.0:
+            _last_scores.append(reward)
+            if len(_last_scores) > STORE_SCORES_LEN:
+                _last_scores.popleft()
 
-        # 动态调整是否采用预测的值作为一下步
-        SUCCESS_COUNT = 0
-        IGNORE_COUNT = 0  # 为了加快运算，计分成功后的100张不需要考虑
-        while not coord.should_stop():            
-            argmax_t = np.zeros([output], dtype=np.int)
+        if _last_state is None:  # 填充第一次的4张图片            
+            _last_state = np.stack(tuple(screen_resized_binary for _ in range(STATE_FRAMES)), axis=2)
+
+        screen_resized_binary = np.reshape(screen_resized_binary, (RESIZED_SCREEN_X, RESIZED_SCREEN_Y, 1))
+        current_state = np.append(_last_state[:, :, 1:], screen_resized_binary, axis=2)
+        
+        _observations.append((_last_state, _last_action, reward, current_state))
+        if len(_observations) > MEMORY_SIZE:
+            _observations.popleft()
+        
+        if len(_observations) > OBSERVATION_STEPS:
+            mini_batch = random.sample(_observations, MINI_BATCH_SIZE)
+            previous_states = [d[OBS_LAST_STATE_INDEX] for d in mini_batch]
+            actions = [d[OBS_ACTION_INDEX] for d in mini_batch]
+            rewards = [d[OBS_REWARD_INDEX] for d in mini_batch]
+            current_states = [d[OBS_CURRENT_STATE_INDEX] for d in mini_batch]
+
+            agents_expected_reward = []
+            agents_reward_per_action = _session.run(_output_layer, feed_dict={_input_layer: current_states})
+            for i in range(len(mini_batch)):
+                agents_expected_reward.append(rewards[i]  + FUTURE_REWARD_DISCOUNT * np.max(agents_reward_per_action[i]))
+
+            if DEBUG:
+                _, _step, train_summary_op =  _session.run([_train_operation,global_step,_train_summary_op], feed_dict={_input_layer: previous_states,_action: actions,
+                        _target: agents_expected_reward})
+            else:            
+                _, _step = _session.run([_train_operation,global_step], feed_dict={_input_layer: previous_states,_action: actions,
+                        _target: agents_expected_reward})
+
+            if _step % SAVE_EVERY_X_STEPS == 0:
+                _saver.save(_session, _checkpoint_path, global_step=_step)
+                if DEBUG:
+                    _train_summary_writer.add_summary(train_summary_op, _step)
+                print("step: %s random_action_prob: %s reward %s scores differential %s" %
+                  (_step, _probability_of_random_action, reward, sum(_last_scores) / STORE_SCORES_LEN))
+
+        _last_state = current_state
+
+        # 游戏执行下一步,按概率选择下一次是随机还是机器进行移动
+        _last_action = np.zeros([ACTIONS_COUNT],dtype=np.int)
+        if random.random() <= _probability_of_random_action:
+            action_index = random.randrange(ACTIONS_COUNT)
+        else:
+            readout_t = _session.run(_output_layer, feed_dict={_input_layer: [_last_state]})[0]
+            action_index = np.argmax(readout_t)
+        _last_action[action_index] = 1
+
+        # 随机移动的概率逐步降低
+        if _probability_of_random_action > FINAL_RANDOM_ACTION_PROB and len(_observations) > OBSERVATION_STEPS:
+            _probability_of_random_action -= (INITIAL_RANDOM_ACTION_PROB - FINAL_RANDOM_ACTION_PROB) / EXPLORE_STEPS
             
-            # 如果累计有10次成功就尝试机器走
-            if SUCCESS_COUNT < 10:
-                maxIndex = random.randrange(output)
-            else:
-                action_t = predict_action.eval(feed_dict = {input_image : [input_image_data]})[0]
-                maxIndex = np.argmax(action_t)
 
-            argmax_t[maxIndex] = 1
-
-            # 游戏按预测的下一步                
-            reward, image = game.step(list(argmax_t))           
-
-            # 如果有1次失败，清除成功率
-            if reward == -1 and SUCCESS_COUNT >= 10 :
-                SUCCESS_COUNT = 0
-            elif reward == 1:            
-                SUCCESS_COUNT += 1 
-
-            if reward == 0 and IGNORE_COUNT >0 :
-                IGNORE_COUNT -= 1
-                continue
-
-            if reward != 0 and IGNORE_COUNT == 0:
-                IGNORE_COUNT = 140
-
-            if platform.system()!="Linux":
-                for event in pygame.event.get():  # Linux不需要事件循环，其余需要否则白屏
-                    if event.type == QUIT:
-                        pygame.quit()
-                        sys.exit()   
-
-            # 获得游戏截图
-            image = cv2.cvtColor(cv2.resize(image, (100, 80)), cv2.COLOR_BGR2GRAY)
-            ret, image = cv2.threshold(image, 1, 255, cv2.THRESH_BINARY)
-            image = np.reshape(image, (80, 100, 1))
-            # 保存游戏截图到 input_image_data 的第一张，并将结果返回给 input_image_data1
-            input_image_data1 = np.append(image, input_image_data[:, :, 0:3], axis = 2)
- 
-            # 添加到list中
-            # input_image_data ：移动前的4张图片，本次移动方向，本次移动的得分，移动后的1张图片和前3张
-            D.append((input_image_data, argmax_t, reward, input_image_data1))
- 
-            # 如果list太长，删除最早的
-            D_size = len(D)
-            if D_size > REPLAY_MEMORY:
-                D.popleft()
-
-            # 计分成功后的100张图片全部不用计算
-            if D_size >= BATCH:
-                # 从列表中抓出一批照片
-                minibatch = random.sample(D, BATCH)
-                input_image_data_batch = [d[0] for d in minibatch] # 移动前的4张
-                argmax_batch = [d[1] for d in minibatch]
-                reward_batch = [d[2] for d in minibatch]
-                input_image_data1_batch = [d[3] for d in minibatch] # 移动后的1张+前3张               
-                # out_batch = predict_action.eval(feed_dict = {input_image : input_image_data1_batch}) # 得到一下步的预测
-
-                # 对结果进行评价
-                gt_batch = []
-                 # 最后一次的评价 + 0.99 * 最可能的概率 范围： -1 ~ 1.99 按照下一次的概率给评价加分
-                for i in range(0, len(minibatch)):
-                    #gt_batch.append(reward_batch[i] + 0.99 * np.max(out_batch[i]))
-                    gt_batch.append(reward_batch[i])
- 
-                # optimizer.run(feed_dict = {gt : gt_batch, argmax : argmax_batch, input_image : input_image_data_batch})
-                # 将评价的结果重新输入到系统进行学习                         结果评价        移动的方向               移动前的照片
-
-                _step,_train_summary_op,_,_cost =sess.run([global_step,train_summary_op,optimizer,cost],feed_dict = {gt : gt_batch, argmax : argmax_batch, input_image : input_image_data_batch})
-
-                if _step % 10 == 0:                
-                    saver.save(sess, saver_prefix, global_step=_step)  # 保存模型
-                    # 获得下一次的预测步骤
-                    train_summary_writer.add_summary(_train_summary_op, _step)
-                    out_batch = predict_action.eval(feed_dict = {input_image : input_image_data1_batch}) # 得到一下步的预测
-                    next_action = np.bincount(np.argmax(out_batch,axis=1))
-                    print(_step,"action:", argmax_t, "reward:", reward, "success_count:", SUCCESS_COUNT,"cost:",_cost,"next_action:",next_action)
-            else:
-                print(D_size)
-            input_image_data = input_image_data1
-
-        coord.request_stop()
-        coord.join(threads)
-        train_summary_writer.close()
-
-train_neural_network(input_image)
+if __name__ == '__main__':
+    train()
