278f95d1ec624437de360fdc111a4bac4901439f
王翩 <wp@bsw.com.cn>
2017-03-30 11:14:38 +0800

    .

--- 5/3/1.py
+++ 5/3/1.py
@@ -479,7 +479,7 @@ class Tetromino(object):
 # 参数设置
 DEBUG = True    # 是否开启调试 到程序目录执行 tensorboard --logdir=game_model ，访问 http://127.0.0.1:6006
 ACTIONS_COUNT = 3  # 可选的动作，针对 左移 翻转 右移
-FUTURE_REWARD_DISCOUNT = 0.99  # 下一次奖励的衰变率
+FUTURE_REWARD_DISCOUNT = 0.05  # 下一次奖励的衰变率 0.99
 OBSERVATION_STEPS = 5000.  # 在学习前观察的次数
 EXPLORE_STEPS = 500000.  # 每次机器自动参与的概率的除数
 INITIAL_RANDOM_ACTION_PROB = 1.0  # 随机移动的最大概率
