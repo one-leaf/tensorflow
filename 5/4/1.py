c9dd5a66e93a2ce8c997e63e7805346f00a33bfe
oneleaf <oneleaf@gmail.com>
2017-04-08 11:28:26 +0800

    .

--- 5/4/1.py
+++ 5/4/1.py
@@ -148,7 +148,66 @@ def train_neural_network():
                 print(epoch, train_loss)
                 saver.save(sess, checkpoint_path, global_step=epoch)
 
+def to_word(weights):
+    t = np.cumsum(weights)
+    s = np.sum(weights)
+    sample = int(np.searchsorted(t, np.random.rand(1)*s))
+    return words.keys()[sample]
+
+def gen_poetry():
+
+	_, last_state, probs, cell, initial_state = neural_network()
+ 
+	with tf.Session() as sess:
+		sess.run(tf.global_variables_initializer())
+ 
+        saver,checkpoint_path = restore(sess)
+		state_ = sess.run(cell.zero_state(1, tf.float32))
+ 
+		x = np.array([list(map(words.get, '['))])
+		[probs_, state_] = sess.run([probs, last_state], feed_dict={input_data: x, initial_state: state_})
+		word = to_word(probs_)
+		#word = words[np.argmax(probs_)]
+		poem = ''
+		while word != ']':
+			poem += word
+			x = np.zeros((1,1))
+			x[0,0] = words[word]
+			[probs_, state_] = sess.run([probs, last_state], feed_dict={input_data: x, initial_state: state_})
+			word = to_word(probs_)
+			#word = words[np.argmax(probs_)]
+		return poem
+ 
+def gen_poetry_with_head(head):
+ 
+	_, last_state, probs, cell, initial_state = neural_network()
+ 
+	with tf.Session() as sess:
+		sess.run(tf.global_variables_initializer())
+ 
+        saver,checkpoint_path = restore(sess)
+ 
+		state_ = sess.run(cell.zero_state(1, tf.float32))
+		poem = ''
+		i = 0
+		for word in head:
+			while word != '，' and word != '。':
+				poem += word
+				x = np.array([list(map(words.get, word))])
+				[probs_, state_] = sess.run([probs, last_state], feed_dict={input_data: x, initial_state: state_})
+				word = to_word(probs_)
+				time.sleep(1)
+			if i % 2 == 0:
+				poem += '，'
+			else:
+				poem += '。'
+			i += 1
+		return poem
+ 
+
 if __name__ == '__main__':
     train_neural_network()
+    # print(gen_poetry())
+    # print(gen_poetry_with_head('一二三四')) 
     
     
