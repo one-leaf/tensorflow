0dd9790b0933a816e852453bef04e9621f22a27e
王翩 <wp@bsw.com.cn>
2017-08-02 15:54:05 +0800

    .

--- 5/3/8.py
+++ 5/3/8.py
@@ -555,7 +555,7 @@ def restore(sess):
     return saver, model_dir, saver_prefix
 
 # CNN网络
-def get_network(x, output_size, filter_size=[3,3,3,3,3], strides_size=[1,1,1,1,1], filter_nums=[64,64,64,64,64], pool_scale=[-1,-1,-1,-1,-1], pool_type=[1,1,1,1,1],full_nums=384, output_name="output_layer"):
+def get_network(x, output_size, filter_size=[3,3,3,3,3], strides_size=[1,1,1,1,1], filter_nums=[32,64,128,256,512], pool_scale=[-1,-1,-1,-1,-1], pool_type=[1,1,1,1,1],full_nums=384, output_name="output_layer"):
     def get_w_b(w_shape,w_name="w",b_name="b"):
         w = tf.get_variable(w_name, w_shape, initializer=tf.contrib.layers.xavier_initializer())
         b = tf.get_variable(b_name, [w_shape[-1]], initializer=tf.constant_initializer(0.01))
