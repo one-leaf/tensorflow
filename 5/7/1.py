626323d6f2fb37732b523b264c9405da180cbc5c
王翩 <wp@bsw.com.cn>
2017-09-01 10:18:57 +0800

    .

--- 5/7/1.py
+++ 5/7/1.py
@@ -182,13 +182,11 @@ def train():
 
     def do_report():
         test_inputs,test_labels,test_seq_len = get_next_batch(10)
-        # print(test_inputs)
-        # print(test_labels)
-        # print(test_seq_len)
         test_feed = {inputs: test_inputs,
                      labels: test_labels,
                      seq_len: test_seq_len}
         dd, log_probs, accuracy = session.run([decoded[0], log_prob, acc], test_feed)
+        print("acc",accuracy)
         report_accuracy(dd, test_labels)
  
     def do_batch():
