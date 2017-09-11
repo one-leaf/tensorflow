0fe6cadb967eea25a7e291c4d1b9ae26b5cd2cd8
王翩 <wp@bsw.com.cn>
2017-09-11 16:47:49 +0800

    .

--- 5/7/test.py
+++ 5/7/test.py
@@ -1,29 +1,35 @@
-import pygame, sys
-from pygame.locals import *
- 
-pygame.init()
- 
-pygame.display.set_caption('font example')
-size = [640, 480]
-screen = pygame.display.set_mode(size)
- 
-clock = pygame.time.Clock()
- 
-basicfont = pygame.font.SysFont('simsun', 9)
-text = basicfont.render('60片', False, (255, 0, 0), (255, 255, 255),size=12)
-textrect = text.get_rect()
-textrect.centerx = screen.get_rect().centerx
-textrect.centery = screen.get_rect().centery
- 
-screen.fill((255, 255, 255))
-screen.blit(text, textrect)
- 
-pygame.display.update()
- 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
- 
-    clock.tick(20)
+import os
+import tensorflow as tf
+
+
+def init():
+    curr_dir = os.path.dirname(__file__)
+    model_dir = os.path.join(curr_dir, "model")
+
+    checkpoint_prefix = os.path.join(model_dir, "model.ckpt")
+
+    # 找到最新的运算模型文件
+    metaFile= sorted(
+        [
+            (x, os.path.getctime(os.path.join(model_dir,x)))                  
+            for x in os.listdir(model_dir) if x.endswith('.meta')  
+        ],
+        key=lambda i: i[1])[-1][0]
+
+    sess = tf.Session()
+
+    saver = tf.train.import_meta_graph(os.path.join(model_dir,metaFile))
+    ckpt = tf.train.get_checkpoint_state(model_dir)
+    if ckpt and ckpt.model_checkpoint_path:
+        saver.restore(sess, ckpt.model_checkpoint_path)
+    else:
+        raise Exception("error: can't load checkpoint data")
+    # for tensor in tf.get_default_graph().as_graph_def().node:
+    #     print(tensor.name,tensor.attr['shape'])
+    inputs = tf.get_default_graph().get_tensor_by_name('inputs:0')
+    decoded = tf.get_default_graph().get_tensor_by_name('decoded:0')
+    return sess,inputs,decoded
+
+sess,inputs,decoded = init()
+
+
