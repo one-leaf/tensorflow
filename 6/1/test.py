7f74571bf0550c2e76ffdc8b92f82c794d6881b2
王翩 <wp@bsw.com.cn>
2017-09-25 17:31:02 +0800

    .

--- 6/1/test.py
+++ 6/1/test.py
@@ -5,6 +5,7 @@ import ocr
 import utils
 import cv2
 import random
+import time
 
 curr_dir = os.path.dirname(__file__)
 
@@ -33,27 +34,27 @@ def scan():
         # if idx != 1: continue
         ocr_texts=[]
 
-        ocr_inputs = np.zeros([len(images_group), ocr.image_size[1], ocr.image_size[0]])
-        for i, image in enumerate(images_group):
-            # print(i,image.shape)
-            # for _ in range(4):
-            #     image = np.insert(image, 0, values=0, axis=0)
-            # for _ in range(4):
-            #     image = np.insert(image, 0, values=0, axis=1)
-            image = utils.dropZeroEdges(image)
-            # utils.show(utils.dropZeroEdges(image))
+        # 取最大宽度做为本组的统一输入长度
+        widths=[image.shape[1] for image in images_group]
+        maxImageWidth = max(widths)+5
 
+        ocr_inputs = np.zeros([len(images_group), maxImageWidth, ocr.image_size[0]])
+        for i, image in enumerate(images_group):
+            # image = utils.dropZeroEdges(image)
+            # utils.show(utils.dropZeroEdges(image))
             utils.save(image,os.path.join(curr_dir,"test","%s-%s.png"%(idx,i)))
-            image_vec = utils.img2vec(image,ocr.image_size[0],ocr.image_size[1])
-            ocr_inputs[i,:] = np.transpose(image_vec.reshape((ocr.image_size[0],ocr.image_size[1])))         
+            image_vec = utils.img2vec(image,ocr.image_size[0],maxImageWidth)
+            ocr_inputs[i,:] = np.transpose(image_vec.reshape((ocr.image_size[0],maxImageWidth)))         
             # utils.show(image)
             # return
-        ocr_seq_len = np.ones(ocr_inputs.shape[0]) * ocr.image_size[1]
+        ocr_seq_len = np.ones(ocr_inputs.shape[0]) * maxImageWidth
 
         feed = {inputs: ocr_inputs, seq_len: ocr_seq_len,  input_keep_prob: 1.0}
         print("starting ocr inputs %s ..." % idx)
-        decoded_list,_  = session.run([decoded[0],log_prob], feed)
-        print("filished ocr inputs %s ..." % idx)
+        start = time.time()
+        decoded_list = session.run(decoded[0], feed)
+        seconds = round(time.time() - start,2)
+        print("filished ocr inputs %s, paid %s seconds" % (idx,seconds))
         detected_list = ocr.decode_sparse_tensor(decoded_list)
         for detect_number in detected_list:
             ocr_texts.append(ocr.list_to_chars(detect_number))
