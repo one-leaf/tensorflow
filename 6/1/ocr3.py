f7eeecc200c8bfe6b7458b2914b0ad159c11f860
王翩 <wp@bsw.com.cn>
2017-10-11 17:28:57 +0800

    .

--- 6/1/ocr3.py
+++ 6/1/ocr3.py
@@ -94,11 +94,11 @@ def neural_networks():
 
     # Reshaping to apply the same weights over the timesteps
     outputs1, _ = tf.nn.dynamic_rnn(stack, inputs, seq_len, dtype=tf.float32)
-    outputs1 = tf.reshape(outputs, [-1, num_hidden])
+    outputs1 = tf.reshape(outputs1, [-1, num_hidden])
 
     inputs_reverse = tf.reverse(inputs,axis=[1])
     outputs2, _ = tf.nn.dynamic_rnn(stack, inputs_reverse, seq_len, dtype=tf.float32)
-    outputs2 = tf.reshape(outputs, [-1, num_hidden])
+    outputs2 = tf.reshape(outputs2, [-1, num_hidden])
 
     outputs = outputs1 + outputs2
     
