ef1ef24b10ef066ed7f2d5714db6e0dbc022b703
王翩 <wp@bsw.com.cn>
2017-10-11 18:24:01 +0800

    。

--- 6/1/ocr3.py
+++ 6/1/ocr3.py
@@ -93,23 +93,20 @@ def neural_networks():
     stack = tf.contrib.rnn.MultiRNNCell(cells, state_is_tuple=True)
 
     # Reshaping to apply the same weights over the timesteps
-    outputs1, _ = tf.nn.dynamic_rnn(stack, inputs, seq_len, dtype=tf.float32)
-    outputs1 = tf.reshape(outputs1, [-1, num_hidden])
+    with tf.device('/gpu:0'):
+        outputs1, _ = tf.nn.dynamic_rnn(stack, inputs, seq_len, dtype=tf.float32)
+        outputs1 = tf.reshape(outputs1, [-1, num_hidden])
+        W1 = tf.Variable(tf.truncated_normal([num_hidden, num_classes], stddev=0.1))
+        b1 = tf.Variable(tf.constant(0., shape=[num_classes]))
+        logits1 = tf.matmul(outputs1, W1) + b1
 
-    inputs_reverse = tf.reverse(inputs,axis=[1])
-    outputs2, _ = tf.nn.dynamic_rnn(stack, inputs_reverse, seq_len, dtype=tf.float32)
-    outputs2 = tf.reshape(outputs2, [-1, num_hidden])
-
-    # outputs = outputs1 + outputs2
-    # outputs = tf.reshape(outputs, [-1, num_hidden])
-
-    W1 = tf.Variable(tf.truncated_normal([num_hidden, num_classes], stddev=0.1))
-    b1 = tf.Variable(tf.constant(0., shape=[num_classes]))
-    logits1 = tf.matmul(outputs1, W1) + b1
-
-    W2 = tf.Variable(tf.truncated_normal([num_hidden, num_classes], stddev=0.1))
-    b2 = tf.Variable(tf.constant(0., shape=[num_classes]))
-    logits2 = tf.matmul(outputs2, W2) + b2
+    with tf.device('/gpu:1'):
+        inputs_reverse = tf.reverse(inputs,axis=[1])
+        outputs2, _ = tf.nn.dynamic_rnn(stack, inputs_reverse, seq_len, dtype=tf.float32)
+        outputs2 = tf.reshape(outputs2, [-1, num_hidden])
+        W2 = tf.Variable(tf.truncated_normal([num_hidden, num_classes], stddev=0.1))
+        b2 = tf.Variable(tf.constant(0., shape=[num_classes]))
+        logits2 = tf.matmul(outputs2, W2) + b2
 
     logits = logits1 + logits2
 
