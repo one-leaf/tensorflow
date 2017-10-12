407800b0326e6748e56b08031a5b769019d0a0e6
王翩 <wp@bsw.com.cn>
2017-10-12 09:31:45 +0800

    .

--- 6/1/ocr3.py
+++ 6/1/ocr3.py
@@ -92,10 +92,18 @@ def neural_networks():
         cells.append(cell)
     stack = tf.contrib.rnn.MultiRNNCell(cells, state_is_tuple=True)
 
+    cells2 = []
+    for _ in range(num_layers):
+        cell = tf.contrib.rnn.LSTMCell(num_hidden, state_is_tuple=True, activation=tf.nn.relu)
+        # 随机抛弃
+        cell = tf.contrib.rnn.DropoutWrapper(cell, input_keep_prob=input_keep_prob)
+        cells2.append(cell)
+    stack2 = tf.contrib.rnn.MultiRNNCell(cells2, state_is_tuple=True)
+
     # Reshaping to apply the same weights over the timesteps
     outputs1, _ = tf.nn.dynamic_rnn(stack, inputs, seq_len, dtype=tf.float32)
     inputs_reverse = tf.reverse(inputs, axis=[1])
-    outputs2, _ = tf.nn.dynamic_rnn(stack, inputs_reverse, seq_len, dtype=tf.float32)
+    outputs2, _ = tf.nn.dynamic_rnn(stack2, inputs_reverse, seq_len, dtype=tf.float32)
     outputs = outputs1 + outputs2
     outputs = tf.reshape(outputs, [-1, num_hidden])
     W = tf.Variable(tf.truncated_normal([num_hidden, num_classes], stddev=0.1))
