90d107cefc8364c07f289893251b2ba599987ca8
oneleaf <oneleaf@gmail.com>
2017-10-15 22:13:36 +0800

    .

--- 6/1/ocr3.py
+++ 6/1/ocr3.py
@@ -107,12 +107,12 @@ def neural_networks():
     # logits = tf.add(tf.matmul(outputs_fw, W_fw),tf.matmul(outputs_bw, W_bw)) + b
 
     # 第三种双向LSTM方法
-    # cell_fw = tf.contrib.rnn.LSTMCell(num_hidden/2, state_is_tuple=True)
-    # cell_bw = tf.contrib.rnn.LSTMCell(num_hidden/2, state_is_tuple=True)
+    # cell_fw = tf.contrib.rnn.LSTMCell(num_hidden, state_is_tuple=True)
+    # cell_bw = tf.contrib.rnn.LSTMCell(num_hidden, state_is_tuple=True)
     # outputs, _ = tf.nn.bidirectional_dynamic_rnn(cell_fw, cell_bw, inputs, seq_len, dtype=tf.float32)
     # outputs = tf.concat(outputs, axis=2)
-    # outputs = tf.reshape(outputs, [-1, num_hidden])
-    # W = tf.Variable(tf.truncated_normal([num_hidden, num_classes], stddev=0.1))
+    # outputs = tf.reshape(outputs, [-1, num_hidden*2])
+    # W = tf.Variable(tf.truncated_normal([num_hidden*2, num_classes], stddev=0.1))
     # b = tf.Variable(tf.constant(0.1, shape=[num_classes]))
     # logits = tf.matmul(outputs, W) + b   
     
