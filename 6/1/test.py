dbeb6c471fac3c87c8d40c3257a5a7073e2c9b92
王翩 <wp@bsw.com.cn>
2017-10-30 10:10:02 +0800

    .

--- 6/1/test.py
+++ 6/1/test.py
@@ -1,24 +1,35 @@
 import os
 import tensorflow as tf
 import numpy as np
-import ocr
 import utils
 import cv2
 import random
 import time
+import ocr3
 
 curr_dir = os.path.dirname(__file__)
 
 def init():
-    logits, inputs, labels, seq_len, W, b, input_keep_prob = ocr.neural_networks()
-    decoded, log_prob = tf.nn.ctc_beam_search_decoder(logits, seq_len, merge_repeated=False)
+#    logits, inputs, labels, seq_len, input_keep_prob = ocr3.neural_networks()
+    global_step = tf.Variable(0, trainable=False)
+    curr_learning_rate = 1e-5
+    learning_rate = tf.placeholder(tf.float32, shape=[])                                            
+    logits, inputs, labels, seq_len, input_keep_prob = ocr3.neural_networks()
+    loss = tf.nn.ctc_loss(labels=labels,inputs=logits, sequence_length=seq_len)
+    cost = tf.reduce_mean(loss, name="cost")
+    grads_optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+    grads_and_vars = grads_optimizer.compute_gradients(loss)
+    capped_grads_and_vars = [(tf.clip_by_value(grad, -1., 1.), var) for grad, var in grads_and_vars]
+    optimizer = grads_optimizer.apply_gradients(capped_grads_and_vars, global_step=global_step)
+    decoded, log_prob = tf.nn.ctc_beam_search_decoder(logits, seq_len, beam_width=10, merge_repeated=False)
+    
     sess = tf.Session()
     sess.run(tf.global_variables_initializer())
     # restore sess
     model_dir = os.path.join(curr_dir, "model")
     saver_prefix = os.path.join(model_dir, "model.ckpt")        
     ckpt = tf.train.get_checkpoint_state(model_dir)
-    saver = tf.train.Saver(max_to_keep=5)
+    saver = tf.train.Saver()
     if ckpt and ckpt.model_checkpoint_path:
         print("Restore Model ...")
         saver.restore(sess, ckpt.model_checkpoint_path)    
@@ -38,13 +49,13 @@ def scan():
         widths=[image.shape[1] for image in images_group]
         maxImageWidth = max(widths)+5
 
-        ocr_inputs = np.zeros([len(images_group), maxImageWidth, ocr.image_size[0]])
+        ocr_inputs = np.zeros([len(images_group), maxImageWidth, ocr3.image_size[0]])
         for i, image in enumerate(images_group):
             # image = utils.dropZeroEdges(image)
             # utils.show(utils.dropZeroEdges(image))
             utils.save(image,os.path.join(curr_dir,"test","%s-%s.png"%(idx,i)))
-            image_vec = utils.img2vec(image,ocr.image_size[0],maxImageWidth)
-            ocr_inputs[i,:] = np.transpose(image_vec.reshape((ocr.image_size[0],maxImageWidth)))         
+            image_vec = utils.img2vec(image,ocr3.image_size[0],maxImageWidth)
+            ocr_inputs[i,:] = np.transpose(image_vec.reshape((ocr3.image_size[0],maxImageWidth)))         
             # utils.show(image)
             # return
         ocr_seq_len = np.ones(ocr_inputs.shape[0]) * maxImageWidth
@@ -55,9 +66,9 @@ def scan():
         decoded_list = session.run(decoded[0], feed)
         seconds = round(time.time() - start,2)
         print("filished ocr inputs %s, paid %s seconds" % (idx,seconds))
-        detected_list = ocr.decode_sparse_tensor(decoded_list)
+        detected_list = ocr3.decode_sparse_tensor(decoded_list)
         for detect_number in detected_list:
-            ocr_texts.append(ocr.list_to_chars(detect_number))
+            ocr_texts.append(ocr3.list_to_chars(detect_number))
 
         ocr_text_groups.append(ocr_texts)
         # break   
@@ -65,18 +76,20 @@ def scan():
 
 def scan2():
     session, inputs, seq_len, input_keep_prob, decoded, log_prob = init()
-    need_ocr_images = utils.loadImage(os.path.join(curr_dir,'test','1.jpg'),0)
+    need_ocr_images = utils.loadImage(os.path.join(curr_dir,'test','0.jpg'),0)
     ocr_text_groups = []
     for idx,images_group in enumerate(need_ocr_images):
         # if idx != 1: continue
         ocr_texts=[]
 
         for i, image in enumerate(images_group):
-            maxImageWidth = image.shape[1]+5
-            ocr_inputs = np.zeros([1, maxImageWidth, ocr.image_size[0]])
+            image = utils.dropZeroEdges(image)  
+            image = utils.resize(image,ocr3.image_height)
             utils.save(image,os.path.join(curr_dir,"test","%s-%s.png"%(idx,i)))
-            image_vec = utils.img2vec(image,ocr.image_size[0],maxImageWidth)
-            ocr_inputs[0,:] = np.transpose(image_vec.reshape((ocr.image_size[0],maxImageWidth)))         
+            maxImageWidth = image.shape[1]+5
+            image_vec = utils.img2vec(image,ocr3.image_height,maxImageWidth)
+            ocr_inputs = np.zeros([1, maxImageWidth, ocr3.image_height])
+            ocr_inputs[0,:] = np.transpose(image_vec.reshape((ocr3.image_height,maxImageWidth)))         
             ocr_seq_len = np.ones(ocr_inputs.shape[0]) * maxImageWidth
             feed = {inputs: ocr_inputs, seq_len: ocr_seq_len,  input_keep_prob: 1.0}
             print("starting ocr inputs %s:%s ..." % (idx,i))
@@ -84,9 +97,9 @@ def scan2():
             decoded_list = session.run(decoded[0], feed)
             seconds = round(time.time() - start,2)
             print("filished ocr inputs %s, paid %s seconds" % (idx,seconds))
-            detected_list = ocr.decode_sparse_tensor(decoded_list)            
+            detected_list = ocr3.decode_sparse_tensor(decoded_list)            
             for detect_number in detected_list:
-                ocr_texts.append(ocr.list_to_chars(detect_number))
+                ocr_texts.append(ocr3.list_to_chars(detect_number))
 
         ocr_text_groups.append(ocr_texts)
         # break   
