23a8b7467dda492b85ed48267e783538f33ca6fd
王翩 <wp@bsw.com.cn>
2017-11-01 11:37:59 +0800

    .

--- Linear Regression/3.py
+++ Linear Regression/3.py
@@ -15,12 +15,11 @@ def getBatch(batchSize,start=None):
     y = np.cos(n)
     return x,y,n
 
-# 增加层 w1 * x * x + w2 * x + b
+# 增加层
 def add_layer(inputs, in_size, out_size, activation_function=None):
-    Weights1 = tf.Variable(tf.random_normal([in_size, out_size]))
-    Weights2 = tf.Variable(tf.random_normal([in_size, out_size]))
+    Weights = tf.Variable(tf.random_normal([in_size, out_size]))
     biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
-    Wx_plus_b = tf.matmul(inputs * inputs, Weights1) + tf.matmul(inputs, Weights2) + biases
+    Wx_plus_b = tf.matmul(inputs, Weights) + biases
     if activation_function is None:
         outputs = Wx_plus_b
     else:
@@ -30,9 +29,17 @@ def add_layer(inputs, in_size, out_size, activation_function=None):
 # 神经网络定义
 def neural_networks():
     x = tf.placeholder(tf.float32, [None, 1], name='x')
-    y = tf.placeholder(tf.float32, [None, 1], name='y')   
-    layer = add_layer(x, 1, 200, tf.nn.relu)
-    prediction = add_layer(layer, 200, 1)
+    y = tf.placeholder(tf.float32, [None, 1], name='y')
+
+    x_list=[]
+    for i in range(1,10):
+        x_list.append(tf.sin(x*i))
+        x_list.append(tf.cos(x*i))
+        x_list.append(tf.pow(x,i))
+    _x=tf.concat(x_list,axis=1)
+
+    layer = add_layer(_x, len(x_list), 128, tf.nn.relu)
+    prediction = add_layer(layer, 128, 1)
     cost = tf.reduce_sum(tf.square(y - prediction))
     optimizer = tf.train.AdamOptimizer(0.001).minimize(cost)
     return x, y, prediction, optimizer, cost
@@ -47,7 +54,7 @@ if __name__ == '__main__':
     for i in range(100000):
         batch_x, batch_y, batch_n= getBatch(200,0)
         _, loss, pred = sess.run([optimizer, cost, prediction], feed_dict={x: batch_x, y: batch_y})
-        if i % 50 == 0 :
+        if i % 50 == 0:
             print(i, loss)
             plt.clf()
             plt.plot(batch_n, batch_y, 'r', batch_n, pred, 'b')
