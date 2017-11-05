bd727629e26bbb2a5424eac6d5967a4b59949dc4
oneleaf <oneleaf@gmail.com>
2017-11-05 16:10:09 +0800

    .

--- Logistic Regression/6.1.py
+++ Logistic Regression/6.1.py
@@ -60,18 +60,18 @@ def neural_networks():
     # input_size = 28
     # x_image = tf.reshape(x_image, [-1, input_size])
     # x_image_shape  = tf.shape(x_image)
-    layer = add_layer(x, 28*28, 28*28, activation_function=tf.nn.relu, norm=True)
+    layer = add_layer(x, 28*28, 512, activation_function=tf.nn.relu, norm=True)
     layer = tf.nn.dropout(layer, keep_prob)
 
-    # layer = add_layer(layer, 256, 512 , activation_function=tf.nn.relu)
-    # layer = tf.minimum(layer, 20.0)    
-    # layer = tf.nn.dropout(layer, keep_prob)
+    layer = add_layer(layer, 512, 512 , activation_function=tf.nn.relu, norm=True)
+    layer = tf.minimum(layer, 20.0)    
+    layer = tf.nn.dropout(layer, keep_prob)
 
-    # layer = add_layer(layer, 512, 28*28 , activation_function=tf.nn.relu)
-    # layer = tf.minimum(layer, 20.0)    
-    # layer = tf.nn.dropout(layer, keep_prob)  # [time_step, 2800]
+    layer = add_layer(layer, 512, 1024 , activation_function=tf.nn.relu, norm=True)
+    layer = tf.minimum(layer, 20.0)    
+    layer = tf.nn.dropout(layer, keep_prob)  # [time_step, 2800]
 
-    x_image = tf.reshape(layer, [-1, 28, 28]) #[-1, time_step , input_size]
+    x_image = tf.reshape(layer, [-1, 1024, 1]) #[-1, time_step , input_size]
 
     # x_image = tf.transpose(x_image, (1, 0, 2))
     num_units = 64
@@ -84,9 +84,9 @@ def neural_networks():
     logits = tf.concat(outputs, axis=2)
     logits = tf.transpose(logits, (0, 2, 1)) 
     # [batch_size, time_step, num_units] = > [batch_size, num_units, time_step] 不转也能学的
-    logits = tf.reshape(logits,[-1, 28 * num_units])
+    logits = tf.reshape(logits,[-1, 1024 * num_units])
 
-    layer = add_layer(logits, 28 * num_units, 512, activation_function=tf.nn.relu)
+    layer = add_layer(logits, 1024 * num_units, 512, activation_function=tf.nn.relu)
     layer = tf.minimum(layer, 20.0)    
     layer = tf.nn.dropout(layer, keep_prob)
 
