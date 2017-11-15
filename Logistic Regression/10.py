d95e7496fcbabd7c7dfca63f594d9d789b16e95d
王翩 <wp@bsw.com.cn>
2017-11-15 15:38:04 +0800

    .

--- Logistic Regression/10.py
+++ Logistic Regression/10.py
@@ -87,4 +87,4 @@ if __name__ == '__main__':
 
     acc = sess.run(accuracy, feed_dict={x: test_x, y: test_y})
     print("Last accuracy:",acc)
-    # Last accuracy: 0.9925
+    # Last accuracy: 0.9782
