732cc26fecb726c531df33bee26d9b40bef3d283
王翩 <wp@bsw.com.cn>
2017-11-17 13:22:58 +0800

    .

--- 6/1/ocr_ascii.py
+++ 6/1/ocr_ascii.py
@@ -110,7 +110,7 @@ def get_next_batch(batch_size=128):
     codes = []
     images = []   
     max_width_image = 0
-    font_min_length = random.randint(10, 50)
+    font_min_length = random.randint(10, 30)
     for i in range(batch_size):
         font_name = random.choice(FontNames)
         font_length = random.randint(font_min_length-5, font_min_length+5)
