a2a3f54f02521555cf660220e8b8aad434893bd9
王翩 <wp@bsw.com.cn>
2017-11-17 11:07:46 +0800

    .

--- 6/1/web_ascii.py
+++ 6/1/web_ascii.py
@@ -60,10 +60,11 @@ def scan(file):
 
     for i, split_image in enumerate(split_images):
         # image = utils.img2bwinv(split_image)
-        image = 1 - split_image/255.
+        image = 255. - split_image
         image = utils.dropZeroEdges(image)  
         image = utils.resize(image, ocr.image_height)
         utils.save(image,os.path.join(curr_dir,"test","%s.png"%i))
+        image = image /255.
         maxImageWidth = image.shape[1]
         maxImageWidth = maxImageWidth + (4 - maxImageWidth % 4)
         image_vec = utils.img2vec(image,ocr.image_height,maxImageWidth)
