69a52ea629fc3ff8425b3618aa6ae91a743e64ee
oneleaf <oneleaf@gmail.com>
2017-11-18 23:13:01 +0800

    .

--- 6/1/ocr_ascii.py
+++ 6/1/ocr_ascii.py
@@ -40,7 +40,7 @@ REPORT_STEPS = 500
 MOMENTUM = 0.9
 
 BATCHES = 64
-BATCH_SIZE = 20
+BATCH_SIZE = 10
 TRAIN_SIZE = BATCHES * BATCH_SIZE
 TEST_BATCH_SIZE = BATCH_SIZE
 POOL_COUNT = 3
@@ -112,7 +112,7 @@ def get_next_batch(batch_size=128):
     codes = []
     images = []   
     max_width_image = 0
-    font_min_length = random.randint(10, 17)
+    font_min_length = random.randint(40, 50)
     for i in range(batch_size):
         font_name = random.choice(FontNames)
         font_length = random.randint(font_min_length-3, font_min_length+3)
