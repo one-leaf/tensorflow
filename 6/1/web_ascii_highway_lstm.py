af5794967bf1ea18c54a2f2deb510e754045e56c
王翩 <wp@bsw.com.cn>
2017-11-23 13:53:38 +0800

    .

--- 6/1/web_ascii_highway_lstm.py
+++ 6/1/web_ascii_highway_lstm.py
@@ -60,7 +60,7 @@ def scan(file):
 
     for i, split_image in enumerate(split_images):
         # image = utils.img2bwinv(split_image)
-        image = utils.clearLineImg(image)    
+        image = utils.clearLineImg(split_image)    
         image = 255. - split_image
         image = utils.dropZeroEdges(image)  
         image = utils.resize(image, ocr.image_height)
