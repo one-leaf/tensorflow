1d676ef433df7817e08c757afef12681c4ecae86
王翩 <wp@bsw.com.cn>
2017-11-23 19:04:46 +0800

    .

--- 6/1/ocr_ascii_highway_lstm.py
+++ 6/1/ocr_ascii_highway_lstm.py
@@ -127,7 +127,7 @@ def get_next_batch(batch_size=128):
     codes = []
     images = []   
     max_width_image = 0
-    font_min_length = random.randint(10, 60)
+    font_min_length = random.randint(10, 80)
     for i in range(batch_size):
         font_name = random.choice(FontNames)
         font_length = random.randint(font_min_length-5, font_min_length+5)
