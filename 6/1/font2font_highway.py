3975561271c16d7e43a876d4034725ec204b2551
oneleaf <oneleaf@gmail.com>
2017-11-24 21:13:15 +0800

    .

--- 6/1/font2font_highway.py
+++ 6/1/font2font_highway.py
@@ -99,7 +99,7 @@ def get_next_batch(batch_size=128):
     images = []   
     to_images = []
     max_width_image = 0
-    font_min_length = random.randint(50, 60)
+    font_min_length = random.randint(10, 20)
     for i in range(batch_size):
         font_name = random.choice(FontNames)
         font_length = random.randint(font_min_length-5, font_min_length+5)
