3450964ef1ae1a3551ca906b4603aa4f377b021f
王翩 <wp@bsw.com.cn>
2017-11-29 16:20:05 +0800

    .

---
+++ 6/1/fontUtils/Program.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace RenderFontHttpServer
+{
+    public class Request
+    {
+        public string method, url, protocol;
+        public Dictionary<string, string> headers;
+        public Request(StreamReader sr)
+        {
+            string line = sr.ReadLine();
+            string[] p = line.Split(' ');
+            method = p[0];
+            url = p[1];
+            protocol = p[2];
+
+            headers = new Dictionary<string, string>();
+            while (!String.IsNullOrEmpty(line = sr.ReadLine()))
+            {
+              //  Console.WriteLine(line);
+
+                int i = line.IndexOf(":");
+                if (i >= 0)
+                {
+                    headers.Add(line.Substring(0, i), line.Substring(i + 1));
+                }
+                else
+                {
+                    headers.Add(line, "");
+                }
+            }
+
+        }
+    }
+    public class Response
+    {
+        public string status = "200 OK";
+        public class ContentType
+        {
+            public static string text = "text/plain";
+            public static string png = "image/png";
+            public static string json = "application/json";
+        }
+        public string contentType = ContentType.json;
+        public Dictionary<string, string> headers = new Dictionary<string, string>();
+        public byte[] data = new byte[] { };
+    }
+    class Program
+    {
+        private Thread serverThread;
+        TcpListener listener;
+
+        public void start(int port, Func<Request, Response> reqProc)
+        {
+            IPAddress ipAddr = IPAddress.Any;
+            listener = new TcpListener(ipAddr, port);
+            serverThread = new Thread(() =>
+            {
+                listener.Start();
+                while (true)
+                {
+                    Socket s = listener.AcceptSocket();
+                    NetworkStream ns = new NetworkStream(s);
+                    try
+                    {
+                        StreamReader sr = new StreamReader(ns);
+                        //Console.WriteLine("Connect");
+                        Request req = new Request(sr);
+                        //Console.WriteLine("req");
+
+                        Response resp = reqProc(req);
+                        //Console.WriteLine("resp");
+
+                        StreamWriter sw = new StreamWriter(ns);
+                        sw.WriteLine("HTTP/1.0 {0}", resp.status);
+                        sw.WriteLine("Content-Type: {0}", resp.contentType);
+                        foreach (string k in resp.headers.Keys)
+                            sw.WriteLine("{0}: {1}", k, resp.headers[k]);
+                        sw.WriteLine("Content-Length: {0}", resp.data.Length);
+                        sw.WriteLine();
+                        sw.Flush();
+                        s.Send(resp.data);
+                        s.Shutdown(SocketShutdown.Both);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+
+                    }
+                    finally { 
+                        ns.Close();
+                    }
+                }
+            });
+            serverThread.Start();
+        }
+
+        public void stop()
+        {
+            listener.Stop();
+            serverThread.Abort();
+        }
+
+        Response getAllFonts()
+        {
+            Dictionary<String, String[]> fonts = new Dictionary<string, string[]>();
+            fonts.Add("eng", Utils.engFontNames);
+            fonts.Add("chi", Utils.chiFontNames);
+            Response resp = new Response();
+            resp.contentType = Response.ContentType.json;
+            DataContractJsonSerializer json = new DataContractJsonSerializer(fonts.GetType(), 
+                new DataContractJsonSerializerSettings() { UseSimpleDictionaryFormat = true});
+            
+            MemoryStream ms = new MemoryStream();
+            json.WriteObject(ms, fonts);
+            resp.data = ms.ToArray();
+            return resp;
+        }
+
+        Response getImage(string url)
+        {
+            var nvc = HttpUtility.ParseQueryString(url.Substring(url.IndexOf("?")+1));
+            IDictionary<String, String> param = nvc.AllKeys.ToDictionary(k=>k,k=>nvc[k]);
+            String text = param["text"];
+            String fontname = param["fontname"];
+            int fontsize = int.Parse(param["fontsize"]);
+            int fontmode = int.Parse(param["fontmode"]);
+            int fonthint = int.Parse(param["fonthint"]);
+            Bitmap image = Utils.Render(text, fontname, fontsize, fontmode, fonthint);
+            Response resp = new Response();
+            resp.contentType = Response.ContentType.png;
+            MemoryStream ms = new MemoryStream();
+            image.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+            resp.data = ms.ToArray();
+            return resp;
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.start(8888, (req)=>
+            {
+               // Console.WriteLine("{0} {1}",req.method, req.url);
+                if (req.url == "/")
+                {
+                    return p.getAllFonts();
+                }
+                else
+                {
+                    return p.getImage(req.url);
+                }
+            });
+            Console.WriteLine("press any key to exit.");
+            Console.Read();
+            p.stop();
+        }
+    }
+}
---
+++ 6/1/fontUtils/Utils.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Text;
+
+namespace RenderFontHttpServer
+{
+    class Utils
+    {
+      public static String[] engFontNames = new String[]{
+            "Arial","Calibri","Cambria","Candara","Comic Sans MS","Consolas",
+            "Constantia","Corbel","Courier New","Courier","Fixedsys",
+            "Franklin Gothic","Gabriola","Georgia","Impact","Lucida Console",
+            "Lucida Sans Unicode","Microsoft Sans Serif","Palatino linotype",
+            "MS Sans Serif","MS Serif","Nina","Segoe","Segoe UI",
+            "Small Fonts","System","Tahoma","Terminal","Times New Roman",
+            "Trebuchet MS","Verdana"
+        };
+        public static String[] chiFontNames = new String[] {
+            "方正兰亭超细黑简体","方正舒体","方正姚体",
+            "仿宋","黑体","华文仿宋","华文行楷","华文楷体","华文隶书","华文宋体",
+            "华文细黑","华文新魏","华文中宋","楷体","隶书","宋体","微软雅黑",
+            "新宋体","幼圆"
+        };
+
+        public static IEnumerable<String> GetEngFonts()
+        {
+            return engFontNames;
+        }
+        public static IEnumerable<String> GetChiFonts()
+        {
+            return chiFontNames;
+        }
+
+        public static Bitmap Render(string text, string fontname, int fontsize, int fontmode, int renderhint)
+        {
+            Bitmap bmp = new Bitmap(1, 1);
+            Graphics graphics = Graphics.FromImage(bmp);
+            FontStyle style;
+            switch (fontmode)
+            {
+                case 1:
+                    style = FontStyle.Bold;
+                    break;
+                case 2:
+                    style = FontStyle.Italic;
+                    break;
+                case 4:
+                    style = FontStyle.Underline;
+                    break;
+                case 8:
+                    style = FontStyle.Strikeout;
+                    break;
+                default:
+                    style = FontStyle.Regular;
+                    break;
+            }
+            Console.WriteLine("{0} {1} {2} {3}", fontname,fontsize,fontmode,text);
+            Font font = new Font(fontname, fontsize, style);
+            SizeF stringSize = graphics.MeasureString(text, font);
+            bmp = new Bitmap(bmp, (int)stringSize.Width, (int)stringSize.Height);
+            graphics = Graphics.FromImage(bmp);
+            graphics.CompositingQuality = CompositingQuality.HighQuality;
+            graphics.InterpolationMode = InterpolationMode.HighQualityBilinear;
+            graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+            graphics.SmoothingMode = SmoothingMode.HighQuality;
+
+            switch (renderhint)
+            {
+                case 1:
+                    graphics.TextRenderingHint = TextRenderingHint.SingleBitPerPixelGridFit;
+                    break;
+                case 2:
+                    graphics.TextRenderingHint = TextRenderingHint.SingleBitPerPixel;
+                    break;
+                case 3:
+                    graphics.TextRenderingHint = TextRenderingHint.AntiAliasGridFit;
+                    break;
+                case 4:
+                    graphics.TextRenderingHint = TextRenderingHint.AntiAlias;
+                    break;
+                case 5:
+                    graphics.TextRenderingHint = TextRenderingHint.ClearTypeGridFit;
+                    break;
+                default:
+                    graphics.TextRenderingHint = TextRenderingHint.SystemDefault;
+                    break;
+            }
+            graphics.DrawString(text, font, Brushes.Black, 0, 0);
+            font.Dispose();
+            graphics.Flush();
+            graphics.Dispose();
+            return bmp;
+        }
+    }
+}
