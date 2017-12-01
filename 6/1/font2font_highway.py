a7b240f3ce0aac5f7bdfbe0874ed17eb0c501a0e
王翩 <wp@bsw.com.cn>
2017-12-01 17:50:32 +0800

    .

--- 6/1/font2font_highway.py
+++ 6/1/font2font_highway.py
@@ -243,10 +243,13 @@ def train():
                     test_inputs, test_labels = get_next_batch(1)             
                     feed = {inputs: test_inputs, labels: test_labels, keep_prob: 1}
                     b_predictions = session.run([predictions], feed)                     
-                    b_predictions = np.reshape(b_predictions[0],test_labels[0].shape)             
+                    b_predictions = np.reshape(b_predictions[0],test_labels[0].shape)   
+                    _pred = np.transpose(b_predictions)
+                    imin, imax = _pred.min(), _pred.max()
+                    _pred = (_pred - imin)/(imax - imin)          
                     cv2.imwrite(os.path.join(curr_dir,"test","%s_input.png"%steps), np.transpose(test_inputs[0]*255))
                     cv2.imwrite(os.path.join(curr_dir,"test","%s_label.png"%steps), np.transpose(test_labels[0]*255))
-                    cv2.imwrite(os.path.join(curr_dir,"test","%s_pred.png"%steps), np.transpose(b_predictions*255))
+                    cv2.imwrite(os.path.join(curr_dir,"test","%s_pred.png"%steps), _pred*255)
 
 
             saver.save(session, saver_prefix, global_step=steps)
