8bd4c61bf665e1819a3bf190f670671cedb53016
oneleaf <oneleaf@gmail.com>
2017-12-02 00:47:26 +0800

    .

---
+++ 6/1/utils_font.py
@@ -0,0 +1,116 @@
+import urllib,json,io
+import urllib.parse , urllib.request
+import random
+import utils_pil
+from PIL import Image
+
+def http(url,param=None):
+    if param !=None:
+        paramurl = urllib.parse.urlencode(param)
+        url = "%s?%s"%(url,paramurl)
+        r = urllib.request.urlopen(url, timeout=30)
+    else:    
+        r = urllib.request.urlopen(url, timeout=30)
+    return r.read()
+
+# 从字体服务器上获取字体图片
+def get_font_image_from_url(text, font_name, font_size, noise=False, fontmode=None, fonthint=None):
+    params= {}
+    params['text'] = text
+    params['fontname'] = font_name
+    params['fontsize'] = font_size
+    # params['fontmode'] = random.choice([0,1,2,4,8])
+    if fontmode == None:
+        params['fontmode'] = random.choice([0,1,2,4])
+    else:
+        params['fontmode'] = fontmode
+    if fonthint == None:
+        params['fonthint'] = random.choice([0,1,2,3,4,5])
+    else:
+        params['fonthint'] = fonthint  
+    r = http('http://192.168.2.113:8888/',params)
+    img = Image.open(io.BytesIO(r))
+    img = utils_pil.convert_to_rgb(img)
+    img = trim(img)
+    return img
+
+# 获得随机字符串
+def get_random_text(CHARS, word_dict, font_length):
+    text=''
+    n = random.random()
+    if n<0.1:
+        for i in range(font_length):
+            text += random.choice("123456789012345678901234567890-./$,:()+-*=><")
+    elif n<0.5 and n>=0.1:
+        for i in range(font_length):
+            text += random.choice(CHARS)        
+    else:
+        while len(text)<font_length:
+            word = random.choice(word_dict)
+            _word=""
+            for c in word:
+                if c in CHARS:
+                    _word += c
+            text = text+" "+_word.strip()
+    text = text.strip()
+    return text
+
+def get_font_image_from_pygame(font_file, font_size, text):
+    pygame.init()
+    freetype.init()
+    try:
+        font = freetype.Font(font_file, font_size)
+        font.antialiased = random.random()>0.5 
+        styles = [freetype.STYLE_DEFAULT,freetype.STYLE_NORMAL,freetype.STYLE_OBLIQUE,
+                    freetype.STYLE_STRONG,freetype.STYLE_UNDERLINE,freetype.STYLE_WIDE]
+        while True:
+            try:
+                style =random.choice(styles)
+                rtext = font.render(text, (0, 0, 0), (255, 255, 255), style=style)[0]
+                break
+            except:
+                print("rechoise font style for",font_file)           
+        data = pygame.image.tostring(rtext, 'RGBA')
+        _img = Image.frombytes("RGBA",rtext.get_size(),data)
+        size = _img.size
+        img=Image.new("RGBA",(size[0]+100,size[1]+100),(255,255,255))
+        img.paste(_img,(50,50))
+        return img
+    except:
+        raise Exception("Error font %s" % font_file)        
+
+def get_font_image_from_pil(font_file, font_size, text, random_font_mode=False):
+    try:
+        font = ImageFont.truetype(font_file, font_size, index = 0)
+        size = font.getsize(text)
+        img  = Image.new("RGBA",(size[0]+100,size[1]+100),(255,255,255))
+        draw = ImageDraw.Draw(img)
+        if random_font_mode:
+            fontmode = random.choice(["1", "P", "I", "F", "L"])
+            draw.fontmode=fontmode
+        draw.text((50,50),text,fill='black',font=font)
+        return img
+    except:t
+        raise Exception("Error font %s" % font_file)    
+
+def get_font_image_and_text_from_local(CHARS, font_file, font_length=30, font_size=12, word_dict=None):
+    text = get_random_text(CHARS, word_dict, font_length)    
+    r = random.random()
+    if r>=0.5:
+       img = renderFontBypyGame(font_file, font_size, text)        
+    else:
+       img = renderFontByPIL(font_file, font_size, text, random_font_mode=True)
+    img = utils_pil.convert_to_rgb(img)
+    img = trim(img) 
+    return text, img
+
+def get_font_image_and_text_from_url(CHARS, font_name, font_length=30, font_size=12, word_dict=None):
+    text = get_random_text(CHARS, word_dict, font_length)
+    img  = get_font_image_from_url(text, font_name, font_size)   
+    return text, img
+
+def add_noise(img):
+    img = utils_pil.random_brightness(img)
+    img = utils_pil.random_color(img)
+    img = utils_pil.random_contrast(img)
+    return img
---
+++ 6/1/utils_pil.py
@@ -0,0 +1,81 @@
+from PIL import Image, ImageDraw, ImageFont, ImageChops, ImageEnhance 
+import random
+import matplotlib.pyplot as plt
+
+# 随机截图
+def random_crop(img, width, height):  
+    width1 = randint(0, img.size[0] - width )  
+    height1 = randint(0, img.size[1] - height)  
+    width2 = width1 + width  
+    height2 = height1 + height  
+    img = img.crop((width1, height1, width2, height2))  
+    return img  
+
+# 随机左右翻转  
+def random_flip_left_right(img):  
+    prob = randint(0,1)  
+    if prob == 1:  
+        img = img.transpose(Image.FLIP_LEFT_RIGHT)  
+    return img  
+
+# 随机对比度变换 
+def random_contrast(img, lower = 0.2, upper = 1.8):  
+    factor = random.uniform(lower, upper)  
+    img = ImageEnhance.Sharpness(img)  
+    img = img.enhance(factor)  
+    return img  
+
+# 随机亮度  
+def random_brightness(img, lower = 0.6, upper = 1.4):  
+    factor = random.uniform(lower, upper)  
+    img = ImageEnhance.Brightness(img)  
+    img = img.enhance(factor)  
+    return img  
+  
+# 随机白平衡 
+def random_color(img, lower = 0.6, upper = 1.5):  
+    factor = random.uniform(lower, upper)  
+    img = ImageEnhance.Color(img)  
+    img = img.enhance(factor)  
+    return img  
+
+# 显示图片  
+def show(img, cmap='gray'):
+    plt.imshow(img, cmap = cmap)
+    plt.show() 
+
+# 删除边框
+def trim(img):
+    bg = Image.new(img.mode, img.size, img.getpixel((0,0)))
+    diff = ImageChops.difference(img, bg)
+    diff = ImageChops.add(diff, diff, 2.0, -100)
+    bbox = diff.getbbox()
+    if bbox:
+        return img.crop(bbox)
+
+# 图片缩放
+def resize(img, width, height):
+    return img.resize((width, height),Image.ANTIALIAS) 
+        
+# RGBA to RGB
+def convert_to_rgb(img):
+    mode = img.mode
+    if mode not in ('L', 'RGB'):
+        if mode == 'RGBA':
+            # 透明图片需要加白色底
+            alpha = img.split()[3]
+            bgmask = alpha.point(lambda x: 255-x)
+            img = img.convert('RGB')
+            img.paste((255,255,255), None, bgmask)
+        else:
+            img = img.convert('RGB')
+    return img
+
+def main():
+    img = getImage("abced12323","Arial",16)
+    show(img)    
+    img = random_color(img)
+    show(img, None)
+
+if __name__ == '__main__':
+    main()
