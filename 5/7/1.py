6b7955a680aac1de7f9b3baaf9bf8c2442112437
oneleaf <oneleaf@gmail.com>
2017-12-03 21:36:01 +0800

    .

--- 5/7/1.py
+++ 5/7/1.py
@@ -4,7 +4,8 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
-draw_point_count = 20
+draw_point_count = 100
+radom_data_count = 5
 
 def true_data(batch_size):
     a = np.random.uniform(1,2,size=batch_size)[:,np.newaxis] # a=[[...]]
@@ -13,23 +14,25 @@ def true_data(batch_size):
     return data
 
 with tf.variable_scope("G"):
-    g_in = tf.placeholder(tf.float32, [None, 5])
+    g_in = tf.placeholder(tf.float32, [None, radom_data_count])
     g_1 = tf.layers.dense(g_in, 256, tf.nn.relu)
     g_out = tf.layers.dense(g_1, draw_point_count)
 
 with tf.variable_scope("D"):
     true_in = tf.placeholder(tf.float32,[None, draw_point_count])
     d_1     = tf.layers.dense(true_in, 256, tf.nn.relu, name='1')
-    prob_1  = tf.layers.dense(d_1, 1, tf.nn.sigmoid, name='out')
-    d_2     = tf.layers.dense(g_out, 256, tf.nn.relu, name='1', reuse=True)
-    prob_2  = tf.layers.dense(d_2, 1, tf.nn.sigmoid, name='out', reuse=True)
+    prob_1  = tf.layers.dense(d_1, 1, tf.nn.sigmoid, name='out')    # 0~1
 
-d_loss = -tf.reduce_mean(tf.log(prob_1) + tf.log(1-prob_2))
-g_loss = tf.reduce_mean(tf.log(1-prob_2))
+    d_2     = tf.layers.dense(g_out, 256, tf.nn.relu, name='1', reuse=True)
+    prob_2  = tf.layers.dense(d_2, 1, tf.nn.sigmoid, name='out', reuse=True)    #0~1
+
+d_loss = -tf.reduce_mean(tf.log(prob_1) + tf.log(1-prob_2))     #-00 ~ 0  0~-00  => prob1 -> 1 prob2 -> 0 
+g_loss = -tf.reduce_mean(tf.log(prob_2))                        # prob2 -> 1
 
 train_d = tf.train.AdamOptimizer(1e-4).minimize(d_loss, var_list=tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='D'))
 train_g = tf.train.AdamOptimizer(1e-4).minimize(g_loss, var_list=tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='G'))
-
+# train_d = tf.train.AdamOptimizer(1e-4).minimize(d_loss)
+# train_g = tf.train.AdamOptimizer(1e-4).minimize(g_loss)
 
 def main():
     sess = tf.Session()
@@ -40,11 +43,14 @@ def main():
 
     for step in range(100000):
         _true_data = true_data(batch_size)
-        _g_ideas = np.random.randn(batch_size, 5)
-        _g_out, _prob_1, _d_loss,_,_ = sess.run([g_out, prob_1, d_loss, train_d, train_g],
-            {g_in:_g_ideas, true_in: _true_data})
-        print(step, _d_loss)
-        if step % 1000 == 0:  # plotting
+        _g_ideas = np.random.randn(batch_size, radom_data_count)
+        _feed = {g_in:_g_ideas, true_in: _true_data}
+        _, _g_loss,_prob_2=sess.run([train_g, g_loss, prob_2], _feed)
+        # print("g:",_g_loss,_prob_2[0])
+        sess.run(train_d, _feed)
+        _, _g_out, _prob_1, _d_loss = sess.run([train_d, g_out, prob_1, d_loss ], _feed)
+        # print("d:",_d_loss, _prob_1[0])
+        if step % 200 == 0:  # plotting
             plt.cla()
             plt.plot(PAINT_POINTS[0], _g_out[0], c='#4AD631', lw=3, label='Generated painting',)
             plt.plot(PAINT_POINTS[0], 2 * np.power(PAINT_POINTS[0], 2) + 1, c='#74BCFF', lw=3, label='upper bound')
