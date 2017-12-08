4b7e0e22d242711d27debc64141a597f2d48f61e
王翩 <wp@bsw.com.cn>
2017-12-08 17:50:22 +0800

    .

--- 6/1/font2font_srgan.py
+++ 6/1/font2font_srgan.py
@@ -32,6 +32,7 @@ ASCII_CHARS = [chr(c) for c in range(32,126+1)]
 
 CHARS = ASCII_CHARS #+ ZH_CHARS + ZH_CHARS_PUN
 # CHARS = ASCII_CHARS
+CLASSES_NUMBER = len(CHARS)
 
 #初始化学习速率
 LEARNING_RATE_INITIAL = 1e-3
@@ -87,74 +88,73 @@ def SRGAN_d(inputs, reuse=False):
             net = slim.conv2d(net, df_dim * n, [3,3], normalizer_fn = None, activation_fn = tf.nn.relu)
             net = slim.batch_norm(net, activation_fn = tf.nn.relu)            
         net = tf.nn.relu(net + layer)
-        #net = tf.contrib.layers.flatten(net)
         net = tf.reshape(net,(-1, df_dim * 8))
         logits = slim.fully_connected(net, 1, activation_fn=tf.identity)
         net_ho = tf.nn.sigmoid(logits)
         return net_ho, logits
 
 def vgg19(inputs, reuse = False):
-    layer = inputs
     with tf.variable_scope("VGG19", reuse=reuse):
+        layer = slim.conv2d(inputs, 64, [3,3], normalizer_fn = None, activation_fn = tf.nn.relu)
         layer = slim.conv2d(layer, 64, [3,3], normalizer_fn = None, activation_fn = tf.nn.relu)
-        layer = slim.conv2d(layer, 64, [3,3], normalizer_fn = None, activation_fn = tf.nn.relu)
-        layer = slim.max_pool2d(layer, [2, 2], padding="SAME", stride=2)
+        layer = slim.max_pool2d(layer, [2, 2], padding="SAME", stride=1)
         layer = slim.conv2d(layer, 128, [3,3], normalizer_fn = None, activation_fn = tf.nn.relu)
         layer = slim.conv2d(layer, 128, [3,3], normalizer_fn = None, activation_fn = tf.nn.relu)
-        layer = slim.max_pool2d(layer, [2, 2], padding="SAME", stride=2)
+        layer = slim.max_pool2d(layer, [2, 2], padding="SAME", stride=1)
         layer = slim.conv2d(layer, 256, [3,3], normalizer_fn = None, activation_fn = tf.nn.relu)
         layer = slim.conv2d(layer, 256, [3,3], normalizer_fn = None, activation_fn = tf.nn.relu)
         layer = slim.conv2d(layer, 256, [3,3], normalizer_fn = None, activation_fn = tf.nn.relu)
         layer = slim.conv2d(layer, 256, [3,3], normalizer_fn = None, activation_fn = tf.nn.relu)
-        layer = slim.max_pool2d(layer, [2, 2], padding="SAME", stride=2)
+        layer = slim.max_pool2d(layer, [2, 2], padding="SAME", stride=1)
         layer = slim.conv2d(layer, 512, [3,3], normalizer_fn = None, activation_fn = tf.nn.relu)
         layer = slim.conv2d(layer, 512, [3,3], normalizer_fn = None, activation_fn = tf.nn.relu)
         layer = slim.conv2d(layer, 512, [3,3], normalizer_fn = None, activation_fn = tf.nn.relu)
         layer = slim.conv2d(layer, 512, [3,3], normalizer_fn = None, activation_fn = tf.nn.relu)
-        layer = slim.max_pool2d(layer, [2, 2], padding="SAME", stride=2)
+        layer = slim.max_pool2d(layer, [2, 2], padding="SAME", stride=1)
         conv = layer
         layer = slim.conv2d(layer, 512, [3,3], normalizer_fn = None, activation_fn = tf.nn.relu)
         layer = slim.conv2d(layer, 512, [3,3], normalizer_fn = None, activation_fn = tf.nn.relu)
         layer = slim.conv2d(layer, 512, [3,3], normalizer_fn = None, activation_fn = tf.nn.relu)
         layer = slim.conv2d(layer, 512, [3,3], normalizer_fn = None, activation_fn = tf.nn.relu)
-        layer = slim.max_pool2d(layer, [2, 2], padding="SAME", stride=2)
-
-        #layer = tf.contrib.layers.flatten(layer)  
-        layer = tf.reshape(layer, (-1,256))
+        layer = slim.max_pool2d(layer, [2, 2], padding="SAME", stride=1)
+        layer = tf.reshape(layer, (-1,512))
         layer = slim.fully_connected(layer, 4096, activation_fn=tf.nn.relu)   
         layer = slim.fully_connected(layer, 4096, activation_fn=tf.nn.relu)   
-        layer = slim.fully_connected(layer, 1000, activation_fn=tf.identity)   
+        layer = slim.fully_connected(layer, CLASSES_NUMBER, activation_fn=tf.identity)    
         return layer, conv
 
 def neural_networks():
     # 输入：训练的数量，一张图片的宽度，一张图片的高度 [-1,-1,16]
     inputs = tf.placeholder(tf.float32, [None, None, image_height], name="inputs")
-    labels = tf.placeholder(tf.float32, [None, None, image_height], name="labels")
-    
-    keep_prob = tf.placeholder(tf.float32, name="keep_prob")
-    drop_prob = 1 - keep_prob
+    targets = tf.placeholder(tf.float32, [None, None, image_height], name="targets")
+    labels = tf.placeholder(tf.int32, [None, CLASSES_NUMBER], name="labels") 
 
     shape = tf.shape(inputs)
     batch_size, image_width = shape[0], shape[1]
 
     layer = tf.reshape(inputs, (batch_size, image_width, image_height, 1))
-    layer_labels = tf.reshape(labels, (batch_size, image_width, image_height, 1))
+    layer_targets = tf.reshape(targets, (batch_size, image_width, image_height, 1))
+
+    vgg_vars     = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='VGG19')
+    net_vgg, _ = vgg19(inputs, reuse = False)
+    vgg_loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=net_vgg, labels=labels)
+    vgg_optim = tf.train.AdamOptimizer(LEARNING_RATE_INITIAL).minimize(vgg_loss, global_step=global_step, var_list=vgg_vars)
 
     net_g = SRGAN_g(layer, reuse = False)
-    net_d, logits_real = SRGAN_d(layer_labels, reuse = False)
+    net_d, logits_real = SRGAN_d(layer_targets, reuse = False)
     _,     logits_fake = SRGAN_d(net_g, reuse = True)
 
-    net_vgg, vgg_target_emb = vgg19(layer_labels, reuse = False)
-    _, vgg_predict_emb      = vgg19(net_g, reuse = True)
+    _, vgg_target_emb   = vgg19(layer_targets, reuse = True)
+    _, vgg_predict_emb  = vgg19(net_g, reuse = True)
 
     d_loss1 = tf.losses.sigmoid_cross_entropy(logits_real, tf.ones_like(logits_real))
     d_loss2 = tf.losses.sigmoid_cross_entropy(logits_fake, tf.zeros_like(logits_fake))
     d_loss  = d_loss1 + d_loss2
 
     g_gan_loss = 1e-3 * tf.losses.sigmoid_cross_entropy(logits_fake, tf.ones_like(logits_real))
-    mse_loss   = tf.losses.mean_squared_error(net_g, layer_labels)
-    vgg_loss   = 2e-6 * tf.losses.mean_squared_error(vgg_target_emb, vgg_predict_emb)
-    g_loss     = g_gan_loss + mse_loss + vgg_loss
+    g_gan_mse_loss   = tf.losses.mean_squared_error(net_g, layer_targets)
+    g_gan_vgg_loss   = 2e-6 * tf.losses.mean_squared_error(vgg_target_emb, vgg_predict_emb)
+    g_loss     = g_gan_loss + g_gan_mse_loss + g_gan_vgg_loss
     
     g_vars     = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='SRGAN_g')
     d_vars     = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='SRGAN_d')
@@ -165,7 +165,8 @@ def neural_networks():
     g_optim = tf.train.AdamOptimizer(LEARNING_RATE_INITIAL).minimize(g_loss, global_step=global_step, var_list=g_vars)
     d_optim = tf.train.AdamOptimizer(LEARNING_RATE_INITIAL).minimize(d_loss, global_step=global_step, var_list=d_vars)
 
-    return inputs, labels, global_step, mse_loss, g_optim_init, d_loss, d_optim, g_loss, mse_loss, vgg_loss, g_gan_loss, g_optim, net_g
+    return inputs, targets, labels, global_step, mse_loss, g_optim_init, d_loss, d_optim, 
+            g_loss, g_gan_mse_loss, g_gan_vgg_loss, g_gan_loss, g_optim, net_g, vgg_loss, vgg_optim
 
 
 ENGFontNames, CHIFontNames = utils_font.get_font_names_from_url()
@@ -174,6 +175,27 @@ print("CHIFontNames", CHIFontNames)
 AllFontNames = ENGFontNames + CHIFontNames
 
 eng_world_list = open(os.path.join(curr_dir,"eng.wordlist.txt"),encoding="UTF-8").readlines() 
+
+def get_vgg_next_batch(batch_size=128):
+    images = []  
+    labels = []
+    for i in range(batch_size):
+        font_name = random.choice(AllFontNames)
+        # font_length = random.randint(font_min_length-5, font_min_length+5)
+        font_length = 1
+        font_size = random.randint(image_height, 64)    
+        font_mode = random.choice([0,1,2,4]) 
+        font_hint = random.choice([0,1,2,3,4,5]) 
+        text  = utils_font.get_random_text(CHARS, eng_world_list, font_length)    
+        image = utils_font.get_font_image_from_url(text, font_name ,font_size)
+        image = utils_pil.convert_to_gray(image)
+        image = np.asarray(image)     
+        image = utils.resize(image, height=image_height)
+        image = (255. - image) / 255.
+        images.append(image)
+        labels.append(CHARS.index(text))
+    return image, labels
+    
 # 生成一个训练batch ,每一个批次采用最大图片宽度
 def get_next_batch(batch_size=128):
     images = []   
@@ -226,7 +248,8 @@ def get_next_batch(batch_size=128):
 
 def train():
     global_step = tf.Variable(0, trainable=False)
-    inputs, labels, global_step, mse_loss, g_optim_init, d_loss, d_optim, g_loss, mse_loss, vgg_loss, g_gan_loss, g_optim, net_g = neural_networks()
+    inputs, targets, labels, global_step, mse_loss, g_optim_init, d_loss, d_optim, 
+        g_loss, g_gan_mse_loss, g_gan_vgg_loss, g_gan_loss, g_optim, net_g, vgg_loss, vgg_optim = neural_networks()
 
     curr_dir = os.path.dirname(__file__)
     model_dir = os.path.join(curr_dir, MODEL_SAVE_NAME)
@@ -242,14 +265,23 @@ def train():
             print("Restore Model ...")
             saver.restore(session, ckpt.model_checkpoint_path)    
 
+        steps = session.run(global_step)
+        # train vgg19
+        while steps < 100000:
+            for batch in range(BATCHES):
+                start = time.time() 
+                train_inputs, train_labels = get_vgg_next_batch(BATCH_SIZE)
+                errM, _ , steps= session.run([mse_loss, g_optim_init, global_step], {inputs: train_inputs, labels: train_labels})
+                print("%8d time: %4.4fs, mse: %.8f " % (steps, time.time() - start, errM))
+            saver.save(session, saver_prefix, global_step=steps)                
+
         # initialize G
-        while True:
+        while steps < 200000:
             for batch in range(BATCHES):
                 start = time.time() 
                 train_inputs, train_labels = get_next_batch(BATCH_SIZE)
-                errM, _ , steps= session.run([mse_loss, g_optim_init, global_step], {inputs: train_inputs, labels: train_labels})
+                errM, _ , steps= session.run([mse_loss, g_optim_init, global_step], {inputs: train_inputs, targets: train_labels})
                 print("%8d time: %4.4fs, mse: %.8f " % (steps, time.time() - start, errM))
-            if steps > 100: break
             saver.save(session, saver_prefix, global_step=steps)
 
         # train GAN (SRGAN)
@@ -259,9 +291,10 @@ def train():
                 train_inputs, train_labels = get_next_batch(BATCH_SIZE)  
 
                 ## update D
-                errD, _ = session.run([d_loss, d_optim], {inputs: train_inputs, labels: train_labels})
+                errD, _ = session.run([d_loss, d_optim], {inputs: train_inputs, targets: train_labels})
                 ## update G
-                errG, errM, errV, errA, _, steps = session.run([g_loss, mse_loss, vgg_loss, g_gan_loss, g_optim, global_step], {inputs: train_inputs, labels: train_labels})
+                errG, errM, errV, errA, _, steps = session.run([g_loss, mse_loss, vgg_loss, g_gan_loss, g_optim, global_step],
+                     {inputs: train_inputs, targets: train_labels})
                 print("%8d time: %4.4fs, d_loss: %.8f g_loss: %.8f (mse: %.6f vgg: %.6f adv: %.6f)" % (steps, time.time() - start, errD, errG, errM, errV, errA))
 
                 if np.isnan(errG) or np.isinf(errG) or np.isnan(errA) or np.isinf(errA) or np.isnan(errD) or np.isinf(errD):
@@ -274,7 +307,7 @@ def train():
 
                 if steps > 0 and steps % REPORT_STEPS == 0:
                     test_inputs, test_labels = get_next_batch(1)             
-                    feed = {inputs: test_inputs, labels: test_labels}
+                    feed = {inputs: test_inputs, targets: test_labels}
                     b_predictions = session.run([net_g], feed)                     
                     b_predictions = np.reshape(b_predictions[0],test_labels[0].shape)   
                     _pred = np.transpose(b_predictions)        
