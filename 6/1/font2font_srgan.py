e8ed7f52d54b80f4c1ba2087593949b3ada85664
王翩 <wp@bsw.com.cn>
2017-12-14 10:26:37 +0800

    .

--- 6/1/font2font_srgan.py
+++ 6/1/font2font_srgan.py
@@ -277,13 +277,13 @@ def train():
                     train_inputs, train_targets, train_labels, train_seq_len = get_next_batch(BATCH_SIZE)
                     feed = {inputs: train_inputs, targets: train_targets, labels: train_labels, seq_len: train_seq_len}
                 
-                    # # train G
-                    # start = time.time() 
-                    # errM, _ , steps= session.run([g_mse_loss, g_optim_init, global_step], feed)
-                    # print("%8d time: %4.4fs, g_mse_loss: %.8f " % (steps, time.time() - start, errM))
-                    # if np.isnan(errM) or np.isinf(errM) :
-                    #     print("Error: cost is nan or inf")
-                    #     return                    
+                    # train G
+                    start = time.time() 
+                    errM, _ , steps= session.run([g_mse_loss, g_optim_init, global_step], feed)
+                    print("%8d time: %4.4fs, g_mse_loss: %.8f " % (steps, time.time() - start, errM))
+                    if np.isnan(errM) or np.isinf(errM) :
+                        print("Error: cost is nan or inf")
+                        return                    
 
                     # train highway
                     start = time.time() 
@@ -316,7 +316,7 @@ def train():
                     print("Error: cost is nan or inf")
                     return 
 
-                if steps > 0 and steps % REPORT_STEPS < 10:
+                if steps > 0 and steps % REPORT_STEPS < 15:
                     train_inputs, train_targets, train_labels, train_seq_len = get_next_batch(1)             
                     feed = {inputs: train_inputs, targets: train_targets}
                     b_predictions = session.run([net_g], feed)                     
