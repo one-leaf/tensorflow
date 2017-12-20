e9ce4b3846e0f2a7cc65c994c79ee6a21ca129a5
王翩 <wp@bsw.com.cn>
2017-12-20 11:32:30 +0800

    。

--- 6/1/ocr_ascii_srgan.py
+++ 6/1/ocr_ascii_srgan.py
@@ -409,8 +409,19 @@ def train():
                         return                       
                     if errM > 15 :  print(train_info)
 
-                train_inputs, train_targets = get_next_batch_for_srgan(4)
-                feed = {inputs: train_inputs, targets: train_targets}
+                for i in range(10):
+                    train_inputs, train_targets = get_next_batch_for_srgan(4)
+                    feed = {inputs: train_inputs, targets: train_targets}
+                    start = time.time()                                
+                    ## update G
+                    errG, errM, errV, errA, _, steps = session.run([g_loss, g_mse_loss, g_res_loss, g_gan_loss, g_optim, global_step], feed)
+                    print("%d time: %4.4fs, g_loss: %.8f (mse: %.6f res: %.6f adv: %.6f)" % (steps, time.time() - start, errG, errM, errV, errA))
+                    if np.isnan(errG) or np.isinf(errG) or np.isnan(errA) or np.isinf(errA):
+                        print("Error: cost is nan or inf")
+                        return 
+
+                # train_inputs, train_targets = get_next_batch_for_srgan(4)
+                # feed = {inputs: train_inputs, targets: train_targets}
 
                 # train G
                 start = time.time() 
@@ -420,23 +431,14 @@ def train():
                     print("Error: cost is nan or inf")
                     return    
 
-                # train GAN (SRGAN)
-                start = time.time()                
-                ## update D
-                errD, errD1, errD2, _, steps = session.run([d_loss, d_loss1, d_loss2, d_optim, global_step], feed)
-                print("%d time: %4.4fs, d_loss: %.8f (d_loss1: %.6f  d_loss2: %.6f)" % (steps, time.time() - start, errD, errD1, errD2))
-                if np.isnan(errD) or np.isinf(errD):
-                    print("Error: cost is nan or inf")
-                    return 
-
-                for i in range(10):
-                    train_inputs, train_targets = get_next_batch_for_srgan(4)
-                    feed = {inputs: train_inputs, targets: train_targets}
-                    start = time.time()                                
-                    ## update G
-                    errG, errM, errV, errA, _, steps = session.run([g_loss, g_mse_loss, g_res_loss, g_gan_loss, g_optim, global_step], feed)
-                    print("%d time: %4.4fs, g_loss: %.8f (mse: %.6f res: %.6f adv: %.6f)" % (steps, time.time() - start, errG, errM, errV, errA))
-                    if np.isnan(errG) or np.isinf(errG) or np.isnan(errA) or np.isinf(errA):
+                # 如果 G 网络没有训练出来，暂时不训练 D 网络
+                if  errA < 0:
+                    # train GAN (SRGAN)
+                    start = time.time()                
+                    ## update D
+                    errD, errD1, errD2, _, steps = session.run([d_loss, d_loss1, d_loss2, d_optim, global_step], feed)
+                    print("%d time: %4.4fs, d_loss: %.8f (d_loss1: %.6f  d_loss2: %.6f)" % (steps, time.time() - start, errD, errD1, errD2))
+                    if np.isnan(errD) or np.isinf(errD):
                         print("Error: cost is nan or inf")
                         return 
 
