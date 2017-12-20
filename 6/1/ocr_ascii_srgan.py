fc69732cb6380853bc365e12bcdff88bbc3a39ae
oneleaf <oneleaf@gmail.com>
2017-12-20 23:24:37 +0800

    .

--- 6/1/ocr_ascii_srgan.py
+++ 6/1/ocr_ascii_srgan.py
@@ -603,7 +603,7 @@ def train():
                         if np.isnan(errR) or np.isinf(errR) :
                             print("Error: cost is nan or inf")
                             return                       
-                        if errR > 15 and acc > 0.8: print(train_info)
+                        # if errR > 15 and acc > 0.8: print(train_info)
 
                 if steps > 0 and steps % REPORT_STEPS < (steps-start_steps):
                     train_inputs, train_labels, train_seq_len, train_info = get_next_batch_for_res(4)   
