7af6abe6ec98af3b447cc8c00905e09fbedc5d55
王翩 <wp@bsw.com.cn>
2017-12-21 18:19:50 +0800

    .

--- 6/1/ocr_ascii_res.py
+++ 6/1/ocr_ascii_res.py
@@ -692,8 +692,10 @@ def train():
                     train_inputs, train_labels, train_seq_len, train_info = get_next_batch_for_res(4)   
                     print(train_info)          
                     p_dcCnn = session.run(dncnn, {inputs: train_inputs})
-                    p_net_g = session.run(net_g, {inputs: train_inputs, clears: np.squeeze(p_dcCnn)}) 
-                    decoded_list = session.run(res_decoded[0], {inputs: np.squeeze(p_net_g), seq_len: train_seq_len}) 
+                    p_dcCnn = np.squeeze(p_dcCnn)
+                    p_net_g = session.run(net_g, {inputs: train_inputs, clears: p_dcCnn}) 
+                    p_net_g = np.squeeze(p_net_g)
+                    decoded_list = session.run(res_decoded[0], {inputs: p_net_g, seq_len: train_seq_len}) 
 
                     for i in range(4): 
                         _p_dcCnn = np.transpose(p_dcCnn[i])                    
