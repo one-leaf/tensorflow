92fa883c059f87495ca32a70af7e2464cb00ecaa
王翩 <wp@bsw.com.cn>
2017-12-21 17:15:51 +0800

    .

--- 6/1/ocr_ascii_res.py
+++ 6/1/ocr_ascii_res.py
@@ -678,7 +678,7 @@ def train():
 
                 # 训练RES
                 for i in range(16):
-                    train_inputs, train_labels, train_seq_len, train_info = get_next_batch_for_res_train(4)
+                    train_inputs, train_labels, train_seq_len, train_info = get_next_batch_for_res_train(16)
                     feed = {inputs: train_inputs, labels: train_labels, seq_len: train_seq_len}
                     start = time.time() 
                     errR, acc, _ , steps= session.run([res_loss, res_acc, res_optim, global_step], feed)
