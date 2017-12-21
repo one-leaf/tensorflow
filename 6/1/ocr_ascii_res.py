019e1b882b7ac8ddbdc47408384b1aaeccbb5515
王翩 <wp@bsw.com.cn>
2017-12-21 18:02:13 +0800

    。

--- 6/1/ocr_ascii_res.py
+++ 6/1/ocr_ascii_res.py
@@ -691,9 +691,9 @@ def train():
                 if steps > 0 and steps % REPORT_STEPS < 16:
                     train_inputs, train_labels, train_seq_len, train_info = get_next_batch_for_res(4)   
                     print(train_info)          
-                    p_dcCnn = session.run(dncnn, {inputs: train_inputs})
-                    p_net_g = session.run(net_g, {clears: p_dcCnn}) 
-                    decoded_list = session.run(res_decoded[0], {inputs: p_net_g, seq_len: train_seq_len}) 
+                    p_dcCnn = session.run(dncnn, {inputs: np.squeeze(train_inputs, axis=(2,))})
+                    p_net_g = session.run(net_g, {clears: np.squeeze(p_dcCnn, axis=(2,))}) 
+                    decoded_list = session.run(res_decoded[0], {inputs: np.squeeze(p_net_g, axis=(2,)), seq_len: train_seq_len}) 
 
                     for i in range(4): 
                         _p_dcCnn = np.transpose(p_dcCnn[i])                    
