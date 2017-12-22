5ee3004ddb414aaaadc33727dca8d9de8572e553
王翩 <wp@bsw.com.cn>
2017-12-22 14:49:13 +0800

    .

--- 6/1/ocr_ascii_res.py
+++ 6/1/ocr_ascii_res.py
@@ -67,7 +67,7 @@ def SRGAN_d(inputs, reuse=False):
         shape = tf.shape(inputs)
         batch_size = shape[0]
         layer = tf.reshape(layer, [batch_size, -1, 4 * 512])
-        layer = slim.fully_connected(layer, 1000, normalizer_fn=slim.batch_norm, activation_fn=tf.nn.relu)
+        # layer = slim.fully_connected(layer, 1000, normalizer_fn=slim.batch_norm, activation_fn=tf.nn.relu)
         layer = slim.fully_connected(layer, 1, normalizer_fn=slim.batch_norm, activation_fn=None)        
         return layer
 
@@ -76,7 +76,7 @@ def RES(inputs, reuse = False):
         layer, conv = utils_nn.resNet50(inputs)
         shape = tf.shape(inputs)
         batch_size = shape[0] 
-        layer = slim.fully_connected(layer, 1000, normalizer_fn=slim.batch_norm, activation_fn=tf.nn.relu)
+        # layer = slim.fully_connected(layer, 1000, normalizer_fn=slim.batch_norm, activation_fn=tf.nn.relu)
         layer = slim.fully_connected(layer, CLASSES_NUMBER, normalizer_fn=slim.batch_norm, activation_fn=None)
         layer = tf.reshape(layer, [batch_size, -1, CLASSES_NUMBER])
         return layer, conv
