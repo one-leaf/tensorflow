c3c051ab79b8e136f71237d64816fd976d92e36a
王翩 <wp@bsw.com.cn>
2017-12-22 18:07:35 +0800

    .

--- 6/1/ocr_ascii_res.py
+++ 6/1/ocr_ascii_res.py
@@ -196,7 +196,7 @@ def get_next_batch_for_res(batch_size=128):
         if image.shape[1] > max_width_image: 
             max_width_image = image.shape[1]
         info = info+"%s\n\r" % utils_font.get_font_url(text, font_name, font_size, font_mode, font_hint)
-    max_width_image = max_width_image #+ (POOL_SIZE - max_width_image % POOL_SIZE)
+    #max_width_image = max_width_image + (POOL_SIZE - max_width_image % POOL_SIZE)
     inputs = np.zeros([batch_size, max_width_image, image_height])
     for i in range(len(images)):
         image_vec = utils.img2vec(images[i], height=image_height, width=max_width_image, flatten=False)
@@ -233,7 +233,7 @@ def get_next_batch_for_res_train(batch_size=128):
         if image.shape[1] > max_width_image: 
             max_width_image = image.shape[1]
         info = info+"%s\n\r" % utils_font.get_font_url(text, font_name, font_size, font_mode, font_hint)
-    max_width_image = max_width_image #+ (POOL_SIZE - max_width_image % POOL_SIZE)
+    #max_width_image = max_width_image + (POOL_SIZE - max_width_image % POOL_SIZE)
     inputs = np.zeros([batch_size, max_width_image, image_height])
     for i in range(len(images)):
         image_vec = utils.img2vec(images[i], height=image_height, width=max_width_image, flatten=False)
@@ -293,7 +293,7 @@ def get_next_batch_for_srgan(batch_size=128):
         if targets_image.shape[1] > max_width_image: 
             max_width_image = targets_image.shape[1]      
 
-    max_width_image = max_width_image + (POOL_SIZE - max_width_image % POOL_SIZE)
+    # max_width_image = max_width_image + (POOL_SIZE - max_width_image % POOL_SIZE)
     inputs = np.zeros([batch_size, max_width_image, image_height])
     for i in range(batch_size):
         image_vec = utils.img2vec(inputs_images[i], height=image_height, width=max_width_image, flatten=False)
