0df40a90509938b99d642394d92fe5b91f399256
oneleaf <oneleaf@gmail.com>
2017-12-22 22:51:38 +0800

    .

--- Logistic Regression/4.4.py
+++ Logistic Regression/4.4.py
@@ -98,7 +98,7 @@ if __name__ == '__main__':
 
     step = 0
     while mnist.train.epochs_completed < 8:
-        batch_x, batch_y= getBatch(64)
+        batch_x, batch_y= getBatch(128)
         _, loss, pred = sess.run([optimizer, cost, prediction], feed_dict={x: batch_x, y: batch_y, drop_prob: 0.25})
         if step % 10 == 0 :
             acc = 0
