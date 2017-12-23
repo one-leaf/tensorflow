06509d53789e75dd384bdae1af369eb9b71e3568
oneleaf <oneleaf@gmail.com>
2017-12-23 23:22:36 +0800

    .

--- 6/1/ocr_ascii_clean.py
+++ 6/1/ocr_ascii_clean.py
@@ -59,9 +59,8 @@ def RES(inputs, reuse = False):
         layer, conv = utils_nn.resNet50(inputs, True)
         shape = tf.shape(inputs)
         batch_size = shape[0] 
-        layer = slim.conv2d(layer, CLASSES_NUMBER,   [1,1], normalizer_fn=slim.batch_norm, activation_fn=tf.nn.tanh)
-        # layer = slim.fully_connected(layer, 1000, normalizer_fn=slim.batch_norm, activation_fn=tf.nn.relu)
-        # layer = slim.fully_connected(layer, CLASSES_NUMBER, normalizer_fn=slim.batch_norm, activation_fn=tf.nn.tanh)  
+        layer = slim.fully_connected(layer, 1000, normalizer_fn=slim.batch_norm, activation_fn=tf.nn.relu)
+        layer = slim.fully_connected(layer, CLASSES_NUMBER, normalizer_fn=None, activation_fn=None)  
         layer = tf.reshape(layer, [batch_size, -1, CLASSES_NUMBER])
         return layer, conv
 
@@ -69,8 +68,6 @@ def neural_networks():
     # 输入：训练的数量，一张图片的宽度，一张图片的高度 [-1,-1,16]
     inputs = tf.placeholder(tf.float32, [None, None, image_height], name="inputs")
     # 干净的图片
-    clears = tf.placeholder(tf.float32, [None, None, image_height], name="clears")
-    # 放大后的图片
     targets = tf.placeholder(tf.float32, [None, None, image_height], name="targets")
     labels = tf.sparse_placeholder(tf.int32, name="labels")
     global_step = tf.Variable(0, trainable=False)
@@ -79,14 +76,7 @@ def neural_networks():
     batch_size, image_width = shape[0], shape[1]
 
     layer = tf.reshape(inputs, (batch_size, image_width, image_height, 1))
-    layer_clears = tf.reshape(clears, (batch_size, image_width, image_height, 1))
     layer_targets = tf.reshape(targets, (batch_size, image_width, image_height, 1))
-
-    # 降噪网络
-    dncnn = DnCNN(layer)
-    dncnn_loss  = tf.losses.mean_squared_error(layer_clears, dncnn)
-    dncnn_vars  = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='DnCNN')    
-    dncnn_optim = tf.train.AdamOptimizer(LEARNING_RATE_INITIAL).minimize(dncnn_loss, global_step=global_step, var_list=dncnn_vars)
 
     # OCR RESNET 识别 网络
     # net_res, _ = RES(layer_targets, reuse = True)
@@ -102,7 +92,7 @@ def neural_networks():
     res_acc = 1 - res_acc / tf.to_float(tf.size(labels.values))
 
     # 对抗网络
-    net_g = SRGAN_g(layer_clears, reuse = False)
+    net_g = SRGAN_g(layer, reuse = False)
     logits_real = SRGAN_d(layer_targets, reuse = False)
     logits_fake = SRGAN_d(net_g, reuse = True)
     _, res_target_emb   = RES(layer_targets, reuse = True)
@@ -128,8 +118,7 @@ def neural_networks():
     g_optim = tf.train.AdamOptimizer(LEARNING_RATE_INITIAL).minimize(g_loss, global_step=global_step, var_list=g_vars)
     d_optim = tf.train.AdamOptimizer(LEARNING_RATE_INITIAL).minimize(d_loss, global_step=global_step, var_list=d_vars)
 
-    return  inputs, clears, targets, labels, global_step, \
-            dncnn, dncnn_loss, dncnn_optim, \
+    return  inputs, targets, labels, global_step, \
             g_optim_mse, d_loss, d_loss1, d_loss2, d_optim, \
             g_loss, g_mse_loss, g_res_loss, g_gan_loss, g_optim, net_g, \
             res_loss, res_optim, seq_len, res_acc, res_decoded
@@ -151,92 +140,9 @@ eng_world_list = open(os.path.join(curr_dir,"eng.wordlist.txt"),encoding="UTF-8"
 def list_to_chars(list):
     return "".join([CHARS[v] for v in list])
 
-def get_next_batch_for_res(batch_size=128):
-    images = []   
-    codes = []
-    max_width_image = 0
-    info = ""
-    for i in range(batch_size):
-        font_name = random.choice(AllFontNames)
-        font_length = random.randint(25, 30)
-        if random.random()>0.5:
-            font_size = random.randint(8, 49)    
-        else:
-            font_size = random.randint(8, 15) 
-        font_mode = random.choice([0,1,2,4]) 
-        font_hint = random.choice([0,1,3,4,5])     #删除了2
-        text = random.sample(CHARS, 12)
-        text = text+text+[" "," "]
-        random.shuffle(text)
-        text = "".join(text).strip()
-        codes.append([CHARS.index(char) for char in text])          
-        image = utils_font.get_font_image_from_url(text, font_name, font_size, font_mode, font_hint )
-        image = utils_pil.resize_by_height(image, image_height, random.random()>0.5)
-        image = utils_font.add_noise(image)   
-        image = utils_pil.convert_to_gray(image)                   
-        image = np.asarray(image)     
-        image = utils.resize(image, height=image_height)
-        if random.random()>0.5:
-            image = (255. - image) / 255.
-        else:
-            image = image / 255.
-        images.append(image)
-        if image.shape[1] > max_width_image: 
-            max_width_image = image.shape[1]
-        info = info+"%s\n\r" % utils_font.get_font_url(text, font_name, font_size, font_mode, font_hint)
-    max_width_image = max_width_image + (POOL_SIZE - max_width_image % POOL_SIZE)
-    inputs = np.zeros([batch_size, max_width_image, image_height])
-    for i in range(len(images)):
-        image_vec = utils.img2vec(images[i], height=image_height, width=max_width_image, flatten=False)
-        inputs[i,:] = np.transpose(image_vec)
-
-    labels = [np.asarray(i) for i in codes]
-    sparse_labels = utils.sparse_tuple_from(labels)
-    seq_len = np.ones(batch_size) * (max_width_image * image_height ) // (POOL_SIZE * POOL_SIZE)                
-    return inputs, sparse_labels, seq_len, info
-     
-def get_next_batch_for_res_train(batch_size=128):
-    images = []   
-    codes = []
-    max_width_image = 0
-    info = ""
-    for i in range(batch_size):
-        font_name = random.choice(AllFontNames)
-        font_length = random.randint(25, 30)
-        font_size = 36    
-        font_mode = random.choice([0,1,2,4]) 
-        font_hint = random.choice([0,1,3,4,5])     #删除了2
-        text = random.sample(CHARS, 12)
-        text = text+text+[" "," "]
-        random.shuffle(text)
-        text = "".join(text).strip()
-        codes.append([CHARS.index(char) for char in text])          
-        image = utils_font.get_font_image_from_url(text, font_name, font_size, font_mode, font_hint )
-        image = utils_pil.resize_by_height(image, image_height)
-        image = utils_pil.convert_to_gray(image)                           
-        image = np.asarray(image)     
-        # image = utils.resize(image, height=image_height)
-        # image = utils.img2bwinv(image)
-        image = utils_pil.convert_to_bw(image)        
-        images.append((255. - image) / 255.)
-        if image.shape[1] > max_width_image: 
-            max_width_image = image.shape[1]
-        info = info+"%s\n\r" % utils_font.get_font_url(text, font_name, font_size, font_mode, font_hint)
-    max_width_image = max_width_image + (POOL_SIZE - max_width_image % POOL_SIZE)
-    inputs = np.zeros([batch_size, max_width_image, image_height])
-    for i in range(len(images)):
-        image_vec = utils.img2vec(images[i], height=image_height, width=max_width_image, flatten=False)
-        inputs[i,:] = np.transpose(image_vec)
-
-    labels = [np.asarray(i) for i in codes]
-    sparse_labels = utils.sparse_tuple_from(labels)
-    seq_len = np.ones(batch_size) * (max_width_image * image_height ) // (POOL_SIZE * POOL_SIZE)                
-    return inputs, sparse_labels, seq_len, info
-
 # 生成一个训练batch ,每一个批次采用最大图片宽度
 def get_next_batch_for_srgan(batch_size=128):
     inputs_images  = []
-    clears_images  = []   
     targets_images = []
     max_width_image = 0
     for i in range(batch_size):
@@ -249,17 +155,16 @@ def get_next_batch_for_srgan(batch_size=128):
         image = utils_font.get_font_image_from_url(text, font_name, font_size, font_mode, font_hint)
         image = utils_pil.resize_by_height(image, image_height)
         image = utils_pil.convert_to_gray(image)
-        targets_image = image.copy()
 
         _h =  random.randint(9, image_height // random.choice([1,1.5,2,2.5]))
         image = utils_pil.resize_by_height(image, _h)        
         image = utils_pil.resize_by_height(image, image_height, random.random()>0.5) 
 
-        clears_image = image.copy()
-        clears_image = np.asarray(clears_image)
-        # clears_image = utils.resize(clears_image, height=image_height)
-        clears_image = utils_pil.convert_to_bw(clears_image)
-        clears_images.append((255. - clears_image) / 255.)
+        targets_image = image.copy()
+        targets_image = np.asarray(targets_image)
+        # targets_image = utils.resize(targets_image, height=image_height)
+        targets_image = utils_pil.convert_to_bw(targets_image)
+        targets_images.append((255. - clears_image) / 255.)
 
         image = utils_font.add_noise(image)   
         image = np.asarray(image)
@@ -269,18 +174,10 @@ def get_next_batch_for_srgan(batch_size=128):
             image = (255. - image) / 255.
         else:
             image = image / 255.
-        inputs_images.append(image)
-
-        targets_image = np.asarray(targets_image)   
-        # targets_image = utils.resize(targets_image, height=image_height)
-        # targets_image = utils.img2bwinv(targets_image)
-        targets_image = utils_pil.convert_to_bw(targets_image)        
-        targets_images.append((255. - targets_image) / 255.)
+        inputs_images.append(image)        
 
         if image.shape[1] > max_width_image: 
             max_width_image = image.shape[1]
-        if clears_image.shape[1] > max_width_image: 
-            max_width_image = clears_image.shape[1] 
         if targets_image.shape[1] > max_width_image: 
             max_width_image = targets_image.shape[1]      
 
@@ -290,17 +187,12 @@ def get_next_batch_for_srgan(batch_size=128):
         image_vec = utils.img2vec(inputs_images[i], height=image_height, width=max_width_image, flatten=False)
         inputs[i,:] = np.transpose(image_vec)
 
-    clears = np.zeros([batch_size, max_width_image, image_height])
-    for i in range(batch_size):
-        image_vec = utils.img2vec(clears_images[i], height=image_height, width=max_width_image, flatten=False)
-        clears[i,:] = np.transpose(image_vec)
-
     targets = np.zeros([batch_size, max_width_image, image_height])
     for i in range(batch_size):
         image_vec = utils.img2vec(targets_images[i], height=image_height, width=max_width_image, flatten=False)
         targets[i,:] = np.transpose(image_vec)
 
-    return inputs, clears, targets
+    return inputs, targets
 
 def train():
     inputs, clears, targets, labels, global_step, \
@@ -313,27 +205,20 @@ def train():
     model_dir = os.path.join(curr_dir, MODEL_SAVE_NAME)
     if not os.path.exists(model_dir): os.mkdir(model_dir)
     model_R_dir = os.path.join(model_dir, "R")
-    model_D_dir = os.path.join(model_dir, "D")
-    model_G_dir = os.path.join(model_dir, "G")
-    model_C_dir = os.path.join(model_dir, "C")
+    model_D_dir = os.path.join(model_dir, "ND")
+    model_G_dir = os.path.join(model_dir, "NG")
     if not os.path.exists(model_R_dir): os.mkdir(model_R_dir)
     if not os.path.exists(model_D_dir): os.mkdir(model_D_dir)
     if not os.path.exists(model_G_dir): os.mkdir(model_G_dir)  
-    if not os.path.exists(model_C_dir): os.mkdir(model_C_dir)  
  
     init = tf.global_variables_initializer()
     with tf.Session() as session:
         session.run(init)
 
-        c_saver = tf.train.Saver(tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='DnCNN'), max_to_keep=5)
         r_saver = tf.train.Saver(tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='RES'), max_to_keep=5)
         d_saver = tf.train.Saver(tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='SRGAN_d'), max_to_keep=5)
         g_saver = tf.train.Saver(tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='SRGAN_g'), max_to_keep=5)
 
-        ckpt = tf.train.get_checkpoint_state(model_C_dir)
-        if ckpt and ckpt.model_checkpoint_path:           
-            print("Restore Model C...")
-            c_saver.restore(session, ckpt.model_checkpoint_path)   
         ckpt = tf.train.get_checkpoint_state(model_R_dir)
         ckpt = tf.train.get_checkpoint_state(model_G_dir)
         if ckpt and ckpt.model_checkpoint_path:           
@@ -350,21 +235,8 @@ def train():
  
         while True:
             for batch in range(BATCHES):
-                train_inputs, train_clears, train_targets = get_next_batch_for_srgan(2)
-                feed = {inputs: train_inputs, clears: train_clears, targets: train_targets}
-
-                # train DnCNN
-                start = time.time()                
-                ## update D
-                errDnCNN, _, steps = session.run([dncnn_loss, dncnn_optim, global_step], feed)
-                print("%d time: %4.4fs, dnCNN_loss: %.8f" % (steps, time.time() - start, errDnCNN))
-                if np.isnan(errDnCNN) or np.isinf(errDnCNN):
-                    print("Error: cost is nan or inf")
-                    return 
-                start_steps = steps  
-
-                train_inputs, train_clears, train_targets = get_next_batch_for_srgan(1)
-                feed = {inputs: train_inputs, clears: train_clears, targets: train_targets}
+                train_inputs, train_targets = get_next_batch_for_srgan(2)
+                feed = {inputs: train_inputs, targets: train_targets}
 
                 # train GAN (SRGAN)
                 start = time.time()                
@@ -374,6 +246,8 @@ def train():
                 if np.isnan(errD) or np.isinf(errD):
                     print("Error: cost is nan or inf")
                     return 
+
+                start_steps = steps  
 
                 ## update G
                 start = time.time()                                
@@ -385,8 +259,8 @@ def train():
 
                 # 如果D网络的差异太大，需要多学习下G网络
                 for i in range(8):
-                    train_inputs, train_clears, train_targets = get_next_batch_for_srgan(1)
-                    feed = {inputs: train_inputs, clears: train_clears, targets: train_targets}
+                    train_inputs, train_targets = get_next_batch_for_srgan(2)
+                    feed = {inputs: train_inputs, targets: train_targets}
 
                     if errM > 0.1:
                     # train G
@@ -426,20 +300,16 @@ def train():
                 #         return                       
 
                 # 报告
-                # if steps > 0 and steps % REPORT_STEPS < (steps-start_steps):
-                #     train_inputs, train_labels, train_seq_len, train_info = get_next_batch_for_res(4)   
-                #     print(train_info)          
-                    # p_dcCnn = session.run(dncnn, {inputs: train_inputs})
-                    # p_dcCnn = np.squeeze(p_dcCnn)
-                    # p_net_g = session.run(net_g, {inputs: train_inputs, clears: p_dcCnn}) 
-                    # p_net_g = np.squeeze(p_net_g)
-                    # decoded_list = session.run(res_decoded[0], {inputs: p_net_g, seq_len: train_seq_len}) 
+                if steps > 0 and steps % REPORT_STEPS < (steps-start_steps):
+                    train_inputs, train_labels, train_seq_len, train_info = get_next_batch_for_srgan(4)   
+                    print(train_info)          
+                    p_net_g = session.run(net_g, {inputs: train_inputs}) 
+                    p_net_g = np.squeeze(p_net_g)
 
-                    # for i in range(4): 
-                    #     _p_dcCnn = np.transpose(p_dcCnn[i])                    
-                    #     _p_net_g = np.transpose(p_net_g[i])   
-                    #     _img = np.vstack((np.transpose(train_inputs[i]), _p_dcCnn, _p_net_g)) 
-                    #     cv2.imwrite(os.path.join(curr_dir,"test","%s_%s.png"%(steps,i)), _img * 255) 
+                    for i in range(4): 
+                        _p_net_g = np.transpose(p_net_g[i])   
+                        _img = np.vstack((np.transpose(train_inputs[i]), _p_net_g)) 
+                        cv2.imwrite(os.path.join(curr_dir,"test","N%s_%s.png"%(steps,i)), _img * 255) 
 
                 #     original_list = utils.decode_sparse_tensor(train_labels)
                 #     detected_list = utils.decode_sparse_tensor(decoded_list)
@@ -459,8 +329,6 @@ def train():
                 #         acc += Levenshtein.ratio(list_to_chars(number),list_to_chars(detect_number))
                 #     print("Test Accuracy:", acc / len(original_list))
 
-            print("Save Model C ...")
-            c_saver.save(session, os.path.join(model_C_dir, "C.ckpt"), global_step=steps)
             # print("Save Model R ...")
             # r_saver.save(session, os.path.join(model_R_dir, "R.ckpt"), global_step=steps)
             ckpt = tf.train.get_checkpoint_state(model_R_dir)
