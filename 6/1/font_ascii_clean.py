4c6f9a9308afe903728c9fecfa7b19059bf97592
王翩 <wp@bsw.com.cn>
2017-12-27 10:25:05 +0800

    .

--- 6/1/font_ascii_clean.py
+++ 6/1/font_ascii_clean.py
@@ -199,7 +199,7 @@ def train():
         while True:
             errA = errD2 = 1
             for batch in range(BATCHES):
-                train_inputs, train_targets = get_next_batch_for_srgan(16)
+                train_inputs, train_targets = get_next_batch_for_srgan(8)
                 feed = {inputs: train_inputs, targets: train_targets}
 
                 # start = time.time() 
