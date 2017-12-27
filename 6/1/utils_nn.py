4e21bfea732932c61d95e31e1a6ededed8dbb5b4
王翩 <wp@bsw.com.cn>
2017-12-27 10:15:24 +0800

    .

--- 6/1/utils_nn.py
+++ 6/1/utils_nn.py
@@ -357,8 +357,9 @@ def pix2pix_g2(layer, dropout=False):
 
 def pix2pix_d2(layer):
     with slim.arg_scope([slim.conv2d], kernel_size=[4, 4], stride=2, activation_fn=tf.nn.leaky_relu, normalizer_fn=slim.batch_norm):
-        for cnn in (64,128,256,512,512,512,1024,1024,1024):
-            layer = slim.conv2d(layer, cnn)
+        # for cnn in (64,128,256,512,512,512,1024,1024,1024):
+        #     layer = slim.conv2d(layer, cnn)
+        layer, _ = resNet50(layer, True)
         layer = slim.flatten(layer)
         layer = slim.fully_connected(layer, 1000)
         layer = slim.fully_connected(layer, 1)
