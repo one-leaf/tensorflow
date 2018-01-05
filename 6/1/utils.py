79ac38e011ebcef8f760cf65de1f7f3ca4d557d3
王翩 <wp@bsw.com.cn>
2018-01-05 18:36:49 +0800

    .

--- 6/1/utils.py
+++ 6/1/utils.py
@@ -217,11 +217,9 @@ def dropZeroEdgesGray(img_gray):
 # 清除边缘 输入为反色图片
 def dropZeroEdges(img_inv):
     w_sums = np.sum(img_inv, axis=1)
-    print(w_sums)
-    avg = np.average(w_sums)
-    for i in [0,-1]:
-        print(w_sums[i], avg, w_sums[i]/avg)
-        if w_sums[i]/avg < 0.2:
+    avg = np.average(np.trim_zeros(w_sums))
+    for i in range(len(w_sums)):
+        if w_sums[i]/avg < 0.1:
             img_inv[i] = 0
 
     true_points = np.argwhere(img_inv)
