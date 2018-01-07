d89cc41d41a9034fc4dce3e6e8ae85fa4d4ddd2b
oneleaf <oneleaf@gmail.com>
2018-01-07 21:33:51 +0800

    .

--- 6/1/font_ascii_test.py
+++ 6/1/font_ascii_test.py
@@ -161,7 +161,7 @@ def train():
         session.run(init)
         while True:
             for batch in range(BATCHES):
-                batch_size = 16
+                batch_size = 2
                 train_inputs, train_trims, train_clears, train_half_clears = get_next_batch_for_gan(batch_size)
                 feed = {inputs: train_inputs, targets: train_trims}
 
