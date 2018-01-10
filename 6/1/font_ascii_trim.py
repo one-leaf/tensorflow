37786e8a12b16e2563ce150a87284a5f79f6c870
oneleaf <oneleaf@gmail.com>
2018-01-11 00:21:01 +0800

    .

--- 6/1/font_ascii_trim.py
+++ 6/1/font_ascii_trim.py
@@ -366,11 +366,8 @@ def train():
                         _c_net_g = np.squeeze(c_net_g[i], axis=2)
 
                         _t_img = utils.unsquare_img(_t_net_g, image_height)                        
-                        _t_img_p = np.ravel(_t_img)
-                        avg_vaue = np.average(np.trim_zeros(_t_img_p))
-                        _t_img_bin = np.copy(_t_img)
-                        _t_img_bin[_t_img_bin>=avg_vaue] = 1
-                        _t_img_bin[_t_img_bin<avg_vaue] = 0
+
+                        _t_img_bin=np.where(_t_img>0.4, 1, 0)
 
                         _img = np.vstack((_t_img, _t_img_bin))                         
                         cv2.imwrite(os.path.join(curr_dir,"test","S%s_%s.png"%(steps,i)), _img * 255)
