01c985ea79d49246aa51516c99576bd1fd35b038
oneleaf <oneleaf@gmail.com>
2018-01-10 22:25:15 +0800

    .

--- 6/1/utils.py
+++ 6/1/utils.py
@@ -259,8 +259,8 @@ def dropZeroEdges(img_inv, img_gray=None, min_rate=0):
     top_left = true_points.min(axis=0)
     bottom_right = true_points.max(axis=0)
     if top_left[0] == bottom_right[0] or top_left[1] == bottom_right[1] : return img_inv
-    # print(img_inv.shape)
-    # print(top_left[0], bottom_right[0], top_left[1], bottom_right[1])
+    print(img_inv.shape)
+    print(top_left[0], bottom_right[0], top_left[1], bottom_right[1])
     if img_gray == None:
         return img_inv[top_left[0]:bottom_right[0]+1, top_left[1]:bottom_right[1]+1]
     else:
