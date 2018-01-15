0a2548e4ca08d622c2b50c83594d059c151ef52f
王翩 <wp@bsw.com.cn>
2018-01-15 19:03:02 +0800

    .

--- 6/1/font_ascii_trim.py
+++ 6/1/font_ascii_trim.py
@@ -227,7 +227,7 @@ def get_next_batch_for_gan(batch_size=128):
         # 转黑白二值化，降低维度
         trims_image = (255. - trims_image) / 255.        
         trims_image = utils.img2bw(trims_image)
-        trims_image[trims_image==0]=-1
+        # trims_image[trims_image==0]=-1
         trim_images.append(trims_image)
 
         if random.random()>0.5:
