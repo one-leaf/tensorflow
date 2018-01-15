cd8d02fa273e81df952ed0ea29616feb77ea8940
oneleaf <oneleaf@gmail.com>
2018-01-15 21:34:17 +0800

    .

--- 6/1/font_ascii_test.py
+++ 6/1/font_ascii_test.py
@@ -281,7 +281,7 @@ def train():
                         AllLosts[key]=errR
 
                 # 报告
-                if steps >0 and steps % REPORT_STEPS == 0:
+                if steps >0 and steps % REPORT_STEPS < 2:
                     train_inputs, train_labels, train_seq_len, train_info = get_next_batch_for_res(batch_size)   
                     p_net_g = session.run(resize_layer, {inputs: train_inputs}) 
                     p_net_g = np.squeeze(p_net_g, axis=3)
