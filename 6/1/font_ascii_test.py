e111195f60fbd33b4622ad14c8388228d0150234
王翩 <wp@bsw.com.cn>
2018-01-15 17:22:59 +0800

    .

--- 6/1/font_ascii_test.py
+++ 6/1/font_ascii_test.py
@@ -289,7 +289,7 @@ def train():
                         AllLosts[key]=errR
 
                 # 报告
-                if steps % REPORT_STEPS == 1:
+                if steps == 1 or steps % REPORT_STEPS == 0:
                     train_inputs, train_labels, train_seq_len, train_info = get_next_batch_for_res(batch_size)   
                     p_net_g = session.run(resize_layer, {inputs: train_inputs}) 
                     p_net_g = np.squeeze(p_net_g, axis=3)
