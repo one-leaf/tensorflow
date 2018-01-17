79c76c8ad811d51d9d7a429b23c815d24e70c540
王翩 <wp@bsw.com.cn>
2018-01-17 13:40:19 +0800

    .

--- 6/1/font_ascii_test.py
+++ 6/1/font_ascii_test.py
@@ -307,7 +307,8 @@ def train():
                     decoded_list = session.run(res_decoded[0], {inputs: p_net_g, seq_len: train_seq_len, keep_prob: 1}) 
 
                     for i in range(batch_size): 
-                        _img = np.vstack((train_inputs[i], p_net_g[i])) 
+                        
+                        _img = np.vstack((utils.resize_image_size(train_inputs[i], p_net_g[i].size), p_net_g[i])) 
                         cv2.imwrite(os.path.join(curr_dir,"test","%s_%s.png"%(steps,i)), _img * 255) 
 
                     original_list = utils.decode_sparse_tensor(train_labels)
