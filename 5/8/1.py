5812ddc24a4eda9b11ae4b37e57be1107249b348
王翩 <wp@bsw.com.cn>
2018-01-24 16:54:03 +0800

    .

--- 5/8/1.py
+++ 5/8/1.py
@@ -48,6 +48,7 @@ def load_data_test():
         if data['database'][data_id]['subset'] == 'training':
             if f in _training_data:
                 training_data.append({'id':data_id,'data':data['database'][data_id]['annotations'],"shape":_training_data[f]})
+                break
         elif data['database'][data_id]['subset'] == 'validation':
             if f in _validation_data:
                 validation_data.append({'id':data_id,'data':data['database'][data_id]['annotations'],"shape":_validation_data[f]})
@@ -132,7 +133,6 @@ def simple_cnn(ipt):
 def network():
     # -1 ,2048 
     x = paddle.layer.data(name='x', type=paddle.data_type.dense_vector(2048*5))
-    y = paddle.layer.data(name='y', type=paddle.data_type.integer_value(3))
 
     layer = paddle.layer.fc(input=x, size=28*28*3, act=paddle.activation.Linear())
     #layer = resnet_cifar10(layer)   #1*1*64
@@ -150,13 +150,16 @@ def network():
     gru_backward = paddle.networks.simple_gru(input=sliced_feature, size=128, act=paddle.activation.Relu(), reverse=True)
 
     output = paddle.layer.fc(input=[gru_forward,gru_backward], size=class_dim, act=paddle.activation.Softmax())
+
+    y = paddle.layer.data(name='y', type=paddle.data_type.integer_value(3))
+
     cost = paddle.layer.classification_cost(input=output, label=y)
     parameters = paddle.parameters.create(cost)
     adam_optimizer = paddle.optimizer.Adam(
         learning_rate=2e-3,
         regularization=paddle.optimizer.L2Regularization(rate=8e-4),
         model_average=paddle.optimizer.ModelAverage(average_window=0.5))
-    return cost, parameters, adam_optimizer
+    return cost, parameters, adam_optimizer, output
 
  
 def reader_get_image_and_label(isTrain=True):
@@ -225,9 +228,10 @@ def event_handler(event):
 #         print "\nTest with Pass %d, %s" % (event.pass_id, result.metrics)
         
 print("paddle init ...")
-paddle.init(use_gpu=False, trainer_count=2)  
+# paddle.init(use_gpu=False, trainer_count=1) 
+paddle.init(use_gpu=False)  
 print("get network ...")
-cost, parameters, adam_optimizer = network()
+cost, parameters, adam_optimizer,  output= network()
 print('set reader ...')
 train_reader = paddle.batch(reader_get_image_and_label(), batch_size=256)
 feeding={'x': 0, 'y': 1}
@@ -243,3 +247,135 @@ trainer = paddle.trainer.SGD(cost=cost, parameters=parameters, update_equation=a
 print("start train ...")
 trainer.train(reader=train_reader, event_handler=event_handler, feeding=feeding, num_passes=1)
 
+def getTestData(testFileid):
+    if TEST:
+        v_data = np.random.random((random.randint(20,30), 2048))
+    else:
+        v_data = np.load(os.path.join(data_path,"testing", "%s.pkl"%testFileid))
+
+    data = []
+    batch_data = np.zeros((2048,5), dtype=np.float)  
+    for i in range(2):
+        _data = np.reshape(v_data[i], (2048,1))
+        np.append(batch_data[:, 1:], _data, axis=1)
+    w = v_data.shape[0]
+    label = np.zeros([w], dtype=np.int)
+    for i in range(w):
+        if i+2 >= w:
+            _data = np.zeros((2048,1))
+        else:
+            _data = np.reshape(v_data[i+2], (2048,1))
+        np.append(batch_data[:, 1:], _data, axis=1)
+        _data1 = np.ravel(batch_data)
+        data.append((_data1,))
+    return data
+
+# 转换分类到段
+# 0000111111111111100000
+# 0000000000001111111110
+def conv_to_segment(probs):
+    sort_probs = np.argsort(-probs)
+    value_probs = [v[0] for v in sort_probs]
+    items = []
+    score = 0
+    start = None
+    end = None
+    #先找重合的块：
+    for i, v in enumerate(value_probs):  
+        # 如果 v == 2 或者往后5秒内还有>0的，都算
+        _continue = False
+        if start != None and w > i:
+            for j in range(min(w-i,5)):
+                if value_probs[i+j+1]>0:
+                    _continue = True
+                    break
+        if v==2 or _continue :
+            if start==None:
+                start = i
+            else:
+                end = i
+            if v > 0:
+                score += probs[i][v]
+                value_probs[i] = value_probs[i]-1
+            else:
+                score += probs[i][1]
+        else:
+            if start!=None:
+                seg_value ={}
+                seg_value["score"]=score/(end-start)
+                seg_value["segment"]=[start, end]
+                start = None
+                end = None
+                items.append(seg_value)
+    if (start != None) and (end !=None):
+        seg_value ={}
+        seg_value["score"]=score/(end-start)
+        seg_value["segment"]=[start, end]
+        start = None
+        end = None
+        items.append(seg_value)
+
+    # 再来找正常的块
+    score = 0
+    start = None
+    end = None
+    #先找重合的块：
+    for i, v in enumerate(value_probs):  
+        # 如果 v == 1 或者往后5秒内还有>0的，都算
+        _continue = False
+        if start != None and w > i:
+            for j in range(min(w-i,5)):
+                if value_probs[i+j+1]>0:
+                    _continue = True
+                    break
+        if v==1 or _continue :
+            if start==None:
+                start = i
+            else:
+                end = i
+            if v > 0:
+                score += probs[i][v]
+            else:
+                score += probs[i][1]
+        else:
+            if start!=None:
+                seg_value ={}
+                seg_value["score"]=score/(end-start)
+                seg_value["segment"]=[start, end]
+                start = None
+                end = None
+                items.append(seg_value)
+    if (start != None) and (end !=None):
+        seg_value ={}
+        seg_value["score"]=score/(end-start)
+        seg_value["segment"]=[start, end]
+        start = None
+        end = None
+        items.append(seg_value)    
+    return items
+
+def test(output, parameters):
+    if TEST:
+        training_data, validation_data, testing_data = load_data_test()
+    else:
+        training_data, validation_data, testing_data = load_data() 
+
+    result={}
+    result["version"]="VERSION 1.0"
+    result["results"]={}
+
+    for data in testing_data:
+        data_id = data["id"]
+        data = getTestData(data_id)  
+        w = len(data)
+        probs = paddle.infer(output_layer=output, parameters=parameters, input=data)
+        items = conv_to_segment(probs)
+        result["results"][data_id] = items
+        print(json.dumps(result))
+        break
+
+    return result
+
+probs = test(output, parameters)
+
+print(probs)
