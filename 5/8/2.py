1c889282e3d618f68fd980399fd10cb8718679ab
王翩 <wp@bsw.com.cn>
2018-01-29 18:22:34 +0800

    .

--- 5/8/2.py
+++ 5/8/2.py
@@ -106,18 +106,18 @@ def network():
     y = paddle.layer.data(name='y', type=paddle.data_type.integer_value(3))
 
     layer = resnet(x, 8)
-    output = paddle.layer.fc(input=layer,size=class_dim,act=paddle.activation.Softmax())
+    # output = paddle.layer.fc(input=layer,size=class_dim,act=paddle.activation.Softmax())
 
-    # sliced_feature = paddle.layer.block_expand(input=x, num_channels=train_size, stride_x=1, stride_y=1, block_x=2048, block_y=1)
-    # gru_forward = paddle.networks.simple_gru(input=sliced_feature, size=64, act=paddle.activation.Relu())
+    sliced_feature = paddle.layer.block_expand(input=x, num_channels=train_size, stride_x=1, stride_y=1, block_x=2048, block_y=1)
+    gru_forward = paddle.networks.simple_gru(input=sliced_feature, size=64, act=paddle.activation.Relu())
 #     gru_backward = paddle.networks.simple_gru(input=sliced_feature, size=256, act=paddle.activation.Relu(), reverse=True)
 
-    # output = paddle.layer.fc(input=[gru_forward,layer], size=class_dim, act=paddle.activation.Softmax())
+    output = paddle.layer.fc(input=[gru_forward,layer], size=class_dim, act=paddle.activation.Softmax())
     
     cost = paddle.layer.classification_cost(input=output, label=y)
     parameters = paddle.parameters.create(cost)
     adam_optimizer = paddle.optimizer.Adam(
-        learning_rate=1e-2,
+        learning_rate=1e-3,
         regularization=paddle.optimizer.L2Regularization(rate=8e-4),
         model_average=paddle.optimizer.ModelAverage(average_window=0.5))
     return cost, parameters, adam_optimizer, output
