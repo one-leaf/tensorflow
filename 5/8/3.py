9834305facbb8fa5561923d418a4e2ddadd52e1a
王翩 <wp@bsw.com.cn>
2018-01-29 13:38:51 +0800

    .

--- 5/8/3.py
+++ 5/8/3.py
@@ -157,7 +157,7 @@ print("get network ...")
 cost, paddle_parameters, adam_optimizer, output = network()
 print('set reader ...')
 # train_reader = paddle.batch(paddle.reader.shuffle(reader_get_image_and_label(), buf_size=2048), batch_size=128)
-train_reader = paddle.batch(reader_get_image_and_label(True), batch_size=128)
+train_reader = paddle.batch(reader_get_image_and_label(), batch_size=128)
 feeding={'x': 0, 'y': 1}
 
    
