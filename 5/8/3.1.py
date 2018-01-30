cdbe754ecc2d47a70cf87b1d3a49a45173b829f9
王翩 <wp@bsw.com.cn>
2018-01-30 18:36:15 +0800

    .

--- 5/8/3.1.py
+++ 5/8/3.1.py
@@ -31,7 +31,7 @@ if not os.path.exists(model_path): os.mkdir(model_path)
 if not os.path.exists(out_dir): os.mkdir(out_dir)
 
 class_dim = 2 # 0 不是关键 1 是关键 2 重复关键
-train_size = 4 # 学习的关键帧长度
+train_size = 16 # 学习的关键帧长度
 
 def load_data(filter=None):
     data = json.loads(open(os.path.join(data_path,"meta.json")).read())
