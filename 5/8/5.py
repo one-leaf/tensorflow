b3d70f645cd61c072a41f407ebde1671c65d2e59
王翩 <wp@bsw.com.cn>
2018-01-31 17:54:01 +0800

    .

--- 5/8/5.py
+++ 5/8/5.py
@@ -97,7 +97,7 @@ def reader_get_image_and_label():
             for i in range(w):
                 _data = np.reshape(v_data[i], (2048,1))
                 batch_data = np.append(batch_data[:, 1:], _data, axis=1)
-                if i > train_size: 
+                if i > train_size and random.random() > 0.5: 
                     s = sum(label[i-train_size+1:i+1]) 
                     if c > 32 and s == train_size and random.random() > 0.25: continue                    
                     if c < -32 and s == 0 and random.random() > 0.25: continue                    
