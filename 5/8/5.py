54ae7aa6b7a7029065f3ea5e7c94d1310444e5ca
王翩 <wp@bsw.com.cn>
2018-01-31 11:59:59 +0800

    .

--- 5/8/5.py
+++ 5/8/5.py
@@ -86,7 +86,8 @@ def network():
     net3 = cnn(net3, 2, 64, 64, 2, 0)
     net3 = cnn(net3, 2, 64, 64, 2, 0)
 
-    output = paddle.layer.fc(input=[net0,net1,net2,net3],size=class_dim,act=paddle.activation.Softmax())
+    net = paddle.layer.concat([net0,net1,net2,net3])
+    output = paddle.layer.fc(input=net,size=class_dim,act=paddle.activation.Softmax())
 
     # sliced_feature = paddle.layer.block_expand(input=layer, num_channels=64, stride_x=1, stride_y=1, block_x=8, block_y=1)
     # gru_forward = paddle.networks.simple_gru(input=sliced_feature, size=64, act=paddle.activation.Relu())
@@ -127,7 +128,7 @@ def reader_get_image_and_label():
                             v = 0 
                         else:
                             v = 1
-                        if v==0 and random.random>0.5: continue
+                        if v==0 and random.random>0.75: continue
                         yield np.ravel(batch_data), v
             del v_data
     return reader
