0e12a2ff8f84ac846767b3d66b999d19391ad397
王翩 <wp@bsw.com.cn>
2018-02-01 13:39:37 +0800

    .

--- 5/8/5.py
+++ 5/8/5.py
@@ -136,8 +136,8 @@ def event_handler(event):
             sys.stdout.flush()
         
 print("paddle init ...")
-paddle.init(use_gpu=False, trainer_count=1) 
-# paddle.init(use_gpu=True, trainer_count=2)
+# paddle.init(use_gpu=False, trainer_count=1) 
+paddle.init(use_gpu=True, trainer_count=2)
 print("get network ...")
 cost, paddle_parameters, adam_optimizer, output = network()
 print('set reader ...')
