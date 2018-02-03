83277102ae2f32b113d06d839241066d41aa4ee1
oneleaf <oneleaf@gmail.com>
2018-02-03 22:51:36 +0800

    .

--- 5/8/6.py
+++ 5/8/6.py
@@ -112,7 +112,7 @@ def network():
     
     # net_class = paddle.layer.concat(input=nets_class)
     # gru_forward = paddle.networks.simple_gru(input=net, size=128, act=paddle.activation.Relu())
-    
+
     block_expand = paddle.layer.block_expand(input= net, num_channels=1, stride_x=1, stride_y=1, block_x=net.width, block_y=net.height)
     net_class = paddle.layer.fc(input=block_expand, size=class_dim, act=paddle.activation.Softmax())
     net_cost = paddle.layer.classification_cost(input=net_class, label=c)
@@ -126,7 +126,7 @@ def network():
 
     parameters = paddle.parameters.create(costs)
     adam_optimizer = paddle.optimizer.Adam(learning_rate=0.001)
-    return costs, parameters, adam_optimizer, (nets_class, nets_box) 
+    return costs, parameters, adam_optimizer, (net_class, net_box) 
 
 
 data_pool = []
