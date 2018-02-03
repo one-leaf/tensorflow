ac12645706bf7ef4fa0b9ebea6da2447d7b69267
oneleaf <oneleaf@gmail.com>
2018-02-03 23:56:27 +0800

    .

--- 5/8/6.py
+++ 5/8/6.py
@@ -72,7 +72,8 @@ def printLayer(layer):
 
 def network():
     # 每批32张图片，将输入转为 1 * 256 * 256 CHW 
-    x = paddle.layer.data(name='x', height=1, width=2048, type=paddle.data_type.dense_vector(train_size*2048))  
+    # x = paddle.layer.data(name='x', height=1, width=2048, type=paddle.data_type.dense_vector(train_size*2048))  
+    x = paddle.layer.data(name='x', type=paddle.data_type.dense_vector(train_size*2048))  
     # x_emb = paddle.layer.embedding(input=x, size=train_size*2048)  # emb一定要interger数据？
 
     c = paddle.layer.data(name='c', type=paddle.data_type.integer_value_sequence(class_dim))
