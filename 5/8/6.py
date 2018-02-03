4539eff4bcd106bda8f5a456efb271a4e847bdbb
oneleaf <oneleaf@gmail.com>
2018-02-03 21:25:03 +0800

    .

--- 5/8/6.py
+++ 5/8/6.py
@@ -101,14 +101,16 @@ def network():
         net = cnn1(main_nets[i], 3, 64, box_dim, 1, 0)
         nets_box.append(net)
 
-    gru_forward = paddle.networks.simple_gru(input=nets_class, size=128, act=paddle.activation.Relu())
+    
+    net_class = paddle.layer.concat(input=nets_class)
+    gru_forward = paddle.networks.simple_gru(input=net_class, size=128, act=paddle.activation.Relu())
 
     net_class = paddle.layer.fc(input=gru_forward, size=class_dim, act=paddle.activation.Softmax())
     net_cost = paddle.layer.classification_cost(input=net_class, label=c)
+  
+    net_box = paddle.layer.concat(input=nets_box)
+    gru_forward = paddle.networks.simple_gru(input=net_box, size=128, act=paddle.activation.Relu())
 
-    gru_forward = paddle.networks.simple_gru(input=nets_box, size=128, act=paddle.activation.Relu())
-
-    # net_box = paddle.layer.concat(input=nets_box)
     net_box = paddle.layer.fc(input=gru_forward, size=box_dim, act=paddle.activation.Tanh())
     box_cost = paddle.layer.square_error_cost(input=net_box, label=b)
     costs = [net_cost + box_cost]
