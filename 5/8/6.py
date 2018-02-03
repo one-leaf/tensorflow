c66e9680c39f8a1f751e3219ee8ec65c1a55b5d7
oneleaf <oneleaf@gmail.com>
2018-02-03 22:53:31 +0800

    .

--- 5/8/6.py
+++ 5/8/6.py
@@ -33,9 +33,9 @@ if not os.path.exists(out_dir): os.mkdir(out_dir)
 
 class_dim = 2 # 分类 0，背景， 1，精彩
 box_dim = 2 # 偏移，左，右
-train_size = 128 # 学习的关键帧长度
+train_size = 64 # 学习的关键帧长度
 buf_size = 8192
-batch_size = 1
+batch_size = 8
 
 def load_data(filter=None):
     data = json.loads(open(os.path.join(data_path,"meta.json")).read())
