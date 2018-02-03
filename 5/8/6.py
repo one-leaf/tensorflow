76998c1b88498c77307dca796e51b97601fa8c97
oneleaf <oneleaf@gmail.com>
2018-02-04 01:22:06 +0800

    .

--- 5/8/6.py
+++ 5/8/6.py
@@ -33,9 +33,9 @@ if not os.path.exists(out_dir): os.mkdir(out_dir)
 
 class_dim = 2 # 分类 0，背景， 1，精彩
 box_dim = 2 # 偏移，左，右
-train_size = 32 # 学习的关键帧长度
+train_size = 128 # 学习的关键帧长度
 buf_size = 8192
-batch_size = 64
+batch_size = 16
 
 def load_data(filter=None):
     data = json.loads(open(os.path.join(data_path,"meta.json")).read())
@@ -83,15 +83,15 @@ def network():
     # b_emb = paddle.layer.embedding(input=b, size=train_size)
 
     main_nets = []
-    net = cnn2(x,  4,  1, train_size, 1)
+    net = cnn2(x,  4,  1, 64, 1)
     main_nets.append(net)
-    net = cnn2(net, 4, train_size, train_size, 1)
+    net = cnn2(net, 4, 64, 64, 1)
     main_nets.append(net)
-    net = cnn2(net,  4,  train_size, train_size, 1)
+    net = cnn2(net,  4,  64, 64, 1)
     main_nets.append(net)
-    net = cnn2(net,  4,  train_size, train_size, 1)
+    net = cnn2(net,  4,  64, 64, 1)
     main_nets.append(net)
-    net = cnn2(net,  4,  train_size, train_size, 1)
+    net = cnn2(net,  4,  64, 64, 1)
     main_nets.append(net)
   
     # # 分类网络
@@ -114,7 +114,7 @@ def network():
     # net_class = paddle.layer.concat(input=nets_class)
     # gru_forward = paddle.networks.simple_gru(input=net, size=128, act=paddle.activation.Relu())
     printLayer(net)
-    block_expand = paddle.layer.block_expand(input= net, num_channels=train_size, stride_x=2, stride_y=2, block_x=2, block_y=2)
+    block_expand = paddle.layer.block_expand(input= net, num_channels=64, stride_x=2, stride_y=2, block_x=2, block_y=2)
     # block_expand = net
     printLayer(block_expand)
 
