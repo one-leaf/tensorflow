9f0149bc2e71650cacd5a6e343a461dad8f7b435
oneleaf <oneleaf@gmail.com>
2018-02-04 21:05:29 +0800

    .

--- 5/8/6.1.py
+++ 5/8/6.1.py
@@ -200,7 +200,6 @@ def test():
                 print(label[0:train_size])
                 print(len(label[0:train_size]))
                 q = (1.25, 1, 0.75, 0.5)
-                label2 = np.zeros([label_size], dtype=np.int)        
                 for i in sort[0:5]:
                     if has_class[i]<0.5: break
                     j = i//4
@@ -209,9 +208,10 @@ def test():
                     print(max(j*4-train_size*q[k], 0), min(j*4+train_size*q[k], train_size))
                     src= [max((j*4-train_size)*q[k], 0)+probs_net[i][0]*train_size, min((j*4+train_size)*q[k], train_size)+probs_net[i][1]*train_size]
                     print(src)
+                    label2 = np.zeros([label_size], dtype=np.int)        
                     for x in range(int(src[0]),int(src[1]+1)):
                         label2[x] = 1
-                print(label2[0:train_size]) 
+                    print(label2[0:train_size]) 
                 return
                 all_values.append(probs)
                 sys.stdout.write(".")
