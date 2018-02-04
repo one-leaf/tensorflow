ce94bae613f6967bb862f1763ff24a50e71daaf1
oneleaf <oneleaf@gmail.com>
2018-02-04 14:49:19 +0800

    .

--- 5/8/6.py
+++ 5/8/6.py
@@ -157,10 +157,10 @@ def readDatatoPool():
                     continue
                 fix_segments.append([max(0,segment[0]-(i-train_size)),min(train_size-1,segment[1]-(i-train_size))])
                 out_c, out_b = calc_value(fix_segments)
-                print("size:",i-train_size,i)
-                print("segments:", fix_segments)
-                print(out_c[train_size//2])    
-                print(out_b[train_size//2])    
+                # print("size:",i-train_size,i)
+                # print("segments:", fix_segments)
+                # print(out_c[train_size//2])    
+                # print(out_b[train_size//2])    
                 data_pool.append((batch_data, out_c, out_b))
         while len(data_pool)>buf_size:
             # print('r')
