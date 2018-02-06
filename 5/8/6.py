d1e4e4262c56d8fcab881c62950293e35c5deba9
oneleaf <oneleaf@gmail.com>
2018-02-06 19:48:13 +0800

    .

--- 5/8/6.py
+++ 5/8/6.py
@@ -36,7 +36,7 @@ class_dim = 2 # 分类 0，背景， 1，精彩
 box_dim = 2 # 偏移，左，右
 train_size = 512 # 学习的关键帧长度
 buf_size = 8192
-batch_size = 8
+batch_size = 4
 block_size = train_size//4
 area_ratio = (1.75, 1.5, 1.25, 1, 0.75, 0.5, 0.25, 0.125)
 
