875f2fd6e16ac73fa1dd98de823c6ef7bae78a07
王翩 <wp@bsw.com.cn>
2018-02-06 16:35:45 +0800

    .

--- 5/8/6.py
+++ 5/8/6.py
@@ -263,4 +263,4 @@ if os.path.exists(param_file):
 
 trainer = paddle.trainer.SGD(cost=cost, parameters=paddle_parameters, update_equation=adam_optimizer)
 print("start train ...")
-trainer.train(reader=train_reader, event_handler=event_handler, feeding=feeding, num_passes=train_size)
+trainer.train(reader=train_reader, event_handler=event_handler, feeding=feeding, num_passes=1)
