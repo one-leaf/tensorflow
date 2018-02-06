0730d130c4abbaa039498e7235b95fdfd4d83f65
王翩 <wp@bsw.com.cn>
2018-02-06 14:35:49 +0800

    .

--- 5/8/6.py
+++ 5/8/6.py
@@ -34,7 +34,7 @@ if not os.path.exists(out_dir): os.mkdir(out_dir)
 class_dim = 2 # 分类 0，背景， 1，精彩
 box_dim = 2 # 偏移，左，右
 train_size = 512 # 学习的关键帧长度
-buf_size = 128
+buf_size = 8192
 batch_size = 4
 block_size = train_size//4
 area_ratio = (1.75, 1.5, 1.25, 1, 0.75, 0.5, 0.25, 0.125)
