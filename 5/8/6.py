cc121eb6d32190f4082b757b6e05c09bb58c3f86
王翩 <wp@bsw.com.cn>
2018-02-06 17:04:02 +0800

    .

--- 5/8/6.py
+++ 5/8/6.py
@@ -31,7 +31,7 @@ out_dir = os.path.join(model_path, "out")
 if not os.path.exists(model_path): os.mkdir(model_path)
 if not os.path.exists(out_dir): os.mkdir(out_dir)
 
-channels_num = 32   # 图片先分层
+channels_num = 16   # 图片先分层
 class_dim = 2 # 分类 0，背景， 1，精彩
 box_dim = 2 # 偏移，左，右
 train_size = 512 # 学习的关键帧长度
