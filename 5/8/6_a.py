c965dd8ae7d9762d880f07a86560812e4275035e
oneleaf <oneleaf@gmail.com>
2018-02-12 21:19:42 +0800

    .

--- 5/8/6_a.py
+++ 5/8/6_a.py
@@ -150,7 +150,7 @@ def network(drop=True):
 
 
     # 当前图片精彩或非精彩分类
-    net_class_gru = paddle.networks.simple_gru(input=net, size=256, act=paddle.activation.Tanh())
+    net_class_gru = paddle.networks.simple_gru(input=net, size=64, act=paddle.activation.Tanh())
     net_class_fc = paddle.layer.fc(input=net_class_gru, size=class_dim, act=paddle.activation.Softmax())
     cost_class = paddle.layer.classification_cost(input=net_class_fc, label=a)
    
