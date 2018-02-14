bd97676ee64c75784f04279d98ce347bb73c9c54
oneleaf <oneleaf@gmail.com>
2018-02-14 22:01:55 +0800

    .

--- 5/8/6_a.py
+++ 5/8/6_a.py
@@ -202,9 +202,9 @@ status["steptime"]=time.time()
 def event_handler(event):
     if isinstance(event, paddle.event.EndIteration):
         if event.batch_id>0 and event.batch_id % 10 == 0:
-            print "Paid %.2f,Time %.2f, Pass %d, Batch %d/%d, Cost %f, %s (%s)" % (
+            print "Paid %.2f,Time %.2f, Pass %d, Batch %d/%d, Cost %f, %s" % (
                 time.time() - status["starttime"], time.time() - status["steptime"], event.pass_id, 
-                event.batch_id, all_batch_size//batch_size , event.cost, event.metrics, len(data_pools)) 
+                event.batch_id, all_batch_size//batch_size , event.cost, event.metrics) 
             status["steptime"]=time.time()
             # cls_parameters.to_tar(open(cls_param_file, 'wb'))
 
