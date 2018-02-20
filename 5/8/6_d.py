c32b62c1d3de4f6b538ed012962f74cc42a82ad7
oneleaf <oneleaf@gmail.com>
2018-02-20 18:48:45 +0800

    .

--- 5/8/6_d.py
+++ 5/8/6_d.py
@@ -24,7 +24,7 @@ class_dim = 2 # 分类 0，背景， 1，精彩
 train_size = 64 # 学习的关键帧长度
 block_size = 16
 
-buf_size = 10240
+buf_size = 5000
 batch_size = 2048//(train_size*block_size)
 
 model_path = os.path.join(home,"model")
