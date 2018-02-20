a1dd3317423cdba0888c6aee64097147a84322b2
oneleaf <oneleaf@gmail.com>
2018-02-20 18:20:51 +0800

    .

--- 5/8/6_d.py
+++ 5/8/6_d.py
@@ -155,6 +155,8 @@ def pre_data():
             data_0[j%10].append(_data)
             j += 1
 
+        status["progress"]=1.0 * c / size
+
 
 
 def reader_get_image_and_label():
@@ -189,11 +191,12 @@ def reader_get_image_and_label():
 status ={}
 status["starttime"]=time.time()
 status["steptime"]=time.time()
+status["progress"]=0
 def event_handler(event):
     if isinstance(event, paddle.event.EndIteration):
         if event.batch_id>0 and event.batch_id % 100 == 0:
-            print "Paid %.2f,Time %.2f, Pass %d, Batch %d, Cost %f, %s" % (
-                time.time() - status["starttime"], time.time() - status["steptime"], event.pass_id, 
+            print "Paid %.2f,Time %.2f, Progress %.2f, Pass %d, Batch %d, Cost %f, %s" % (
+                time.time() - status["starttime"], time.time() - status["steptime"], status["progress"], event.pass_id, 
                 event.batch_id, event.cost, event.metrics) 
             status["steptime"]=time.time()
             if event.cost<10:
