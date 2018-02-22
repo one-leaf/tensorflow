8961ab69016a2a348aeeed8ad8755975d37e1bf8
oneleaf <oneleaf@gmail.com>
2018-02-22 15:06:54 +0800

    .

--- 5/8/6.1.py
+++ 5/8/6.1.py
@@ -121,7 +121,7 @@ def event_handler(event):
 
 def train():
     print('set reader ...')
-    train_reader = paddle.batch(reader_get_image_and_label(), batch_size=32)
+    train_reader = paddle.batch(reader_get_image_and_label(), batch_size=64)
     feeding_class={'x':0, 'a':1} 
     trainer = paddle.trainer.SGD(cost=cost, parameters=cls_parameters, update_equation=adam_optimizer)
     print("start train class ...")
