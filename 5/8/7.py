7c92ae4f36c7a46591d555431614d9feb050aaca
oneleaf <oneleaf@gmail.com>
2018-02-25 16:33:52 +0800

    .

--- 5/8/7.py
+++ 5/8/7.py
@@ -101,11 +101,11 @@ def network(drop=True):
     bias_attr = paddle.attr.Param(initial_std=0., l2_rate=0.)
     relu = paddle.activation.Relu()
     linear = paddle.activation.Linear()
-    fc1 = paddle.layer.fc(input=net, size=train_size, act=linear, bias_attr=bias_attr)
+    fc1 = paddle.layer.fc(input=net, size=64, act=linear, bias_attr=bias_attr)
     lstm1 = paddle.layer.lstmemory(input=fc1, act=relu, bias_attr=bias_attr)
     inputs = [fc1, lstm1]
-    for i in range(2, 2):
-        fc = paddle.layer.fc(input=inputs, size=train_size, act=linear, param_attr=para_attr, bias_attr=bias_attr)
+    for i in range(2, 4):
+        fc = paddle.layer.fc(input=inputs, size=64, act=linear, param_attr=para_attr, bias_attr=bias_attr)
         lstm = paddle.layer.lstmemory(input=fc, reverse=(i % 2) == 0, act=relu, bias_attr=bias_attr)
         inputs = [fc, lstm]
     net_class_fc = paddle.layer.fc(input=inputs, size=class_dim, act=paddle.activation.Softmax(), bias_attr=bias_attr, param_attr=para_attr)
