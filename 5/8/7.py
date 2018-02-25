d335a7caf5a21493054a7acde0c1405390389cfa
oneleaf <oneleaf@gmail.com>
2018-02-25 22:36:17 +0800

    .

--- 5/8/7.py
+++ 5/8/7.py
@@ -95,9 +95,9 @@ def network(drop=True):
     net = normal_network(x, drop)
     # 当前图片精彩或非精彩分类
 
-    # lstm1 = paddle.layer.lstmemory(input=net, act=paddle.activation.Relu())
-    # lstm2 = paddle.layer.lstmemory(input=net, act=paddle.activation.Relu(), reverse=True)
-    # net_class_fc = paddle.layer.fc(input=[lstm1, lstm2], size=class_dim, act=paddle.activation.Softmax())
+    lstm1 = paddle.layer.lstmemory(input=net, act=paddle.activation.Relu())
+    lstm2 = paddle.layer.lstmemory(input=net, act=paddle.activation.Relu(), reverse=True)
+    net_class_fc = paddle.layer.fc(input=[lstm1, lstm2], size=class_dim, act=paddle.activation.Softmax())
 
     # fc_para_attr = paddle.attr.Param(learning_rate=1e-3)
     # lstm_para_attr = paddle.attr.Param(initial_std=0., learning_rate=1e-3)
@@ -114,10 +114,10 @@ def network(drop=True):
     #     inputs = [fc, lstm]
     # net_class_fc = paddle.layer.fc(input=inputs, size=class_dim, act=paddle.activation.Softmax(), bias_attr=bias_attr, param_attr=para_attr)
 
-    gru_forward = paddle.networks.simple_gru(input=net, size=64, act=paddle.activation.Relu())
-    gru_backward = paddle.networks.simple_gru(input=net, size=64, act=paddle.activation.Relu(), reverse=True)
-    net = paddle.layer.concat(input=[gru_forward, gru_backward])
-    net_class_fc = paddle.layer.fc(input=net, size=class_dim, act=paddle.activation.Softmax())
+    # gru_forward = paddle.networks.simple_gru(input=net, size=64, act=paddle.activation.Relu())
+    # gru_backward = paddle.networks.simple_gru(input=net, size=64, act=paddle.activation.Relu(), reverse=True)
+    # net = paddle.layer.concat(input=[gru_forward, gru_backward])
+    # net_class_fc = paddle.layer.fc(input=net, size=class_dim, act=paddle.activation.Softmax())
 
     # net_class_fc = paddle.layer.fc(input=net, size=class_dim, act=paddle.activation.Softmax())
     cost_class = paddle.layer.classification_cost(input=net_class_fc, label=a)
