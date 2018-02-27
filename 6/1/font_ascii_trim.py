b2c642923d01f39bacf3414d99ee1537676ba1be
王翩 <wp@bsw.com.cn>
2018-02-27 15:07:26 +0800

    .

--- 6/1/font_ascii_trim.py
+++ 6/1/font_ascii_trim.py
@@ -265,12 +265,12 @@ def train():
                 errD, errD1, errD2, _, steps = session.run([t_d_loss, t_d_loss_real, t_d_loss_fake, t_d_optim, t_global_step], feed)
                 print("T %d time: %4.4fs, d_loss: %.8f (d_loss_real: %.6f  d_loss_fake: %.6f)" % (steps, time.time() - start, errD, errD1, errD2))
                 
-                if errD1/errD1 < 0.9:
+                if errD1/errD2 < 0.9:
                     start = time.time()                
                     errD, errD1, errD2, _, steps = session.run([t_d_loss, t_d_loss_real, t_d_loss_fake, t_d_optim, t_global_step], feed)
                     print("T %d time: %4.4fs, d_loss: %.8f (d_loss_real: %.6f  d_loss_fake: %.6f)" % (steps, time.time() - start, errD, errD1, errD2))
                 
-                if errD1/errD1 < 0.9:
+                if errD1/errD2 < 0.9:
                     start = time.time()                
                     errD, errD1, errD2, _, steps = session.run([t_d_loss, t_d_loss_real, t_d_loss_fake, t_d_optim, t_global_step], feed)
                     print("T %d time: %4.4fs, d_loss: %.8f (d_loss_real: %.6f  d_loss_fake: %.6f)" % (steps, time.time() - start, errD, errD1, errD2))
