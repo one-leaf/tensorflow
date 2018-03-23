8b20634ace88dee3c76aa50313fe592832864455
王翩 <wp@bsw.com.cn>
2018-03-23 18:02:41 +0800

    .

--- 6/1/font_ascii_test.py
+++ 6/1/font_ascii_test.py
@@ -68,7 +68,7 @@ def RES(inputs, seq_len, reuse = False):
         layer = tf.reshape(layer, [batch_size, SEQ_LENGHT, 7*7]) # -1,1024,256
         print("resNet_seq shape:",layer.shape,"seq_len:",SEQ_LENGHT)
         # res_layer = slim.fully_connected(layer, 256, normalizer_fn=slim.batch_norm, activation_fn=None)  
-        # layer = LSTM(layer, seq_len, 256, 7*7)    # -1, 1024, 354
+        layer = LSTM(layer, seq_len, 256, 7*7)    # -1, 1024, 354
         # print("lstm shape:",layer.shape)
 
         # layer = tf.concat([layer, lstm_layer], axis=2)
@@ -81,13 +81,13 @@ def LSTM(inputs, seq_len, fc_size, lstm_size):
     layer = inputs
     for i in range(4):
         with tf.variable_scope("rnn-%s"%i):
-            # layer = slim.fully_connected(layer, fc_size, normalizer_fn=slim.batch_norm, activation_fn=None)
-            layer = slim.fully_connected(layer, fc_size, normalizer_fn=None, activation_fn=None)
+            layer = slim.fully_connected(layer, fc_size, normalizer_fn=slim.batch_norm, activation_fn=None)
+            # layer = slim.fully_connected(layer, fc_size, normalizer_fn=None, activation_fn=None)
             cell_fw = tf.contrib.rnn.GRUCell(lstm_size, activation=tf.nn.relu)
             cell_bw = tf.contrib.rnn.GRUCell(lstm_size, activation=tf.nn.relu)
             outputs, _ = tf.nn.bidirectional_dynamic_rnn(cell_fw, cell_bw, layer, seq_len, dtype=tf.float32)
             layer = tf.concat([outputs[0],outputs[1],layer], axis=2)
-            layer = slim.batch_norm(layer)
+            # layer = slim.batch_norm(layer)
     return layer
 
 def neural_networks():
