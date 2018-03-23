9e6a5986c02c7d6a56b6acafbeb67e2b7959787d
王翩 <wp@bsw.com.cn>
2018-03-23 15:22:30 +0800

    .

--- 6/1/font_ascii_test.py
+++ 6/1/font_ascii_test.py
@@ -145,6 +145,7 @@ def get_next_batch_for_res(batch_size=128, if_to_G=True, _font_name=None, _font_
     codes = []
     max_width_image = 0
     info = []
+    seq_len = np.ones(batch_size)
     for i in range(batch_size):
         font_name = _font_name
         font_size = _font_size
@@ -231,14 +232,14 @@ def get_next_batch_for_res(batch_size=128, if_to_G=True, _font_name=None, _font_
         codes.append([CHARS.index(char) for char in text])                  
 
         info.append([font_name, str(font_size), str(font_mode), str(font_hint)])
-
+        seq_len[i] = len(text)
     inputs = np.zeros([batch_size, image_size, image_size])
     for i in range(batch_size):
         inputs[i,:] = utils.square_img(inputs_images[i], np.zeros([image_size, image_size]), image_height)
 
     labels = [np.asarray(i) for i in codes]
     sparse_labels = utils.sparse_tuple_from(labels)
-    seq_len = np.ones(batch_size) * SEQ_LENGHT                
+    # seq_len = np.ones(batch_size) * SEQ_LENGHT
     return inputs, sparse_labels, seq_len, info
 
 def train():
--- 6/2/README.md
+++ 6/2/README.md
@@ -1,1 +1,9 @@
 参考 https://github.com/chineseocr/chinese-ocr
+
+1. 编译 bbox, CPU版本需要修改 setup.py 和 __init__.py 关闭 cuda 模块
+> cd lib/utils
+> sh make.sh
+
+2. 下载 VOCdevkit2007 到 data 目录 解压缩并改名为 VOCdevkit2007
+> http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtrainval_06-Nov-2007.tar
+
