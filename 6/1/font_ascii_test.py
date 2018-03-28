241c85187d6fe8d886d3a22a1231f50475baa4a9
oneleaf <oneleaf@gmail.com>
2018-03-28 22:42:11 +0800

    .

--- 6/1/font_ascii_test.py
+++ 6/1/font_ascii_test.py
@@ -197,7 +197,7 @@ def get_next_batch_for_res(batch_size=128, _font_name=None, _font_size=None, _fo
         codes.append([CHARS.index(char) for char in text])                  
 
         info.append([font_name, str(font_size), str(font_mode), str(font_hint)])
-        # seq_len[i] = len(text)
+        seq_len[i] = len(text)
 
         if max_width_image < image.shape[1]:
             max_width_image = image.shape[1]
@@ -210,11 +210,11 @@ def get_next_batch_for_res(batch_size=128, _font_name=None, _font_size=None, _fo
         image_vec = utils.img2vec(inputs_images[i], height=image_height, width=max_width_image, flatten=False)
         inputs[i,:] = np.reshape(image_vec,(image_height, max_width_image, 1))
 
-    # print(inputs.shape)
+    print(inputs.shape)
     labels = [np.asarray(i) for i in codes]
     sparse_labels = utils.sparse_tuple_from(labels)
-    seq_len = np.ones(batch_size) * SEQ_LENGHT
-    # print(seq_len)
+    # seq_len = np.ones(batch_size) * SEQ_LENGHT
+    print(seq_len)
     return inputs, sparse_labels, seq_len, info
 
 def train():
