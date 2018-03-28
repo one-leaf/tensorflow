fae8b94373d74a26255321e6aa6dd72ed2f4baf3
oneleaf <oneleaf@gmail.com>
2018-03-28 22:38:17 +0800

    .

--- 6/1/font_ascii_test.py
+++ 6/1/font_ascii_test.py
@@ -50,15 +50,15 @@ SEQ_LENGHT = 1024
 
 def RES(inputs, seq_len, reuse = False):
     with tf.variable_scope("OCR", reuse=reuse):
-        shape = tf.shape(inputs)
-        batch_size, height, width, channel = shape[0], shape[1], shape[2], shape[3]
-
         print("inputs shape:",inputs.shape)
-        layer = utils_nn.resNet101(inputs, True)    # -1 2 W/16 2048
+        layer = utils_nn.resNet101(inputs, True)    # -1 1 W/16 2048
 
+        shape = tf.shape(layer)
+        batch_size, height, width, channel = shape[0], shape[1], shape[2], shape[3]
+        
         print("resNet shape:",layer.shape)
         layer = slim.conv2d(layer, SEQ_LENGHT, [1,1], normalizer_fn=slim.batch_norm, activation_fn=tf.nn.relu) 
-        # -1 2 W/16 1024
+        # -1 1 W/16 1024
         # layer = tf.transpose(layer,[0,3,1,2])
         # layer = slim.avg_pool2d(layer,[2, 2])
         layer = tf.reshape(layer, [batch_size*height, width, SEQ_LENGHT]) # -1,1024,256
