893fe2cd7925b53cd79d5f3e1ac3888387a7c8f6
王翩 <wp@bsw.com.cn>
2018-03-29 13:08:02 +0800

    .

--- 6/1/font_ascii_test.py
+++ 6/1/font_ascii_test.py
@@ -213,7 +213,7 @@ def get_next_batch_for_res(batch_size=128, _font_name=None, _font_size=None, _fo
     for i in range(batch_size):
         # image = utils.img2vec(inputs_images[i], height=image_height, width=max_width_image, flatten=False)
         # inputs[i,:] = image_vec # np.reshape(image_vec,(image_height, max_width_image, 1))
-        image = inputs_images[i], image_height, max_width_image)
+        image = tf.image.resize_image_with_crop_or_pad(inputs_images[i], image_height, max_width_image)
         image = image.eval()
         print(image.shape)
 
