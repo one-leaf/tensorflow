8f08621dca9d1fe58487aca08032e1b327f21246
王翩 <wp@bsw.com.cn>
2018-04-03 18:22:56 +0800

    。

--- 6/2/ctpn/lib/datasets/pascal_voc.py
+++ 6/2/ctpn/lib/datasets/pascal_voc.py
@@ -13,8 +13,46 @@ import xml.etree.ElementTree as ET
 from .imdb import imdb
 from . import ds_utils
 from ..fast_rcnn.config import cfg
+'''
+<annotation>
+    <folder>VOC2007</folder>
+    <filename>009961.jpg</filename>
+    <source>
+        <database>The VOC2007 Database</database>
+        <annotation>PASCAL VOC2007</annotation>
+        <image>flickr</image>
+        <flickrid>334575803</flickrid>
+    </source>
+    <owner>
+        <flickrid>dictioncanary</flickrid>
+        <name>Lucy</name>
+    </owner>
+    <size><!--image shape-->
+        <width>500</width>
+        <height>374</height>
+        <depth>3</depth>
+    </size>
+    <segmented>0</segmented><!--是否有分割label-->
+    <object>
+        <name>dog</name> <!--类别-->
+        <pose>Unspecified</pose><!--物体的姿态-->
+        <truncated>0</truncated><!--物体是否被部分遮挡（>15%）-->
+        <difficult>0</difficult><!--是否为难以辨识的物体， 主要指要结体背景才能判断出类别的物体。虽有标注， 但一般忽略这类物体-->
+        <bndbox><!--bounding box-->
+            <xmin>69</xmin>
+            <ymin>4</ymin>
+            <xmax>392</xmax>
+            <ymax>345</ymax>
+        </bndbox>
+    </object>
+</annotation>
 
+分割的label由两部分组成： 
+* class segmentation: 标注出每一个像素的类别 
+* object segmentation: 标注出每一个像素属于哪一个物体 
+'''
 class pascal_voc(imdb):
+    # 初始化 pascal_voc 
     def __init__(self, image_set, year, devkit_path=None):
         imdb.__init__(self, 'voc_' + year + '_' + image_set)
         self._year = year
@@ -22,9 +60,9 @@ class pascal_voc(imdb):
         self._devkit_path = self._get_default_path() if devkit_path is None \
                             else devkit_path
         self._data_path = os.path.join(self._devkit_path, 'VOC' + self._year)
+        # ctpn 的分类，背景和文字【0，1】
         self._classes = ('__background__', # always index 0
                          'text')
-
         self._class_to_ind = dict(list(zip(self.classes, list(range(self.num_classes)))))
         self._image_ext = '.jpg'
         self._image_index = self._load_image_set_index()
@@ -37,8 +75,8 @@ class pascal_voc(imdb):
         # PASCAL specific config options
         self.config = {'cleanup'     : True,
                        'use_salt'    : True,
-                       'use_diff'    : False,
-                       'matlab_eval' : False,
+                       'use_diff'    : False,   # 是否使用难以识别的样本
+                       'matlab_eval' : False,   # 是否采用 matlab 来计算
                        'rpn_file'    : None,
                        'min_size'    : 2}
 
@@ -47,12 +85,14 @@ class pascal_voc(imdb):
         assert os.path.exists(self._data_path), \
                 'Path does not exist: {}'.format(self._data_path)
 
+    # 返回第 i 张图片的相对路径
     def image_path_at(self, i):
         """
         Return the absolute path to image i in the image sequence.
         """
         return self.image_path_from_index(self._image_index[i])
 
+    # 按文件名（不含扩展名），返回图片的相对路径
     def image_path_from_index(self, index):
         """
         Construct an image path from the image's "index" identifier.
@@ -63,6 +103,7 @@ class pascal_voc(imdb):
                 'Path does not exist: {}'.format(image_path)
         return image_path
 
+    # 加载所有图片文件名（不含扩展名）的清单
     def _load_image_set_index(self):
         """
         Load the indexes listed in this dataset's image set file.
@@ -77,12 +118,14 @@ class pascal_voc(imdb):
             image_index = [x.strip() for x in f.readlines()]
         return image_index
 
+    # 获得数据集的存放路径
     def _get_default_path(self):
         """
         Return the default path where PASCAL VOC is expected to be installed.
         """
         return os.path.join(cfg.DATA_DIR, 'VOCdevkit' + self._year)
 
+    # 获得真实样本的数据集
     def gt_roidb(self):
         """
         Return the database of ground-truth regions of interest.
@@ -132,6 +175,7 @@ class pascal_voc(imdb):
 
         return roidb
 
+    # 加载 rpn 产生的 roi
     def rpn_roidb(self):
         if int(self._year) == 2007 or self._image_set != 'test':
             gt_roidb = self.gt_roidb()
@@ -142,6 +186,7 @@ class pascal_voc(imdb):
 
         return roidb
 
+    # 读取 rpn 文件
     def _load_rpn_roidb(self, gt_roidb):
         filename = self.config['rpn_file']
         print('loading {}'.format(filename))
