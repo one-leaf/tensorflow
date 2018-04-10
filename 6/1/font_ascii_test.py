3ad590f7d19ea4b4e0bfaf0b6b51c8c19d050321
王翩 <wp@bsw.com.cn>
2018-04-10 10:06:55 +0800

    .

--- 6/1/font_ascii_test.py
+++ 6/1/font_ascii_test.py
@@ -104,10 +104,10 @@ def neural_networks():
     res_loss = tf.reduce_mean(tf.nn.ctc_loss(labels=labels, inputs=net_res, sequence_length=seq_len))
     # res_optim = tf.train.AdamOptimizer(LEARNING_RATE_INITIAL).minimize(res_loss, global_step=global_step, var_list=res_vars)
     # 防止梯度爆炸
-    # tvars = tf.trainable_variables()
+    tvars = tf.trainable_variables()
     res_optim = tf.train.AdamOptimizer(LEARNING_RATE_INITIAL)
-    grads, norm = tf.clip_by_global_norm(tf.gradients(res_loss, res_vars), 10.0)
-    res_optim = res_optim.apply_gradients(list(zip(grads, res_vars)), global_step=global_step)
+    grads, norm = tf.clip_by_global_norm(tf.gradients(res_loss, tvars), 10.0)
+    res_optim = res_optim.apply_gradients(list(zip(grads, tvars)), global_step=global_step)
 
     res_decoded, _ = tf.nn.ctc_beam_search_decoder(net_res, seq_len, beam_width=10, merge_repeated=False)
     res_acc = tf.reduce_sum(tf.edit_distance(tf.cast(res_decoded[0], tf.int32), labels, normalize=False))
