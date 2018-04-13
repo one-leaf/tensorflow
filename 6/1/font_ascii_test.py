aeda188eb10f3b76361eecdbb73e92f04d70883c
王翩 <wp@bsw.com.cn>
2018-04-13 15:53:04 +0800

    .

--- 6/1/font_ascii_test.py
+++ 6/1/font_ascii_test.py
@@ -85,8 +85,8 @@ def LSTM(inputs, fc_size, lstm_size):
             layer = slim.fully_connected(layer, fc_size, normalizer_fn=slim.batch_norm, activation_fn=None)
             # 注意 lstm 必需用 tanh 不能用 relu relu6 或 leaky_relu ,不然在后期会出现 lost nan 问题
             # 用 tanh 根本学习不出来，只能放弃
-            cell_fw = tf.contrib.rnn.GRUCell(lstm_size, activation=tf.nn.tanh)
-            cell_bw = tf.contrib.rnn.GRUCell(lstm_size, activation=tf.nn.tanh)
+            cell_fw = tf.contrib.rnn.GRUCell(lstm_size, activation=tf.nn.elu)
+            cell_bw = tf.contrib.rnn.GRUCell(lstm_size, activation=tf.nn.elu)
             outputs, _ = tf.nn.bidirectional_dynamic_rnn(cell_fw, cell_bw, layer, dtype=tf.float32)
             layer = tf.concat([outputs[0], outputs[1], layer], axis=-1)
             layer = tf.nn.leaky_relu(layer)
