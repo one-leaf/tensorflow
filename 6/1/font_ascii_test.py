8550457809e18890a02d5313b498139141d51411
王翩 <wp@bsw.com.cn>
2018-04-13 16:04:34 +0800

    。

--- 6/1/font_ascii_test.py
+++ 6/1/font_ascii_test.py
@@ -83,7 +83,9 @@ def LSTM(inputs, fc_size, lstm_size):
     for i in range(3):
         with tf.variable_scope("rnn-%s"%i):
             layer = slim.fully_connected(layer, fc_size, normalizer_fn=slim.batch_norm, activation_fn=None)
-            # activation 用 tanh 根本学习不出来 , elu 在后期有更好的表现，但计算的开销比较大，其他地方全部采用 leaky_relu
+            # activation 用 tanh 根本学习不出来 , 
+            # elu 在后期有更好的表现，但计算的开销比较大，
+            # 所以正向采用 leaky_relu， 反向用 elu 补偿一下
             cell_fw = tf.contrib.rnn.GRUCell(lstm_size, activation=tf.nn.leaky_relu)
             cell_bw = tf.contrib.rnn.GRUCell(lstm_size, activation=tf.nn.elu)
             outputs, _ = tf.nn.bidirectional_dynamic_rnn(cell_fw, cell_bw, layer, dtype=tf.float32)
