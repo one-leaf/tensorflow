576f1861df6957fa88f8d202eae2fb959bc57407
王翩 <wp@bsw.com.cn>
2018-04-13 16:44:42 +0800

    。

--- 6/1/font_ascii_test.py
+++ 6/1/font_ascii_test.py
@@ -84,7 +84,7 @@ def LSTM(inputs, fc_size, lstm_size):
         with tf.variable_scope("rnn-%s"%i):
             layer = slim.fully_connected(layer, fc_size, normalizer_fn=slim.batch_norm, activation_fn=None)
             # activation 全部用 tanh 根本学习不出来 , 
-            # 所以正向采用 leaky_relu， 反向用 tanh 在极小值地方补偿一下
+            # 所以在最后一轮的RNN反向用 tanh 在极小值地方补偿一下
             # 注意，没有证据表明这样更好，但心里安慰下
             cell_fw = tf.contrib.rnn.GRUCell(lstm_size, activation=tf.nn.leaky_relu)
             if i==2:
