db854bccd9f776d0d02581f2f8ef0874c2395506
王翩 <wp@bsw.com.cn>
2018-04-17 09:58:59 +0800

    .

--- 6/1/font_ascii_transformer.py
+++ 6/1/font_ascii_transformer.py
@@ -397,13 +397,14 @@ def train():
                 # 如果当前lost低于平均lost，就多训练
                 if errR/avg_losts > 2 or acc/avg_acc < 0.5:
                 # for _ in range(int(errR//avg_losts)):
-                    errR, acc, _ , decoded_list = session.run([res_loss, res_acc, res_optim, res_decoded[0]], feed)
+                    errR, acc, _ = session.run([res_loss, res_acc, res_optim], feed)
                     session.run(global_step.assign(steps))
                     accs.append(acc)
                     avg_acc = sum(accs)/len(accs)                  
                     print("%s, %d time: %4.4fs, res_acc: %.4f, avg_acc: %.4f, res_loss: %.4f, info: %s " % \
                         (time.ctime(), steps, time.time() - start, acc, avg_acc, errR, font_info))
                     if acc/avg_acc < 0.5:
+                        decoded_list = session.run(res_decoded[0], feed)
                         report(train_labels, decoded_list)
 
                 if steps<20000:        
