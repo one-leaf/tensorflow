8430bf4ac02bad873636911debe7a5d2fe2a212b
王翩 <wp@bsw.com.cn>
2018-04-18 16:32:57 +0800

    .

--- 6/1/font_ascii_lstm.py
+++ 6/1/font_ascii_lstm.py
@@ -60,7 +60,7 @@ def RES(inputs, seq_len, reuse = False):
         layer = slim.conv2d(layer, 256, [1,1], normalizer_fn=slim.batch_norm, activation_fn=None) 
        
         # 将图像宽度和高度 // 2
-        layer = slim.conv2d(layer, 256, [2,2], 2, normalizer_fn=slim.batch_norm, activation_fn=None) 
+        layer = slim.avg_pool2d(layer, [2, 3], 2, normalizer_fn=slim.batch_norm, activation_fn=None) 
 
         shape = tf.shape(layer)
         batch_size, width, channel = shape[0], shape[2], shape[3]
@@ -239,8 +239,8 @@ def get_next_batch_for_res(batch_size=128, _font_name=None, _font_size=None, _fo
         seq_len[i]=len(text)
 
     # 凑成2的整数倍
-    if max_width_image % 2 > 0:
-        max_width_image = max_width_image + (2 - max_width_image % 2)
+    # if max_width_image % 2 > 0:
+    #     max_width_image = max_width_image + (2 - max_width_image % 2)
 
     inputs = np.zeros([batch_size, image_height, max_width_image, 1])
     for i in range(batch_size):
