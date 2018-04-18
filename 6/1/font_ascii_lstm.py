49ba73486a82730f05a3c51010cfba5bb88e9158
王翩 <wp@bsw.com.cn>
2018-04-18 17:13:27 +0800

    .

--- 6/1/font_ascii_lstm.py
+++ 6/1/font_ascii_lstm.py
@@ -254,7 +254,7 @@ def get_next_batch_for_res(batch_size=128, _font_name=None, _font_size=None, _fo
     # print(inputs.shape, len(codes))
     labels = [np.asarray(i) for i in codes]
     sparse_labels = utils.sparse_tuple_from(labels)
-    seq_len = np.ones(batch_size) * (max_width_image//2)
+    # seq_len = np.ones(batch_size) * (max_width_image//2)
     # print(inputs.shape, seq_len.shape, [len(l) for l in labels])
     return inputs, sparse_labels, seq_len, info
 
