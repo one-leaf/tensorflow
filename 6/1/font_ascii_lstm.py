90f0b2f286a7e837532ec5984e9837ea0638468c
王翩 <wp@bsw.com.cn>
2018-04-18 17:40:52 +0800

    .

--- 6/1/font_ascii_lstm.py
+++ 6/1/font_ascii_lstm.py
@@ -75,19 +75,15 @@ def RES(inputs, seq_len, reuse = False):
 
 def LSTM(inputs, fc_size, lstm_size):
     layer = inputs
-    for i in range(2):
+    for i in range(3):
         with tf.variable_scope("rnn-%s"%i):
-            # activation 全部用 tanh 根本学习不出来 , 
-            # 所以反向用 tanh 在极小值地方补偿一下
-            # 如果用 tanh ，最后一层不能加 relu
-            # 注意，没有证据表明这样更好，但心里安慰下
+            # activation 用 tanh 根本学习不出来 , 模拟了残差网络
             cell_fw = tf.contrib.rnn.GRUCell(lstm_size, activation=tf.nn.leaky_relu)
             cell_bw = tf.contrib.rnn.GRUCell(lstm_size, activation=tf.nn.leaky_relu)
             # cell_fw = tf.contrib.rnn.GRUCell(lstm_size)
             # cell_bw = tf.contrib.rnn.GRUCell(lstm_size)
             outputs, _ = tf.nn.bidirectional_dynamic_rnn(cell_fw, cell_bw, layer, dtype=tf.float32)
             layer = tf.concat([outputs[0], outputs[1]], axis=-1) + layer
-            # layer = slim.fully_connected(layer, fc_size, normalizer_fn=slim.batch_norm, activation_fn=tf.nn.leaky_relu)
             layer = tf.nn.leaky_relu(layer)
     return layer
 
