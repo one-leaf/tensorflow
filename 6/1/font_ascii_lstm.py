dc97d57a0aee43743aefac3c512e765cde3b8561
王翩 <wp@bsw.com.cn>
2018-04-28 17:53:57 +0800

    。

--- 6/1/font_ascii_lstm.py
+++ 6/1/font_ascii_lstm.py
@@ -112,20 +112,37 @@ def orthogonal_initializer(shape, dtype=tf.float32, *args, **kwargs):
     w = u if u.shape == flat_shape else v
     return tf.constant(w.reshape(shape), dtype=dtype)
 
+# def LSTM(inputs, lstm_size, seq_len):
+#     layer = inputs
+#     for i in range(2):
+#         with tf.variable_scope("rnn-%s"%i):
+#             # activation 用 tanh 根本学习不出来 , 模拟了残差网络
+#             cell_fw = tf.contrib.rnn.GRUCell(lstm_size//2, 
+#                 activation=tf.nn.leaky_relu, 
+#                 kernel_initializer=orthogonal_initializer,
+#                 bias_initializer=tf.zeros_initializer)
+#             cell_bw = tf.contrib.rnn.GRUCell(lstm_size//2, 
+#                 activation=tf.nn.leaky_relu, 
+#                 kernel_initializer=orthogonal_initializer,
+#                 bias_initializer=tf.zeros_initializer)
+#             outputs, _ = tf.nn.bidirectional_dynamic_rnn(cell_fw, cell_bw, layer, sequence_length=seq_len, dtype=tf.float32)
+#             net = tf.concat(outputs, -1)  
+#             net = slim.fully_connected(net, lstm_size, normalizer_fn=slim.batch_norm, activation_fn=None)
+#             layer = tf.nn.leaky_relu(net + layer)
+#     return layer
+
 def LSTM(inputs, lstm_size, seq_len):
     layer = inputs
-    for i in range(2):
+    for i in range(3):
         with tf.variable_scope("rnn-%s"%i):
             # activation 用 tanh 根本学习不出来 , 模拟了残差网络
-            cell_fw = tf.contrib.rnn.GRUCell(lstm_size//2, 
+            cell = tf.contrib.rnn.GRUCell(lstm_size//2, 
                 activation=tf.nn.leaky_relu, 
                 kernel_initializer=orthogonal_initializer,
                 bias_initializer=tf.zeros_initializer)
-            cell_bw = tf.contrib.rnn.GRUCell(lstm_size//2, 
-                activation=tf.nn.leaky_relu, 
-                kernel_initializer=orthogonal_initializer,
-                bias_initializer=tf.zeros_initializer)
-            outputs, _ = tf.nn.bidirectional_dynamic_rnn(cell_fw, cell_bw, layer, sequence_length=seq_len, dtype=tf.float32)
+            # 在预测时，需要关闭
+            cell = tf.contrib.rnn.DropoutWrapper(cell, input_keep_prob=0.5)
+            outputs, _ = tf.nn.bidirectional_dynamic_rnn(cell, cell, layer, sequence_length=seq_len, dtype=tf.float32)
             net = tf.concat(outputs, -1)  
             net = slim.fully_connected(net, lstm_size, normalizer_fn=slim.batch_norm, activation_fn=None)
             layer = tf.nn.leaky_relu(net + layer)
