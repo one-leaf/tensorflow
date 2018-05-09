42fef4b1f60d8a0691479bb4de98bbb0cd93f9a0
王翩 <wp@bsw.com.cn>
2018-05-09 18:03:06 +0800

    .

--- 6/2/attention_ocr/python/train.py
+++ 6/2/attention_ocr/python/train.py
@@ -221,8 +221,8 @@ def main(_):
     print("labels_one_host_0:", data.labels_one_hot[0])
     print("#######################")
     provider = slim.dataset_data_provider.DatasetDataProvider(
-                    dataset, num_readers=3, shuffle=False
-    image, label, text = provider.get(['image', 'label', 'text'])
+                    dataset, num_readers=3, shuffle=False)
+    [image, label, text] = provider.get(['image', 'label', 'text'])
     init = tf.global_variables_initializer()
     with tf.Session() as session:
         session.run(init)
