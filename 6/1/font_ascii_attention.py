568347e8e6679d060b737674c8d2e1b1b879c681
王翩 <wp@bsw.com.cn>
2018-05-11 16:53:37 +0800

    .

--- 6/1/font_ascii_attention.py
+++ 6/1/font_ascii_attention.py
@@ -262,6 +262,8 @@ def accuracy(predictions, targets):
     with tf.variable_scope('CharAccuracy'):
         predictions.get_shape().assert_is_compatible_with(targets.get_shape())
         targets = tf.to_int32(targets)
+        print(NULL_CODE)
+        print(targets.get_shape())
         const_rej_char = tf.constant(NULL_CODE, shape=targets.get_shape(), dtype=tf.int32)
         weights = tf.to_float(tf.not_equal(targets, const_rej_char))
         correct_chars = tf.to_float(tf.equal(predictions, targets))
