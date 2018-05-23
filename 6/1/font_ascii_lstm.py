424126a7d3aefe006171edc362c68f4b7d486f85
王翩 <wp@bsw.com.cn>
2018-05-23 10:50:47 +0800

    .

--- 6/1/font_ascii_lstm.py
+++ 6/1/font_ascii_lstm.py
@@ -149,7 +149,7 @@ def LSTM(inputs, lstm_size, seq_len):
                 kernel_initializer=orthogonal_initializer,
                 bias_initializer=tf.zeros_initializer)
             outputs, _ = tf.nn.bidirectional_dynamic_rnn(cell_fw, cell_bw, layer, dtype=tf.float32)
-        layer += tf.nn.leaky_relu(outputs[0]+outputs[1])
+        layer += slim.batch_norm(outputs[0]+outputs[1])
     return layer
 
 # 这个模型中后期的网络非常不稳定
