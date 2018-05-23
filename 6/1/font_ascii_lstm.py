30704be3ab8b35de7c7fc3d1987c0829721e2058
王翩 <wp@bsw.com.cn>
2018-05-23 19:30:12 +0800

    .

--- 6/1/font_ascii_lstm.py
+++ 6/1/font_ascii_lstm.py
@@ -149,7 +149,7 @@ def LSTM(inputs, lstm_size, seq_len):
                 # activation=tf.nn.relu
                 )
             outputs, _ = tf.nn.bidirectional_dynamic_rnn(cell, cell, layer, dtype=tf.float32)
-        layer = tf.concat(outputs, -1)
+        layer += outputs[0] + outputs[1]
     return layer
 
 # 这个模型中后期的网络非常不稳定
