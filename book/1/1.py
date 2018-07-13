98a2f36f8ad7e5a03028dfbe1e7896c40ce40378
王翩 <wp@bsw.com.cn>
2018-07-13 17:05:45 +0800

    .

--- book/1/1.py
+++ book/1/1.py
@@ -3,9 +3,7 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 
-# 参考： https://www.oudahe.com/p/52564/
-
-# 用两个同心圆，取数据，分为3类
+# 用两个同心圆，取数据，分为3类，类别归一化
 r1 = 140
 r2 = 80
 batch_size=128
@@ -32,41 +30,47 @@ def reshape_matmul(y):
 
 def neural_networks():
     x = tf.placeholder(tf.float32, [batch_size, 2], name='x')
-    y = tf.placeholder(tf.float32, [None, 3], name='y')
+    y = tf.placeholder(tf.float32, [batch_size, 3], name='y')
     prediction_grid = tf.placeholder(tf.float32, [None, 2])
 
-    # svm
+    # svm 参数，一次计算3个
     b = tf.Variable(tf.random_normal(shape=[batch_size, 3]))
 
     # Gaussian (RBF) kernel
     gamma = tf.constant(-0.005)
     dist  = tf.reduce_sum(tf.square(x), 1)  #(batch_size,)
     dist  = tf.reshape(dist, [-1,1])    #(batch_size,1)
-    sq_dists  = tf.multiply(2., tf.matmul(x, tf.transpose(x)))  #(batch_size,batch_size)
+    # 线性核函数 
+    # 原理： my_kernel = tf.matmul(x, tf.transpose(x))
+    sq_dists = tf.multiply(2., tf.matmul(x, tf.transpose(x)))  #(batch_size,batch_size)
+    # 应用广播加法和减法操作，不用也可以
+    sq_dists = tf.add(tf.subtract(dist,sq_dists),tf.transpose(dist))
     my_kernel = tf.exp(tf.multiply(gamma, tf.abs(sq_dists)))    #(batch_size,batch_size)
 
     # Compute SVM Model
-    first_term = tf.reduce_sum(b)   #()
-    print('first_term', first_term.shape)
-    b_vec_cross = tf.matmul(b, tf.transpose(b)) #(256,256)
-    print('b_vec_cross', b_vec_cross.shape)
-    y_target_cross = reshape_matmul(y)          #(3,256,?) 
-    print('y_target_cross', y_target_cross.shape)
+    # 计算对偶损失, 同时计算3个
+    first_term = tf.reduce_sum(b, 0)   #(3,)
+    b_vec_cross = tf.matmul(b, tf.transpose(b)) #(batch_size,batch_size)
+    y_target_cross = reshape_matmul(y)          #(3,batch_size,batch_size) 
+    # 中间步骤：tf.multiply(b_vec_cross, y_target_cross)  #(3,batch_size,batch_size)
+    # 中间步骤：tf.multiply(my_kernel, tf.multiply(b_vec_cross, y_target_cross)) #(3,batch_size,batch_size)
     second_term = tf.reduce_sum(tf.multiply(my_kernel, tf.multiply(b_vec_cross, y_target_cross)),[1,2]) #(3,)
-    print('second_term', second_term.shape)
+    # 对偶问题，用最小化负数损失函数，tf.negative = -1 
     loss = tf.reduce_mean(tf.negative(tf.subtract(first_term, second_term)))
     optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(loss)
 
-    # 预测函数
-    rA = tf.reshape(tf.reduce_sum(tf.square(x),1),[-1,1])
-    rB = tf.reshape(tf.reduce_sum(tf.square(prediction_grid),1),[-1,1])
-    pred_sq_dist = tf.add(tf.subtract(rA, tf.multiply(2., tf.matmul(x, tf.transpose(prediction_grid)))), tf.transpose(rB))
-    pred_kernel = tf.exp(tf.multiply(gamma, tf.abs(pred_sq_dist)))  #(?,?)
-    print('pred_kernel.shape',pred_kernel.shape)
+    # Gaussian (RBF) prediction kernel 预测函数
+    # 原理： my_kernel = tf.matmul(x, tf.transpose(prediction_grid))
+    rA = tf.reshape(tf.reduce_sum(tf.square(x),1),[-1,1])   #（batch_size，1）
+    rB = tf.reshape(tf.reduce_sum(tf.square(prediction_grid),1),[-1,1])  #（?，1）
+    pred_sq_dist = tf.multiply(2., tf.matmul(x, tf.transpose(prediction_grid))) #（batch_size，?）
+    pred_sq_dist = tf.add(tf.subtract(rA, pred_sq_dist), tf.transpose(rB)) #(batch_size,?)
+    pred_kernel = tf.exp(tf.multiply(gamma, tf.abs(pred_sq_dist)))  #(batch_size,?)
 
-    prediction_output = tf.matmul(tf.multiply(tf.transpose(y),tf.transpose(b)), pred_kernel)
-    prediction = tf.arg_max(prediction_output-tf.expand_dims(tf.reduce_mean(prediction_output,1), 1), 0)
-    accuracy = tf.reduce_mean(tf.cast(tf.equal(prediction, tf.argmax(y,1)), tf.float32))
+    # 中间步骤： tf.multiply(y,b)  #（batch_size,3）
+    prediction_output = tf.matmul(tf.transpose(pred_kernel), tf.multiply(y,b))  #(?,3)
+    prediction = prediction_output-tf.expand_dims(tf.reduce_mean(prediction_output,1), 1)   #(?,3)
+    accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(prediction,1), tf.argmax(y,1)), tf.float32))
 
     return x,y,prediction_grid,prediction,accuracy,loss,optimizer
 
@@ -79,7 +83,7 @@ def main():
         loss_vec = []
         batch_accuracy = []
 
-        for epoch in range(5000):
+        for epoch in range(2000):
             train_x, train_y = getDatasets()
             _, cost, acc = sess.run([op,loss,accuracy],feed_dict={x:train_x,y:train_y,prediction_grid:train_x})
             loss_vec.append(cost)
@@ -91,15 +95,11 @@ def main():
         x_min, x_max = -160, 160
         y_min, y_max = -160, 160
         print(x_min, x_max, y_min, y_max)
-        xx, yy = np.meshgrid(np.arange(x_min, x_max, 1), np.arange(y_min, y_max, 1))
-        print('xx.shape',xx.shape)
-        print('yy.shape',yy.shape)
-        grid_points = np.c_[xx.ravel(), yy.ravel()]
-        print('grid_points.shape',grid_points.shape)
-        grid_predictions = sess.run(prediction,feed_dict={x: test_x, y: test_y, prediction_grid:grid_points})
-        print('grid_predictions.shape',grid_predictions.shape)
-        grid_predictions = grid_predictions.reshape(xx.shape)
-        print('grid_predictions.shape',grid_predictions.shape)
+        xx, yy = np.meshgrid(np.arange(x_min, x_max, 1), np.arange(y_min, y_max, 1))  #(320,320) (320,320)
+        grid_points = np.c_[xx.ravel(), yy.ravel()] # (10240，2)
+        grid_predictions = sess.run(prediction,feed_dict={x: test_x, y: test_y, prediction_grid:grid_points})   #（12400，）
+        grid_predictions = np.argmax(grid_predictions, 1)    #(10240，1)
+        grid_predictions = grid_predictions.reshape(xx.shape)   #（320，320）
 
         # Plot points and grid
         test_y = np.argmax(test_y, axis=1)
