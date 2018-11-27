7bb90a3b7b9d7e2374d5e5eb952617d9b4972abc
oneleaf <oneleaf@gmail.com>
2018-11-27 20:55:45 +0800

    .

--- book/deeplearning/code/08-3.py
+++ book/deeplearning/code/08-3.py
@@ -45,7 +45,7 @@ class network():
 
         # 学生网络
         self.student_layers = [self.x]
-        layer_widths=[512,256,128,64,32,16,16,10]
+        layer_widths=[32,32,32,16,16,10]
         for i, width in enumerate(layer_widths):
             if width==layer_widths[-1]:
                 layer = self.add_layer(self.student_layers[-1], width, False, False, 'student', 'layer_%s'%i)
@@ -58,7 +58,7 @@ class network():
         self.student_optimizer= tf.train.AdamOptimizer(0.001).minimize(self.student_cross_entropy, var_list=student_vars)
 
         # 中间层学习
-        self.teacher_student_loss = tf.losses.mean_squared_error(self.teacher_layers[3], self.student_layers[5])
+        self.teacher_student_loss = tf.losses.mean_squared_error(self.teacher_layers[3], self.student_layers[3])
         self.teacher_student_optimizer= tf.train.AdamOptimizer(0.001).minimize(self.teacher_student_loss, var_list=student_vars)
 
 
