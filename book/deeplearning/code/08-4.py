f3eb6577edf5a3514070e615e5efb75758024d4e
oneleaf <wp@bsw.com.cn>
2018-11-28 09:38:39 +0800

    .

--- book/deeplearning/code/08-4.py
+++ book/deeplearning/code/08-4.py
@@ -58,7 +58,7 @@ class network():
         self.teacher_student_loss = tf.losses.mean_squared_error(self.teacher_layers[3], self.student_layers[8])
         self.teacher_student_optimizer= tf.train.AdamOptimizer(0.001).minimize(self.teacher_student_loss, var_list=student_vars)
 
-        self.teacher_optimizer= tf.train.AdamOptimizer(0.001).minimize(self.teacher_cross_entropy)
+        self.teacher_optimizer= tf.train.AdamOptimizer(0.001).minimize(self.teacher_cross_entropy+self.teacher_student_loss)
         self.student_optimizer= tf.train.AdamOptimizer(0.0001).minimize(self.student_cross_entropy, var_list=student_vars)       
 
 def main():
