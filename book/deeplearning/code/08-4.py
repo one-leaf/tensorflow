183d873003f4f23cccf5f304a1a6acb193df4d8b
oneleaf <wp@bsw.com.cn>
2018-11-28 10:43:47 +0800

    .

--- book/deeplearning/code/08-4.py
+++ book/deeplearning/code/08-4.py
@@ -12,14 +12,31 @@ mnist = input_data.read_data_sets(os.path.join(curr_path,"../data"), one_hot=Tru
 
 # 定义神经网络
 class network():
+        # 增加 batch_normalization
+    def batch_norm(self, input, eps=1e-05):
+        ema = tf.train.ExponentialMovingAverage(decay=0.5)  
+        input_shape = input.get_shape().as_list() 
+        axes = list(range(len(input_shape) - 1))
+
+        pop_mean, pop_var = tf.nn.moments(input, axes)
+        def mean_var_with_update():
+            ema_apply_op = ema.apply([pop_mean, pop_var])
+            with tf.control_dependencies([ema_apply_op]):
+                return tf.identity(pop_mean), tf.identity(pop_var)
+        mean, variance = tf.cond(self.training, mean_var_with_update, lambda: (ema.average(pop_mean), ema.average(pop_var)))
+        beta = tf.Variable(initial_value=tf.zeros(input_shape[-1]))
+        gamma = tf.Variable(initial_value=tf.ones(input_shape[-1]))
+        output  = tf.nn.batch_normalization(input, mean, variance, beta, gamma, eps)
+        return output 
+
     # 增加层
-    def add_layer(self, input, out_size, active_fun, bn, name):
+    def add_layer(self, input, out_size, active_fun, bn):
         _, in_size = input.get_shape().as_list() 
         Weights = tf.Variable(tf.random_uniform([in_size, out_size],-1,1))
         biases = tf.Variable(tf.zeros([out_size]) + 0.1)
         Wx_plus_b = tf.matmul(input, Weights) + biases
         if bn:
-            Wx_plus_b = tf.layers.batch_normalization(Wx_plus_b, training=True, name='%s_bn'%name)
+            Wx_plus_b = self.batch_norm(Wx_plus_b)
         if active_fun:
             return tf.nn.relu(Wx_plus_b)
         return Wx_plus_b
@@ -27,15 +44,16 @@ class network():
     def __init__(self):
         self.x = tf.placeholder(tf.float32, [None, 784], name='x')
         self.y = tf.placeholder(tf.float32, [None, 10], name='y')
+        self.training = tf.placeholder_with_default(True, shape=(), name='training')
 
         # 教师网络
         self.teacher_layers = [self.x]
         layer_widths=[512,128,32,128,512,10]
-        for i, width in enumerate(layer_widths):
+        for width in layer_widths:
             if width == layer_widths[-1]:
-                layer = self.add_layer(self.teacher_layers[-1], width, False, False, 'teacher_layer_%s'%i)
+                layer = self.add_layer(self.teacher_layers[-1], width, False, False)
             else:
-                layer = self.add_layer(self.teacher_layers[-1], width, True, True, 'teacher_layer_%s'%i)
+                layer = self.add_layer(self.teacher_layers[-1], width, True, True)
             self.teacher_layers.append(layer)
         self.teacher_cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=self.y, logits=self.teacher_layers[-1]))
         self.teacher_accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(self.teacher_layers[-1],1),tf.argmax(self.y,1)),tf.float32))
@@ -43,12 +61,12 @@ class network():
         # 学生网络
         self.student_layers = [self.x]
         layer_widths=[32,32,32,32,32,32,32,32,32,32,32,10]
-        for i, width in enumerate(layer_widths):
+        for width in layer_widths:
             with tf.variable_scope('student_network'):
                 if width==layer_widths[-1]:
-                    layer = self.add_layer(self.student_layers[-1], width, False, False, 'student_layer_%s'%i)
+                    layer = self.add_layer(self.student_layers[-1], width, False, False)
                 else:
-                    layer = self.add_layer(self.student_layers[-1], width, True, True, 'student_layer_%s'%i)
+                    layer = self.add_layer(self.student_layers[-1], width, True, True)
                 self.student_layers.append(layer)
         self.student_cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=self.y, logits=self.student_layers[-1]))
         self.student_accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(self.student_layers[-1],1),tf.argmax(self.y,1)),tf.float32))
@@ -70,8 +88,8 @@ def main():
     loss_dict["teacher-student"]=[]
 
     for has_teacher in [True, False]:
+        net = network()
         with tf.Session() as sess:
-            net = network()
             sess.run(tf.global_variables_initializer())
             batch_size = 100
 
