20eeab3a1f679d1672a96d46264f970f224be132
oneleaf <wp@bsw.com.cn>
2018-11-28 09:54:26 +0800

    .

--- book/deeplearning/code/08-4.py
+++ book/deeplearning/code/08-4.py
@@ -95,7 +95,7 @@ def main():
                 loss_t,_= sess.run([net.teacher_student_loss, net.teacher_student_optimizer], feed_dict={net.x: batch_xs, net.y: batch_ys})   
             acc_t = net.teacher_accuracy.eval({net.x: mnist.test.images, net.y: mnist.test.labels})
             print(epoch,'teacher_student loss:' ,loss_t, 'teacher acc:', acc_t)
-            if loss_t<100: break
+            if loss_t<0.00001: break
 
         # 训练学生网络
         for epoch in range(50):
