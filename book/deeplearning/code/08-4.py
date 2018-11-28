ca9e15c3ac1e5dff751dc9a625a151a143f6b71e
oneleaf <wp@bsw.com.cn>
2018-11-28 09:51:40 +0800

    .

--- book/deeplearning/code/08-4.py
+++ book/deeplearning/code/08-4.py
@@ -35,7 +35,7 @@ class network():
             if width == layer_widths[-1]:
                 layer = self.add_layer(self.teacher_layers[-1], width, False, False, 'teacher_layer_%s'%i)
             else:
-                layer = self.add_layer(self.teacher_layers[-1], width, True, False, 'teacher_layer_%s'%i)
+                layer = self.add_layer(self.teacher_layers[-1], width, True, i>2, 'teacher_layer_%s'%i)
             self.teacher_layers.append(layer)
         self.teacher_cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=self.y, logits=self.teacher_layers[-1]))
         self.teacher_accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(self.teacher_layers[-1],1),tf.argmax(self.y,1)),tf.float32))
@@ -48,7 +48,7 @@ class network():
                 if width==layer_widths[-1]:
                     layer = self.add_layer(self.student_layers[-1], width, False, False, 'student_layer_%s'%i)
                 else:
-                    layer = self.add_layer(self.student_layers[-1], width, i!=5, i!=5, 'student_layer_%s'%i)
+                    layer = self.add_layer(self.student_layers[-1], width, True, False, 'student_layer_%s'%i)
                 self.student_layers.append(layer)
         self.student_cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=self.y, logits=self.student_layers[-1]))
         self.student_accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(self.student_layers[-1],1),tf.argmax(self.y,1)),tf.float32))
