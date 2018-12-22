91a1d2516bdd985174c656b933838727c823127f
oneleaf <wp@bsw.com.cn>
2018-12-22 11:09:10 +0800

    .

--- book/deeplearning/code/12-6.py
+++ book/deeplearning/code/12-6.py
@@ -14,20 +14,34 @@ if not os.path.exists(data_path): os.makedirs(data_path)
 if not os.path.exists(logs_path): os.makedirs(logs_path)
 
 # 定义wmt18数据集，前面为训练数据，后面为测试数据
-dataset_files = {"training-parallel-nc-v13.tgz":
-                        ["training-parallel-nc-v13/news-commentary-v13.zh-en.en",
-                         "training-parallel-nc-v13/news-commentary-v13.zh-en.zh"],
-                 "dev.tgz":["dev/newsdev2017-enzh-src.en.sgm",
-                            "dev/newsdev2017-enzh-ref.zh.sgm"]} 
+dataset_files = {"train": { "source": "training-parallel-nc-v13.tgz", 
+                            "files": {
+                                "en": "training-parallel-nc-v13/news-commentary-v13.zh-en.en",
+                                "zh": "training-parallel-nc-v13/news-commentary-v13.zh-en.zh"}
+                            },
+                "valid": { "source": "dev.tgz",
+                            "files": {
+                                "en":"dev/newsdev2017-enzh-src.en.sgm",
+                                "zh":"dev/newsdev2017-enzh-ref.zh.sgm"}
+                            }
+                }
+
+# 定义常量
+UNK = "<UNK>"   # 未知单词
+BLK = "<BLK>"   # 句子空白
+STR = "<STR>"   # 翻译开始
+EOF = "<EOF"    # 翻译结束
 
 # 获取 训练和测试数据
 def downloadData():
-    for dataset_file in dataset_files:
+    for key in dataset_files:
+        dataset_file = dataset_files[key]["source"]
         loc_file =  os.path.join(data_path, dataset_file)
         if not os.path.exists(loc_file) :  
             print("Download Dataset File %s ..."%dataset_file)
             urllib.request.urlretrieve ("http://data.statmt.org/wmt18/translation-task/"+dataset_file, loc_file)
-        for extract_file in dataset_files[dataset_file]:
+        for lang in dataset_files[key]["files"]:
+            extract_file = dataset_files[key]["files"][lang]
             loc_extract_file = os.path.join(data_path, extract_file)
             if not os.path.exists(loc_extract_file):
                 tar = tarfile.open(loc_file)
@@ -73,7 +87,7 @@ class word2vec_network():
                 if word in self.words:
                     data.append(self.words[word])
                 else:
-                    data.append(self.words['UNK'])
+                    data.append(self.words[UNK])
             if len(data)<span: continue
             for i in range(len(data)-span+1):
                 buffer = data[i:i+span]
@@ -98,7 +112,8 @@ class word2vec_network():
             return self.normalized_embeddings.eval()
 
 # 输出词向量和词典
-def word2vec(sentences_file, words_filename, words_embedding_filename, words_number=5000, embedding_size=32*32, epoch_number=100):
+def word2vec(sentences_file, words_filename, words_embedding_filename, word_attach=[UNK, BLK], 
+                words_number=5000, embedding_size=32*32, epoch_number=100):
     save_words_dict_file = os.path.join(logs_path, words_filename)
     if not os.path.exists(save_words_dict_file):
         print("Create", words_filename, "...")
@@ -106,9 +121,12 @@ def word2vec(sentences_file, words_filename, words_embedding_filename, words_num
         text = text.lower()
         text = text.replace("\n"," ")
         words=jieba.analyse.extract_tags(text, topK=words_number-1)
-        words_dict={"UNK":0}
-        for i,word in enumerate(words):
-            words_dict[word]=i+1
+        words_dict={}
+        for i, word in enumerate(word_attach):
+            words_dict[word] = i
+        n = len(word_attach)
+        for i, word in enumerate(words):
+            words_dict[word] = i + n
         save_words_dict_file = open(save_words_dict_file,"w",encoding="UTF8")
         json.dump(words_dict, save_words_dict_file, ensure_ascii=False)
     else:
@@ -119,19 +137,20 @@ def word2vec(sentences_file, words_filename, words_embedding_filename, words_num
     if not os.path.exists(save_words_embedding_filename):
         batch_size = 64
         net = word2vec_network(embedding_size, batch_size, words_dict, sentences_file)
-        print("start traing", words_embedding_filename, '...')
+        print("start training", words_embedding_filename, '...')
         final_embeddings = net.train(epoch_number)
         np.save(save_words_embedding_filename, final_embeddings)
 
 class translate_network():
-    def __init__(self, words, embedding_size, batch_size):
-        vocabulary_size = len(words)
+    def __init__(self, en_words, zh_words, embedding_size, batch_size):
+        en_vocabulary_size = len(en_words)
+        zh_vocabulary_size = len(zh_words)
         self.en = tf.placeholder(tf.int32, [None, None])
-        self.en_seq_len = tf.placeholder(tf.int32, [None],
+        self.en_seq_len = tf.placeholder(tf.int32, [None])
         self.zh = tf.placeholder(tf.int32, [None, None])
-        self.zh_seq_len = tf.placeholder(tf.int32, [None],
-        self.en_embedding = tf.Variable(tf.zeros([vocabulary_size, embedding_size]), dtype=tf.float32)
-        self.zh_embedding = tf.Variable(tf.zeros([vocabulary_size, embedding_size]), dtype=tf.float32)
+        self.zh_seq_len = tf.placeholder(tf.int32, [None])
+        self.en_embedding = tf.Variable(tf.zeros([en_vocabulary_size, embedding_size]), dtype=tf.float32)
+        self.zh_embedding = tf.Variable(tf.zeros([zh_vocabulary_size, embedding_size]), dtype=tf.float32)
         self.en_vec = tf.nn.embedding_lookup(self.en_embedding, self.en)
         self.zh_vec = tf.nn.embedding_lookup(self.zh_embedding, self.zh)
     
@@ -143,23 +162,27 @@ class translate_network():
             outputs, _ = tf.nn.bidirectional_dynamic_rnn(cell_fw, cell_bw, self.en_vec, seq_len, dtype=tf.float32)
             en_layer = tf.concat(outputs, axis=-1)
 
-
-          
-
 def main():
     # 下载文件
     downloadData()
 
-    words_number=5000
     embedding_size=32*32
+    en_words_number=8000
+    zh_words_number=5000
 
     # 训练并产生 embedding 文件
-    for sfile in dataset_files["training-parallel-nc-v13.tgz"]:
-        sentences_file = os.path.join(data_path, sfile)
-        words_filename = "%s_words.json"%sfile[-2:]
-        words_embedding_filename = "%s_embedding.npy"%sfile[-2:]
-        word2vec(sentences_file, words_filename, words_embedding_filename,
-                    words_number=words_number, embedding_size=embedding_size, epoch_number=1)
+    # 源语言，增加 BLK 和 UNK
+    en_sentences_file = os.path.join(data_path, dataset_files["train"]["files"]["en"])
+    en_words_filename = "en_words.json"
+    en_words_embedding_filename = "en_words.npy"
+    word2vec(en_sentences_file, en_words_filename, en_words_embedding_filename, word_attach=[BLK, UNK],
+             words_number=en_words_number, embedding_size=embedding_size, epoch_number=10)
+    # 目标语言，增加 BLK UNK STR EOF
+    zh_sentences_file = os.path.join(data_path, dataset_files["train"]["files"]["zh"])
+    zh_words_filename = "zh_words.json"
+    zh_words_embedding_filename = "zh_words.npy"
+    word2vec(zh_sentences_file, zh_words_filename, zh_words_embedding_filename, word_attach=[BLK, UNK, STR, EOF],
+             words_number=zh_words_number, embedding_size=embedding_size, epoch_number=10)
 
     # 
 
