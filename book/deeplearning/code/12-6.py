7b074be3b4db666122cec9494049a611e7604bcf
oneleaf <wp@bsw.com.cn>
2019-01-03 18:41:06 +0800

    .

--- book/deeplearning/code/12-6.py
+++ book/deeplearning/code/12-6.py
@@ -89,84 +89,190 @@ def load_sentences(sentences_file, words_dict, sentences_vec_file):
             for _ in range(words.count(' ')): words.remove(' ') 
             s_vec = []
             for w in words:
-                if w in words_dict:
-                    s_vec.append(words_dict[w])
-                else:
-                    s_vec.append(KEY_WORDS["<UNK>"])
+                s_vec.append(words_dict.get(w, KEY_WORDS["<UNK>"]))
             sentences_vec.append(s_vec)
         json.dump(sentences_vec, open(save_sentences_vec_file,"w",encoding="UTF8"))
     else:
         sentences_vec = json.load(open(save_sentences_vec_file,"r",encoding="UTF8"))
+    print("Sentences length", len(sentences_vec))
     return sentences_vec
 
 
 # 翻译seq2seq网络
 # 原理
-# how are you <SOS> 你 好 吗 <PAD> --> encode --> decode --> 你 好 吗 <EOS> <PAD>
-# 其中 <SOS> --> 你 ， 你 --> 好 , 好 --> 吗， 吗 --> <EOS>
+# how are you <SOS> 你  好  吗 <EOS> 
+# --> encode --> decode --> 
+#               你  好  吗 <EOS> 
+# 其中 <SOS> --> 你 ， 你 --> 好 , 好 --> 吗 ， 吗 --> <EOS>
 class translate_network():
     # 多层 rnn 模型
-    def get_rnn_cell(self, run_size, rnn_layers_num):
-        return tf.contrib.rnn.MultiRNNCell([tf.contrib.rnn.GRUCell(rnn_size) for _ in range(rnn_layers_num)])
+    def get_rnn_cell(self, rnn_size, rnn_layers_num):
+        def single_cell():
+            cell = tf.contrib.rnn.GRUCell(rnn_size)
+            keep_prob = tf.cond(self.is_training, lambda:tf.constant(0.8), lambda:tf.constant(1.0))
+            cell = tf.nn.rnn_cell.DropoutWrapper(cell, keep_prob) 
+            return cell
+        return tf.contrib.rnn.MultiRNNCell([single_cell() for _ in range(rnn_layers_num)])
 
-    def __init__(self, en_words, zh_words, embedding_size, rnn_size=128, rnn_layers_num=3, batch_size=64):
-        en_vocab_size = len(en_words)                                      # 英语词汇表长度
-        zh_vocab_size = len(zh_words)                                      # 中文词汇表长度
+    def __init__(self, en_words, zh_words, embedding_size, rnn_size=128, rnn_layers_num=3, beam_search=True, beam_size=10):
         self.en = tf.placeholder(tf.int32, [None, None])                        # 英文句子 [batch_size, sentence]
         self.en_seq_len = tf.placeholder(tf.int32, [None])                      # 英文句子的长度 [batch_size]
         self.zh = tf.placeholder(tf.int32, [None, None])                        # 中文句子 [batch_size, sentence]
         self.zh_seq_len = tf.placeholder(tf.int32, [None])                      # 中文句子的长度 [batch_size]
+        self.is_training = tf.placeholder_with_default(True, shape=(), name='is_training')
+        self.beam_search = beam_search              # 是否采用 beam_search 函数预测，从 beam_size 个最大概率后选中寻找
+        self.beam_size = beam_size
+
+        batch_size = tf.size(self.en_seq_len)
+        en_vocab_size = len(en_words)                                      # 英语词汇表长度
+        zh_vocab_size = len(zh_words)                                      # 中文词汇表长度
+
         # 英文词嵌入向量表 [batch_size vocab_size embed_dim] 这里可以用word2vec
         self.en_embedding = tf.contrib.layers.embed_sequence(self.en, en_vocab_size, embedding_size) 
         # 中文词嵌入向量表 [batch_size vocab_size embed_dim] 这里可以用word2vec      
         self.zh_embedding = tf.contrib.layers.embed_sequence(self.zh, zh_vocab_size, embedding_size)       
        
+        print(self.en_embedding)
+        print(self.zh_embedding)
+
+        # 单词转向量
+        self.en_embedded = tf.nn.embedding_lookup(self.en_embedding, self.en)
+        self.zh_embedded = tf.nn.embedding_lookup(self.zh_embedding, self.zh)
+
         # encoder 编码器
-        encoder_cell = self.get_rnn_cell(rnn_size, rnn_layers_num)
-        encoder_outputs, encoder_state = tf.nn.dynamic_rnn(encoder_cell, self.en_embedding, self.en_seq_len, dtype=tf.float32)
+        with tf.variable_scope('encoder'):
+            fw_cell = self.get_rnn_cell(rnn_size, rnn_layers_num)
+            bw_cell = self.get_rnn_cell(rnn_size, rnn_layers_num)
+            bi_outputs, bi_state = tf.nn.bidirectional_dynamic_rnn(fw_cell, bw_cell, self.en_embedding, self.en_seq_len, dtype=tf.float32)
+            encoder_outputs = tf.concat(bi_outputs, -1)
+            print('encoder_outputs', encoder_outputs)
+            encoder_state = []
+            for i in range(rnn_layers_num):
+                encoder_state.append(bi_state[0][i])
+                encoder_state.append(bi_state[1][i])
+            encoder_state = tuple(encoder_state)
+            print('encoder_state', encoder_state)
 
-        # attention 注意力
-        attention_mechanism = tf.contrib.seq2seq.LuongAttention(num_units=rnn_size, memory=encoder_outputs)
-        decoder_cell = self.get_rnn_cell(rnn_size, rnn_layers_num)
-        decoder_cell = tf.contrib.seq2seq.AttentionWrapper(decoder_cell, attention_mechanism, attention_layer_size=rnn_size)
+        with tf.variable_scope('decoder'):
+            memory = encoder_outputs
+            if self.beam_search:
+                print("use beamsearch decoding ...")
+                memory = tf.contrib.seq2seq.tile_batch(memory, self.beam_size)
+                x_len = tf.contrib.seq2seq.tile_batch(self.en_seq_len, self.beam_size)
+                encoder_state = tf.contrib.seq2seq.tile_batch(encoder_state, self.beam_size)
+                bs = batch_size * self.beam_size
+            else:
+                x_len = self.en_seq_len
+                bs = batch_size
 
-        # 定义 训练 helper
-        helper = tf.contrib.seq2seq.TrainingHelper(self.zh_embedding, self.zh_seq_len)
-        
-        # decoder 解码器
-        projection_layer = tf.layers.Dense(zh_vocabulary_size, use_bias=False)
-        decoder = tf.contrib.seq2seq.BasicDecoder(decoder_cell, helper, encoder_state, projection_layer)
+            # multiplicative attention 注意力乘法模式
+            attention = tf.contrib.seq2seq.LuongAttention(rnn_size, memory, x_len) 
+            # additive attention 注意力加法模式
+            # attention = tf.contrib.seq2seq.BahdanauAttention(rnn_size, memory, x_len, normalize=True)
+            print('attention', attention) 
 
-        # 定义最长翻译输出为输入的20倍
-        maximum_iterations = tf.round(tf.reduce_max(self.en_seq_len) * 20)
-        outputs, _ = tf.contrib.seq2seq.dynamic_decode(decoder, maximum_iterations=maximum_iterations)
-        self.logits = outputs.rnn_output
+            # 由于encoder采用了双向RNN，所以这里需要*2
+            decoder_cell = self.get_rnn_cell(rnn_size, rnn_layers_num * 2)
+            decoder_cell = tf.contrib.seq2seq.AttentionWrapper(decoder_cell, attention, rnn_size)
+            print('decoder_cell', decoder_cell) 
+            decoder_initial_state = decoder_cell.zero_state(bs, tf.float32).clone(cell_state=encoder_state)
+            print('decoder_initial_state', decoder_initial_state)
 
-        # 定义预测输出 Infer
-        # 定义 预测 helper ，这里的 zh_embedding 为 <STR> zh_seq_len 为 [batch_size] 值为 KEY_WORS["<SOS>"] 表示翻译开始
-        infer_helper = tf.contrib.seq2seq.GreedyEmbeddingHelper(self.zh_embedding, self.zh_seq_len, KEY_WORDS["<EOS>"])         
-        decoder = tf.contrib.seq2seq.BasicDecoder(decoder_cell, infer_helper, encoder_state, projection_layer)
-        infer_outpus, _ = tf.contrib.seq2seq.dynamic_decode(decoder, maximum_iterations=maximum_iterations)
-        self.translations = infer_outpus.sample_id
+            # decoder 解码器
+            projection_layer = tf.layers.Dense(zh_vocab_size, use_bias=False, kernel_initializer=tf.contrib.layers.xavier_initializer())
+            print('projection_layer', projection_layer)
+            # 定义最长翻译输出为输入的20倍
+            maximum_iterations = tf.round(tf.reduce_max(self.en_seq_len) * 20)
 
-        targets = tf.reshape(self.zh, [-1])
-        logits_flat = tf.reshape(logits, [-1, zh_vocabulary_size])
-        self.cost = tf.losses.sparse_softmax_cross_entropy(targets, logits_flat)
-        tvars = tf.trainable_variables()
-        grads, _ = tf.clip_by_global_norm(tf.gradients(self.cost, tvars), grad_clip)
-        optimizer = tf.train.AdamOptimizer(1e-3)
-        self.train_op = optimizer.apply_gradients(zip(grads, tvars))
+            # 定义 训练 helper , 如何根据预测结果得到下一时刻的输入。
+            # TrainingHelper 直接用上一时刻的真实值作为下一时刻的输入
+            train_helper = tf.contrib.seq2seq.TrainingHelper(self.zh_embedding, self.zh_seq_len)
+            train_decoder = tf.contrib.seq2seq.BasicDecoder(decoder_cell, train_helper, decoder_initial_state, projection_layer)
+            train_outputs, final_context_state, _ = tf.contrib.seq2seq.dynamic_decode(train_decoder)
+            self.logits = train_outputs.rnn_output  # [batch_size, zh_seq_len, vocab_size]
+            print("logits", self.logits)
 
-def translate():
-    pass
+        with tf.variable_scope("decoder", reuse=True):
+            # 定义预测输出 Infer
+            # start_tokens = tf.fill([batch_size], KEY_WORDS["<SOS>"])
+            start_tokens = tf.ones([batch_size,],tf.int32) * KEY_WORDS["<SOS>"]
+            print('start_tokens',start_tokens)
+            if self.beam_search:
+                infer_decoder = tf.contrib.seq2seq.BeamSearchDecoder(decoder_cell, self.zh_embedding, 
+                    start_tokens, KEY_WORDS["<EOS>"], decoder_initial_state, self.beam_size, projection_layer)
+                print("infer_decoder",infer_decoder)
+                infer_outpus, final_context_state, _ = tf.contrib.seq2seq.dynamic_decode(infer_decoder, maximum_iterations=maximum_iterations)
+                self.translations = infer_outpus.predicted_ids  # [batch_size, zh_seq_len, beam_size]
+            else:
+                # 定义 预测 helper ，GreedyEmbeddingHelper 用当前概率最大的输出作为下一时刻的输入
+                # 这里的 zh_embedding 为 <STR> zh_seq_len 为 [batch_size] 值为 KEY_WORS["<EOF>"] 表示翻译结束
+                infer_helper = tf.contrib.seq2seq.GreedyEmbeddingHelper(self.zh_embedding, start_tokens, KEY_WORDS["<EOS>"])   
+                infer_decoder = tf.contrib.seq2seq.BasicDecoder(decoder_cell, infer_helper, decoder_initial_state, projection_layer)
+                print("infer_decoder",infer_decoder)
+                infer_outpus, final_context_state, _ = tf.contrib.seq2seq.dynamic_decode(infer_decoder, maximum_iterations=maximum_iterations)
+                self.translations = tf.expand_dims(infer_outpus.sample_id, -1)  # [batch_size, zh_seq_len, 1]
+
+            print("translations", self.translations)
+
+
+            # 定义训练损失函数
+            # targets = tf.reshape(self.zh, [-1])
+            # logits_flat = tf.reshape(self.logits, [-1, zh_vocab_size])
+            # self.cost = tf.losses.sparse_softmax_cross_entropy(targets, logits_flat)
+            max_zh_seq_len = tf.reduce_max(self.zh_seq_len)
+            mask = tf.sequence_mask(self.zh_seq_len, max_zh_seq_len, dtype=tf.float32)
+            logits_train = tf.arg_max(self.logits, axis=-1)
+            self.cost = tf.contrib.seq2seq.sequence_loss(logits=logits_train, targets=self.zh_embedding, weights=mask)
+            tvars = tf.trainable_variables()
+            grads, _ = tf.clip_by_global_norm(tf.gradients(self.cost, tvars), 5.0)
+            optimizer = tf.train.AdamOptimizer(1e-3)
+            self.train_op = optimizer.apply_gradients(zip(grads, tvars))
+
+def get_Data(en_sentences_vec, zh_sentences_vec, batch_size):
+    en_len = len(en_sentences_vec)
+    ids = random.sample(range(en_len), batch_size)
+
+    # 抽取数据，加上前后开头
+    en_sentences_vec_batch=[] 
+    zh_sentences_vec_batch=[] 
+    for id in ids:
+        en_sentences_vec_batch.append(en_sentences_vec[id])
+        zh_sentences_vec_batch.append([KEY_WORDS["<SOS>"]] + zh_sentences_vec[id] + [KEY_WORDS["<EOS>"]])
+
+    # 确定输入序列长度
+    en_sentences_vec_batch_lens = [len(sentence) for sentence in en_sentences_vec_batch]
+    zh_sentences_vec_batch_lens = [len(sentence) for sentence in zh_sentences_vec_batch]
+
+    # 求当前批次最大长度
+    en_sentences_vec_batch_maxlen = max(en_sentences_vec_batch_lens)
+    zh_sentences_vec_batch_maxlen = max(zh_sentences_vec_batch_lens)
+
+    # 按当前批次最大长度补齐
+    en_sentences_vec_batch_pad = [sentence + [KEY_WORDS["<PAD>"]] * (en_sentences_vec_batch_maxlen - len(sentence)) for sentence in en_sentences_vec_batch]
+    zh_sentences_vec_batch_pad = [sentence + [KEY_WORDS["<PAD>"]] * (zh_sentences_vec_batch_maxlen - len(sentence)) for sentence in zh_sentences_vec_batch]
+
+    return en_sentences_vec_batch_pad, en_sentences_vec_batch_lens, zh_sentences_vec_batch_pad, zh_sentences_vec_batch_lens
+
+def train(en_sentences_vec, zh_sentences_vec, net, batch_size=30):
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        for step in range(len(en_sentences_vec)//batch_size): 
+            # 获得训练数据
+            en_batch, en_batch_lens, zh_batch, zh_batch_lens = get_Data(en_sentences_vec, zh_sentences_vec, batch_size)
+            # 训练
+            _, loss = sess.run( [net.train_op, net.cost],
+                {net.en: en_batch, net.en_seq_len: en_batch_lens, 
+                 net.zh: zh_batch, net.zh_seq_len: zh_batch_lens})  
+            print(loss)      
 
 def main():
     # 下载文件
     downloadData()
 
     embedding_size=32*32
-    en_words_number=20000
-    zh_words_number=30000
+    en_words_number=50000   # 共 53839 个单词
+    zh_words_number=90000   # 共 91436 个单词
+    test_eng_sentence = "Knowledge is power."
 
     # 训练并产生 embedding 文件
     # 源语言
@@ -178,9 +284,10 @@ def main():
     zh_words_dict = load_word_dict(zh_sentences_file, "zh_words.json", words_number=zh_words_number)
     zh_sentences_vec = load_sentences(zh_sentences_file, zh_words_dict, "zh_sentences_vec.json")
 
+    net =  translate_network(en_words_dict, zh_words_dict, embedding_size, beam_search=True)
 
     # 训练SEQ-SEQ网络
-    translate()
+    train(en_sentences_vec, zh_sentences_vec, net, batch_size=64)
 
 if __name__ == "__main__":
     main()
