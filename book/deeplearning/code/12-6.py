5c4f6450e7a68167de5f284744cadd548b025cb1
oneleaf <wp@bsw.com.cn>
2019-01-04 16:09:16 +0800

    .

--- book/deeplearning/code/12-6.py
+++ book/deeplearning/code/12-6.py
@@ -297,7 +297,7 @@ def train(en_sentences_vec, zh_sentences_vec, net, batch_size=30):
 
 def get_saver():
     saver = tf.train.Saver(max_to_keep=5, keep_checkpoint_every_n_hours=1)
-    checkpoint_prefix = os.path.join(logs_path, "model.ckpt")
+    checkpoint_prefix = os.path.join(logs_path, "12_6_model.ckpt")
     ckpt = tf.train.get_checkpoint_state(logs_path)
     if ckpt and ckpt.model_checkpoint_path:
         print("restore checkpoint and continue train.")
